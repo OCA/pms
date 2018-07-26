bf886eaeef43cf29c95337f73c3dd4778f26f22a
Pablo Quesada Barriuso <pabloqb@gmail.com>
2018-07-26 17:08:20 +0200

    [WIP] Minor cleanup

--- hotel/models/hotel_room.py
+++ hotel/models/hotel_room.py
@@ -30,18 +30,6 @@ class HotelRoom(models.Model):
                                       'room_amenities', 'rcateg_id',
                                       string='Room Amenities',
                                       help='List of room amenities.')
-
-    # default price for this room
-    list_price = fields.Float(store=True,
-                              string='Room Rate',
-                              help='The room rate is fixed unless a room type'
-                              ' is selected, in which case the rate is taken from'
-                              ' the room type.')
-    # how to manage the price
-    # sale_price_type = fields.Selection([
-    #     ('fixed', 'Fixed Price'),
-    #     ('vroom', 'Room Type'),
-    # ], 'Price Type', default='fixed', required=True)
     # max number of adults and children per room
     max_adult = fields.Integer('Max Adult')
     max_child = fields.Integer('Max Child')
@@ -57,49 +45,3 @@ class HotelRoom(models.Model):
         help="A description of the Product that you want to communicate to "
              " your customers. This description will be copied to every Sales "
              " Order, Delivery Order and Customer Invoice/Credit Note")
-
-
-    # In case the price is managed from a specific type of room
-    # price_virtual_room = fields.Many2one(
-    #     'hotel.virtual.room',
-    #     'Price Virtual Room',
-    #     help='Price will be based on selected Virtual Room')
-
-    # virtual_rooms = fields.Many2many('hotel.virtual.room',
-    #                                  string='Virtual Rooms')
-    # categ_id = fields.Selection([('room', 'Room '),
-    #                              ('shared_room', 'Shared Room'),
-    #                              ('parking', 'Parking')],
-    #                             string='Hotel Lodging Type',
-    #                             store=True, default='room')
-
-#     price_virtual_room_domain = fields.Char(
-#         compute=_compute_price_virtual_room_domain,
-#         readonly=True,
-#         store=False,
-#     )
-
-#     @api.multi
-#     @api.depends('categ_id')
-#     def _compute_price_virtual_room_domain(self):
-#         for rec in self:
-#             rec.price_virtual_room_domain = json.dumps(
-#                 ['|', ('room_ids.id', '=', rec.id), ('room_type_ids.cat_id.id', '=', rec.categ_id.id)]
-#             )
-
-    # @api.onchange('categ_id')
-    # def price_virtual_room_domain(self):
-        # return {
-        #     'domain': {
-        #         'price_virtual_room': [
-        #             '|', ('room_ids.id', '=', self._origin.id),
-        #                  ('room_type_ids.cat_id.id', '=', self.categ_id.id)
-        #         ]
-        #     }
-        # }
-
-    # @api.multi
-    # def unlink(self):
-    #     for record in self:
-    #         record.product_id.unlink()
-    #     return super(HotelRoom, self).unlink()
