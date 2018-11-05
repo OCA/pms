225c55cdc39df1a00f6a43ca2b726fe3d03bc63d
Pablo <pabloqb@gmail.com>
2018-11-05 09:20:32 +0100

    [WIP] Wizard Node Reservation

--- hotel_node_master/wizards/wizard_hotel_node_reservation.py
+++ hotel_node_master/wizards/wizard_hotel_node_reservation.py
@@ -34,74 +34,46 @@ class HotelNodeReservationWizard(models.TransientModel):
         today = fields.Date.context_today(self.with_context())
         return (fields.Date.from_string(today) + timedelta(days=1)).strftime(DEFAULT_SERVER_DATE_FORMAT)
 
-    @api.model
-    def _default_room_type_wizard_ids(self):
-        node_id = self.env['project.project'].browse(self._context.get('node_id'))
-        checkin = self._default_checkin()
-        checkout = self._default_checkout()
-        room_type_wizard_ids = node_id.room_type_ids.mapped(lambda room_type_id: (0, False, {
-            'room_type_id': room_type_id.id,
-            'room_type_availability': 0,
-            'checkin': checkin,
-            'checkout': checkout,
-        }))
-        return room_type_wizard_ids
-
     node_id = fields.Many2one('project.project', 'Hotel', required=True, default=_default_node_id)
     partner_id = fields.Many2one('res.partner', string="Customer", required=True)
-    checkin = fields.Date('Check In', required=True, default=_default_checkin)
-    checkout = fields.Date('Check Out', required=True, default=_default_checkout)
     room_type_wizard_ids = fields.One2many('node.room.type.wizard', 'node_reservation_wizard_id',
-                                           string="Room Types", default=_default_room_type_wizard_ids)
+                                           string="Room Types")
     price_total = fields.Float(string='Total Price', compute='_compute_price_total', store=True)
 
-    @api.constrains('room_type_wizard_ids.room_qty')
-    def _check_room_type_wizard_ids(self):
-        """
-        :raise: ValidationError
-        """
-        total_qty = 0
-        for rec in self.room_type_wizard_ids:
-            total_qty += rec.room_qty
+    # FIXED @constrains parameter 'room_type_wizard_ids.room_qty' is not a field name
+    @api.constrains('room_type_wizard_ids')
+    def _check_room_type_wizard_total_qty(self):
+        for rec in self:
+            total_qty = 0
+            for rec_room_type in rec.room_type_wizard_ids:
+                total_qty += rec_room_type.room_qty
 
-        if total_qty == 0:
-            msg = _("It is not possible to create the reservation.") + " " + \
-                  _("Maybe you forgot adding the quantity to at least one type of room?.")
-            raise ValidationError(msg)
+            if total_qty == 0:
+                msg = _("It is not possible to create the reservation.") + " " + \
+                      _("Maybe you forgot adding the quantity to at least one type of room?.")
+                raise ValidationError(msg)
 
     @api.depends('room_type_wizard_ids.price_total')
     def _compute_price_total(self):
-        _logger.info('_compute_price_total for wizard %s', self.id)
-        self.price_total = 0.0
-        for rec in self.room_type_wizard_ids:
-            self.price_total += rec.price_total
+        for rec in self:
+            _logger.info('_compute_price_total for wizard %s', rec.id)
+            rec.price_total = 0.0
+            for rec_room_type in rec.room_type_wizard_ids:
+                rec.price_total += rec_room_type.price_total
 
     @api.onchange('node_id')
     def _onchange_node_id(self):
-        self.ensure_one()
         if self.node_id:
             _logger.info('_onchange_node_id(self): %s', self)
             # TODO Save your credentials (session)
-
-    @api.onchange('checkin', 'checkout')
-    def _onchange_dates(self):
-        self.ensure_one()
-        _logger.info('_onchange_dates(self): %s', self)
-
-        # TODO check hotel timezone
-        self.checkin = self._default_checkin() if not self.checkin \
-            else fields.Date.from_string(self.checkin)
-        self.checkout = self._default_checkout() if not self.checkout \
-            else fields.Date.from_string(self.checkout)
-
-        if fields.Date.from_string(self.checkin) >= fields.Date.from_string(self.checkout):
-            self.checkout = (fields.Date.from_string(self.checkin) + timedelta(days=1)).strftime(
-                DEFAULT_SERVER_DATE_FORMAT)
-
-        # update room_type_wizard_ids
-        for rec in self.room_type_wizard_ids:
-            if self.checkin != rec.checkin:
-                _logger.warning('_onchange_dates need new data for room_type: %s', rec.room_type_id)
+            _logger.info('_compute_room_types for node %s', self.node_id)
+            cmds = self.node_id.room_type_ids.mapped(lambda room_type_id: (0, False, {
+                'node_id': self.node_id.id,
+                'room_type_id': room_type_id.id,
+                'checkin': self._default_checkin(),
+                'checkout': self._default_checkout(),
+            }))
+            self.room_type_wizard_ids = cmds
 
     @api.multi
     def create_node_reservation(self):
@@ -119,22 +91,18 @@ class HotelNodeReservationWizard(models.TransientModel):
             room_lines = []
             for rec in self.room_type_wizard_ids:
                 for x in range(rec.room_qty):
-                    # vals_reservation_lines = {
-                    #     'partner_id': remote_partner_id,
-                    #     'room_type_id': rec.room_type_id.remote_room_type_id,
-                    # }
-                    # add discount
-                    # reservation_line_ids = noderpc.env['hotel.reservation'].prepare_reservation_lines(
-                    #     rec.checkin,
-                    #     (fields.Date.from_string(rec.checkout) - fields.Date.from_string(rec.checkin)).days,
-                    #     vals_reservation_lines
-                    # )  # [[5, 0, 0], ¿?
                     wdb.set_trace()
+                    # prepare hotel reservation lines with details by day
+                    reservation_line_cmds = rec.room_type_line_ids.mapped(lambda reservation_line: (0, False, {
+                        'date': reservation_line.date,
+                        'price': reservation_line.price,
+                    }))
+                    # add discount
                     room_lines.append((0, False, {
                         'room_type_id': rec.room_type_id.remote_room_type_id,
                         'checkin': rec.checkin,
                         'checkout': rec.checkout,
-                        # 'reservation_line_ids': reservation_line_ids['reservation_line_ids'],
+                        'reservation_line_ids': reservation_line_cmds,
                     }))
             vals.update({'room_lines': room_lines})
 
@@ -147,7 +115,7 @@ class HotelNodeReservationWizard(models.TransientModel):
 
             noderpc.logout()
 
-            # return self._open_wizard_action_search()
+            # TODO return a wizard and preview the resevation
 
         except (odoorpc.error.RPCError, odoorpc.error.InternalError, urllib.error.URLError) as err:
             raise ValidationError(err)
@@ -169,29 +137,41 @@ class NodeRoomTypeWizard(models.TransientModel):
     _name = "node.room.type.wizard"
     _description = "Node Room Type Wizard"
 
-    node_reservation_wizard_id = fields.Many2one('hotel.node.reservation.wizard')
+    @api.model
+    def _default_node_id(self):
+        return self._context.get('node_id') or None
+
+    @api.model
+    def _default_checkin(self):
+        today = fields.Date.context_today(self.with_context())
+        return fields.Date.from_string(today).strftime(DEFAULT_SERVER_DATE_FORMAT)
+
+    @api.model
+    def _default_checkout(self):
+        today = fields.Date.context_today(self.with_context())
+        return (fields.Date.from_string(today) + timedelta(days=1)).strftime(DEFAULT_SERVER_DATE_FORMAT)
+
+    node_reservation_wizard_id = fields.Many2one('hotel.node.reservation.wizard',
+                                                 ondelete = 'cascade', required = True)
+    node_id = fields.Many2one('project.project', 'Hotel', default=_default_node_id, required=True)
 
     room_type_id = fields.Many2one('hotel.node.room.type', 'Rooms Type')
     room_type_availability = fields.Integer('Availability', compute="_compute_restrictions", readonly=True, store=True)
     room_qty = fields.Integer('Quantity', default=0)
     room_type_line_ids = fields.One2many('node.room.type.line.wizard', 'node_room_type_line_wizard_id',
-                                         compute="_compute_restrictions", string="Room type detail per day.")
+                                         string="Room type detail per day")
 
-    checkin = fields.Date('Check In', required=True)
-    checkout = fields.Date('Check Out', required=True)
-    nights = fields.Integer('Nights', compute="_compute_nights", readonly=True, store=True)
+    checkin = fields.Date('Check In', default=_default_checkin, required=True)
+    checkout = fields.Date('Check Out', default=_default_checkout, required=True)
+    nights = fields.Integer('Nights', compute='_compute_nights', readonly=True)
 
     min_stay = fields.Integer('Min. Days', compute="_compute_restrictions", readonly=True, store=True)
-    # price_unit indicates Room Price x Nights
-    price_unit = fields.Float(string='Room Price', compute="_compute_restrictions", store=True)
+    price_unit = fields.Float(string='Room Price', compute="_compute_restrictions", readonly=True, store=True)
     discount = fields.Float(string='Discount (%)', default=0.0)
     price_total = fields.Float(string='Total Price', compute='_compute_price_total', readonly=True, store=True)
 
     @api.constrains('room_qty')
     def _check_room_qty(self):
-        """
-        :raise: ValidationError
-        """
         total_qty = 0
         for rec in self:
             if (rec.room_type_availability < rec.room_qty) or (rec.room_qty > 0 and rec.nights < rec.min_stay):
@@ -217,38 +197,38 @@ class NodeRoomTypeWizard(models.TransientModel):
         for rec in self:
             if rec.checkin and rec.checkout:
                 try:
-                    node_id = rec.node_reservation_wizard_id.node_id
                     # TODO Load your credentials (session) ... should be faster?
-                    noderpc = odoorpc.ODOO(node_id.odoo_host, node_id.odoo_protocol, node_id.odoo_port)
-                    noderpc.login(node_id.odoo_db, node_id.odoo_user, node_id.odoo_password)
+                    noderpc = odoorpc.ODOO(rec.node_id.odoo_host, rec.node_id.odoo_protocol, rec.node_id.odoo_port)
+                    noderpc.login(rec.node_id.odoo_db, rec.node_id.odoo_user, rec.node_id.odoo_password)
 
+                    _logger.info('_compute_restrictions [availability] for room type %s', rec.room_type_id)
                     rec.room_type_availability = noderpc.env['hotel.room.type'].get_room_type_availability(
                             rec.checkin,
                             rec.checkout,
                             rec.room_type_id.remote_room_type_id)
-                    _logger.warning('_compute_restrictions [availability: %s] for room type %s', rec.room_type_availability, rec.room_type_id)
 
-                    # rec.room_type_line_ids = noderpc.env['hotel.room.type'].get_room_type_price_unit(
-                    #         rec.checkin,
-                    #         rec.checkout,
-                    #         rec.room_type_id.remote_room_type_id)
-                    cmds = []
-                    for x in range(rec.nights):
-                        cmds.append((0, False, {
-                            'node_room_type_line_wizard_id': rec.id,
-                            'date': (fields.Date.from_string(rec.checkin) + timedelta(days=x)).strftime(
-                                DEFAULT_SERVER_DATE_FORMAT),
-                            'price': 0.0,
-                        }))
-                    rec.room_type_line_ids = cmds
+                    _logger.info('_compute_restrictions [price_unit] for room type %s', rec.room_type_id)
+                    rec.room_type_line_ids = noderpc.env['hotel.room.type'].get_room_type_price_unit(
+                            rec.checkin,
+                            rec.checkout,
+                            rec.room_type_id.remote_room_type_id)
+                    # cmds = []
+                    # for x in range(rec.nights):
+                    #     cmds.append((0, False, {
+                    #         'date': (fields.Date.from_string(rec.checkin) + timedelta(days=x)).strftime(
+                    #             DEFAULT_SERVER_DATE_FORMAT),
+                    #         'price': 25.0,
+                    #     }))
+                    # from pprint import pprint
+                    # pprint(cmds)
+                    # rec.room_type_line_ids = cmds
                     rec.price_unit = sum(rec.room_type_line_ids.mapped('price'))
-                    _logger.warning('_compute_restrictions [price_unit: %s] for room type %s', rec.price_unit, rec.room_type_id)
 
+                    _logger.info('_compute_restrictions [min days] for room type %s', rec.room_type_id)
                     rec.min_stay = noderpc.env['hotel.room.type'].get_room_type_restrictions(
                         rec.checkin,
                         rec.checkout,
                         rec.room_type_id.remote_room_type_id)
-                    _logger.warning('_compute_restrictions [min days: %s] for room type %s', rec.min_stay, rec.room_type_id)
 
                     noderpc.logout()
                 except (odoorpc.error.RPCError, odoorpc.error.InternalError, urllib.error.URLError) as err:
@@ -267,12 +247,11 @@ class NodeRoomTypeWizard(models.TransientModel):
 
     @api.onchange('checkin', 'checkout')
     def _onchange_dates(self):
-        _logger.info('+++ _onchange_dates for room type %s +++', self.room_type_id)
-
-        self.checkin = self._default_checkin() \
-            if not self.checkin else fields.Date.from_string(self.checkin)
-        self.checkout = self._default_checkout() \
-            if not self.checkout else fields.Date.from_string(self.checkout)
+        _logger.info('_onchange_dates for room type: %s', self.room_type_id)
+        if not self.checkin:
+            self.checkin = self._default_checkin()
+        if not self.checkout:
+            self.checkout = self._default_checkout()
 
         if fields.Date.from_string(self.checkin) >= fields.Date.from_string(self.checkout):
             self.checkout = (fields.Date.from_string(self.checkin) + timedelta(days=1)).strftime(
@@ -298,6 +277,7 @@ class NodeSearchWizard(models.TransientModel):
         return self._context.get('node_id') or None
 
     node_id = fields.Many2one('project.project', 'Hotel', default=_default_node_id)
+    node_folio_wizard_id = fields.Many2one('node.folio.wizard')
     folio = fields.Char('Folio Number')
     partner_id = fields.Many2one('res.partner', string="Customer")
     email = fields.Char('E-mail', related='partner_id.email')
