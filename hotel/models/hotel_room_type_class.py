6740a6de634aa20c00e805b04bfc1d648fadca1b
Dario Lodeiros <dario@aloxa.eu>
2018-11-07 12:46:02 +0100

    [WIP] Refactoring Code - HotelRoomTypeClass

--- hotel/models/hotel_room_type_class.py
+++ hotel/models/hotel_room_type_class.py
@@ -1,7 +1,7 @@
 # Copyright 2017  Alexandre Díaz
 # Copyright 2017  Dario Lodeiros
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models, fields, api
+from odoo import models, fields
 
 class HotelRoomTypeClass(models.Model):
     """ Before creating a 'room type_class', you need to consider the following:
@@ -11,9 +11,7 @@ class HotelRoomTypeClass(models.Model):
     """
     _name = "hotel.room.type.class"
     _description = "Room Type Class"
-    _order = "sequence, code_class, name"
-    _sql_constraints = [('code_class_unique', 'unique(code_class)',
-                         'code must be unique!')]
+    _order = "sequence, name, code_class"
 
     name = fields.Char('Class Name', required=True, translate=True)
     room_type_ids = fields.One2many('hotel.room.type', 'class_id', 'Types')
@@ -22,3 +20,6 @@ class HotelRoomTypeClass(models.Model):
                             category without removing it.")
     sequence = fields.Integer('Sequence', default=0)
     code_class = fields.Char('Code')
+
+    _sql_constraints = [('code_class_unique', 'unique(code_class)',
+                         'code must be unique!')]
