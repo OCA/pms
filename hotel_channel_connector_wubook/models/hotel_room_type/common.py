36e65aef48720bbaa647447ae4957f6f563c2129
Pablo <pabloqb@gmail.com>
2018-12-07 09:21:25 +0100

    [ADD] Missing new arguments for board and room type class

--- hotel_channel_connector_wubook/models/hotel_room_type/common.py
+++ hotel_channel_connector_wubook/models/hotel_room_type/common.py
@@ -9,16 +9,16 @@ class HotelRoomTypeAdapter(Component):
     _inherit = 'wubook.adapter'
     _apply_on = 'channel.hotel.room.type'
 
-    def create_room(self, shortcode, name, capacity, price, availability):
+    def create_room(self, shortcode, name, capacity, price, availability, defboard, rtype):
         return super(HotelRoomTypeAdapter, self).create_room(
-            shortcode, name, capacity, price, availability)
+            shortcode, name, capacity, price, availability, defboard, rtype)
 
     def fetch_rooms(self):
         return super(HotelRoomTypeAdapter, self).fetch_rooms()
 
-    def modify_room(self, channel_room_id, name, capacity, price, availability, scode):
+    def modify_room(self, channel_room_id, name, capacity, price, availability, scode, defboard, rtype):
         return super(HotelRoomTypeAdapter, self).modify_room(
-            channel_room_id, name, capacity, price, availability, scode)
+            channel_room_id, name, capacity, price, availability, scode, defboard, rtype)
 
     def delete_room(self, channel_room_id):
         return super(HotelRoomTypeAdapter, self).delete_room(channel_room_id)
