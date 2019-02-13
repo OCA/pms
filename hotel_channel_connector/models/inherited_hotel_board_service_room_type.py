2c36a6dda2e979583cd34249743b1e20af14d16e
Pablo <pabloqb@gmail.com>
2019-02-13 16:28:22 +0100

    [ADD] sql constrain for channel board service and room type

--- hotel_channel_connector/models/inherited_hotel_board_service_room_type.py
+++ hotel_channel_connector/models/inherited_hotel_board_service_room_type.py
@@ -9,3 +9,8 @@ class HotelBoardServiceRoomType(models.Model):
     _inherit = 'hotel.board.service.room.type'
 
     channel_service = fields.Selection([], string='Channel Board Service')
+
+    _sql_constraints = [
+        ('room_type_channel_service_id_uniq', 'unique(hotel_room_type_id, channel_service)',
+         'The channel board service must be unique for room type.'),
+    ]
