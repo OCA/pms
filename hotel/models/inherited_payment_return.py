7de29f4d387d8a893f5117de830412eafb8a4eb5
Dario Lodeiros <dario@aloxa.eu>
2019-03-12 18:02:40 +0100

    [WIP] Views

--- hotel/models/inherited_payment_return.py
+++ hotel/models/inherited_payment_return.py
@@ -10,7 +10,7 @@ class PaymentReturn(models.Model):
 
     @api.multi
     def action_confirm(self):
-        pay = super(PaymentReturn,self).action_confirm()
+        pay = super(PaymentReturn, self).action_confirm()
         if pay:
             folio_ids = []
             folios = self.env['hotel.folio'].browse(folio_ids)
@@ -20,6 +20,8 @@ class PaymentReturn(models.Model):
                 ])
                 folios_line = self.env['hotel.folio'].browse(payments.mapped('folio_id.id'))
                 for folio in folios_line:
+                    if self.id not in folio.return_ids.ids:
+                        folio.update({'return_ids': [(4, self.id)]})
                     msg = _("Return of %s registered") % (line.amount)
                     folio.message_post(subject=_('Payment Return'), body=msg)
                 folios += folios_line
