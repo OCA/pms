4c77e50653fb217c61a4c30ef138af28d3656546
Dario Lodeiros <dario@aloxa.eu>
2019-08-30 20:07:59 +0200

    [FIX] inherit duplicate_reservation

--- hotel_channel_connector/wizard/__init__.py
+++ hotel_channel_connector/wizard/__init__.py
@@ -1,5 +1,4 @@
 # Copyright 2018 Alexandre Díaz <dev@redneboa.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from . import inherited_massive_changes
-from . import inherited_duplicate_reservation
 from . import inherited_massive_price_reservation_days
