91682df386f92d0caba11b172313617f7df65219
Pablo <pabloqb@gmail.com>
2019-09-24 19:09:06 +0200

    [UPD] updating README files

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
-# HOOTEL PROJECT MODULES [![Build Status](https://travis-ci.org/hootel/hootel.svg?branch=11.0)](https://travis-ci.org/hootel/hootel) [![codecov](https://codecov.io/gh/hootel/hootel/branch/11.0/graph/badge.svg)](https://codecov.io/gh/hootel/hootel) ![Unstable](https://img.shields.io/badge/stability-unstable-yellow.svg)
+[![Build Status](https://travis-ci.org/hootel/hootel.svg?branch=11.0)](https://travis-ci.org/hootel/hootel) [![codecov](https://codecov.io/gh/hootel/hootel/branch/11.0/graph/badge.svg)](https://codecov.io/gh/hootel/hootel) ![Unstable](https://img.shields.io/badge/stability-unstable-yellow.svg)
 
+# ROOMDOO
+All-in-One Property Management System (PMS) focused on medium-sized hotels for managing every aspect of your property's daily operations.
 
-**IMPORTANT:**
-  - Set time zone of users that use the calendar
+You will find modules for:
 
-**MODULES:**
-  - [ ] hotel: Base module (Manage Rooms, Reservations, Services, Customers, Mailing, Invoicing, ...)
-  - [ ] hotel_calendar: Adds calendar for manage hotel reservations and rooms configuration
-  - [ ] hotel_calendar_channel_connector: Unify 'hotel_channel_connector' and 'hotel_calendar' modules
-  - [ ] hotel_channel_connector: Base Channel Connector (Using Odoo Connector)
-  - [ ] hotel_channel_connector_wubook: Wubook API Implementation
-  - [ ] hotel_node_helper: Configure a node as a helper to serve and get information from a master one
-  - [ ] hotel_node_master: Configure a node as a master
+ - Manage hotel properties with multi-hotel and multi-company support.
+ - Manage your rooms inventory, reservations, check-in, daily reports, board services, rate and restriction plans.
+ - Handle reservations in a user friendly calendar management view.
+ - Connect your hotel with a channel manager for booking though Online Travel Agencies, such as, Booking.com, Expedia, Hotelbeds, ...
+ - Revenue management tools for daily pricing and availability strategies.
 
-**HOW WORKS?**
-  - The idea is... the hotel sell 'rooms types' and the customer is assigned to one 'real room'.
-  - The folio have all reservation lines, used services, etc..
+Installation
+------------
+To install this module, you need to:
+
+ 
+Configuration
+-------------
+To configure this module, you need to:
+
+Usage
+-----
+To use this module, you need to:
+
+Contributing
+------------
+Do you want to contribute? Please, do not hesitate to contact any of the current contributors :)
--- hotel/__manifest__.py
+++ hotel/__manifest__.py
@@ -1,17 +1,20 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Alexandre Díaz
+# Copyright 2019 Darío Lodeiros, Alexandre Díaz, Jose Luis Algara, Pablo Quesada
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Hotel Management',
+    'name': 'roomdoo',
+    'summary': "A property management system focused on medium-sized hotels",
     'version': '11.0.2.0.0',
-    'author': 'Odoo Community Association (OCA),\
-    Darío Lodeiros,\
-    Jose Luis Algara,\
-    Alexandre Díaz,\
-    Pablo Quesada,',
+    'development_status': 'Beta',
     'category': 'Generic Modules/Hotel Management',
     'website': 'https://github.com/hootel/hootel',
+    'author': 'Darío Lodeiros, '
+              'Alexandre Díaz, '
+              'Jose Luis Algara, '
+              'Pablo Quesada ',
+    'license': "AGPL-3",
+    'application': False,
+    'installable': True,
     'depends': [
         'base',
         'sale_stock',
@@ -19,56 +22,57 @@
         'partner_firstname',
         'account_cancel'
     ],
-    'license': "AGPL-3",
-    'demo': ['demo/hotel_demo.xml'],
     'data': [
-        'data/hotel_data.xml',
-        'security/hotel_security.xml',
-        'security/ir.model.access.csv',
-        'wizard/massive_changes.xml',
-        'wizard/split_reservation.xml',
-        'wizard/service_on_day.xml',
-        'data/menus.xml',
-        'views/inherited_account_payment_views.xml',
-        'views/inherited_account_invoice_views.xml',
-        'wizard/massive_price_reservation_days.xml',
-        'wizard/folio_make_invoice_advance_views.xml',
-        'data/hotel_sequence.xml',
-        'views/inherited_res_users_views.xml',
-        'views/hotel_property_views.xml',
-        'views/hotel_floor_views.xml',
-        'views/hotel_folio_views.xml',
-        'views/inherited_res_partner_views.xml',
-        'views/hotel_room_type_views.xml',
-        'views/hotel_room_views.xml',
-        'views/hotel_shared_room_views.xml',
-        'views/hotel_room_type_class_views.xml',
-        'views/general.xml',
-        'views/inherited_product_template_views.xml',
-        'views/inherited_product_pricelist_views.xml',
-        'views/hotel_room_amenities_type_views.xml',
-        'views/hotel_room_amenities_views.xml',
-        'views/hotel_room_type_restriction_views.xml',
-        'views/hotel_room_type_restriction_item_views.xml',
-        'views/hotel_reservation_views.xml',
-        'views/hotel_room_closure_reason_views.xml',
-        'views/hotel_service_views.xml',
-        'views/hotel_service_line_views.xml',
-        'views/hotel_board_service_views.xml',
-        'views/hotel_checkin_partner_views.xml',
-        'views/hotel_board_service_room_type_views.xml',
-        'views/hotel_cancelation_rule_views.xml',
-        'views/inherited_webclient_templates.xml',
         'data/cron_jobs.xml',
         'data/email_template_cancel.xml',
         'data/email_template_reserv.xml',
         'data/email_template_exit.xml',
-        'wizard/wizard_reservation.xml',
-        'report/hotel_folio_templates.xml',
+        'data/menus.xml',
+        'data/hotel_data.xml',
+        'data/hotel_sequence.xml',
         'report/hotel_folio.xml'
+        'report/hotel_folio_templates.xml',
+        'security/hotel_security.xml',
+        'security/ir.model.access.csv',
+
+        'views/general.xml',
+        'views/hotel_amenity_views.xml',
+        'views/hotel_amenity_type_views.xml',
+        'views/hotel_board_service_views.xml',
+        'views/hotel_board_service_room_type_views.xml',
+        'views/hotel_cancelation_rule_views.xml',
+        'views/hotel_checkin_partner_views.xml',
+        'views/hotel_floor_views.xml',
+        'views/hotel_folio_views.xml',
+        'views/hotel_property_views.xml',
+        'views/hotel_reservation_views.xml',
+        'views/hotel_room_views.xml',
+        'views/hotel_room_closure_reason_views.xml',
+        'views/inherited_account_payment_views.xml',
+        'views/inherited_account_invoice_views.xml',
+        'views/inherited_res_users_views.xml',
+        'views/hotel_room_type_views.xml',
+        'views/hotel_room_type_class_views.xml',
+        'views/hotel_room_type_restriction_views.xml',
+        'views/hotel_room_type_restriction_item_views.xml',
+        'views/hotel_service_views.xml',
+        'views/hotel_service_line_views.xml',
+        'views/hotel_shared_room_views.xml',
+        'views/inherited_res_partner_views.xml',
+        'views/inherited_product_pricelist_views.xml',
+        'views/inherited_product_template_views.xml',
+        'views/inherited_webclient_templates.xml',
+        'wizard/folio_make_invoice_advance_views.xml',
+        'wizard/massive_changes.xml',
+        'wizard/massive_price_reservation_days.xml',
+        'wizard/service_on_day.xml',
+        'wizard/split_reservation.xml',
+        'wizard/wizard_reservation.xml',
+    ],
+    'demo': [
+        'demo/hotel_demo.xml'
     ],
     'qweb': [
         'static/src/xml/*.xml',
     ],
-    'installable': True
 }
