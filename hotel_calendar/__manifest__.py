22e3ebd09fd0741ab67c665f9dd10f7536d0dd96
Pablo <pabloqb@gmail.com>
2019-09-26 19:02:33 +0200

    [UPD] update manifest data files ordering

--- hotel_calendar/__manifest__.py
+++ hotel_calendar/__manifest__.py
@@ -2,13 +2,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Hotel Calendar',
+    'name': 'Roomdoo Calendar',
+    'summary': 'A calendar view for user friendly handling your roomdoo property.',
     'version': '11.0.2.0',
-    'author': "Alexandre Díaz <dev@redneboa.es>",
-    'website': 'https://www.eiqui.com',
-    'category': 'hotel/calendar',
-    'summary': "Hotel Calendar",
-    'description': "Hotel Calendar",
+    'development_status': 'Beta',
+    'category': 'Generic Modules/Hotel Management',
+    'website': 'https://github.com/hootel/hootel',
+    'author': 'Alexandre Díaz <dev@redneboa.es>',
+    'license': "AGPL-3",
+    'application': False,
+    'installable': True,
     'depends': [
         'bus',
         'web',
@@ -16,9 +19,9 @@
         'hotel',
         'web_widget_color',
     ],
-    'external_dependencies': {
-        'python': []
-    },
+    # 'external_dependencies': {
+    #     'python': []
+    # },
     'data': [
         'views/general.xml',
         'views/actions.xml',
@@ -32,11 +35,8 @@
         'security/ir.model.access.csv',
     ],
     'qweb': [
-        'static/src/xml/*.xml',
+        'static/src/xml/hotel_calendar_management_view.xml',
+        'static/src/xml/hotel_calendar_templates.xml',
+        'static/src/xml/hotel_calendar_view.xml',
     ],
-    'test': [
-    ],
-
-    'installable': True,
-    'license': 'AGPL-3',
 }
