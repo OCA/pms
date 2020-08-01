80c43faa252e830a01ca49ee6659e3515933f902
Darío Lodeiros <dario@aloxa.eu>
2020-08-01 22:36:39 +0200

    [IMP] Precommit

--- pms/models/pms_room_closure_reason.py
+++ pms/models/pms_room_closure_reason.py
@@ -1,6 +1,6 @@
 # Copyright 2017  Dario Lodeiros
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class RoomClosureReason(models.Model):
@@ -8,10 +8,8 @@ class RoomClosureReason(models.Model):
     _description = "Cause of out of service"
 
     # Fields declaration
-    name = fields.Char('Name', translate=True, required=True)
+    name = fields.Char("Name", translate=True, required=True)
     pms_property_ids = fields.Many2many(
-        'pms.property',
-        string='Properties',
-        required=False,
-        ondelete='restrict')
-    description = fields.Text('Description', translate=True)
+        "pms.property", string="Properties", required=False, ondelete="restrict"
+    )
+    description = fields.Text("Description", translate=True)
