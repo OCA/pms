9213e0c2d4df6334932b1e4ddcb740241fdfb4b7
Pablo Quesada Barriuso <pablo.quesada@usc.es>
2020-08-05 17:20:52 +0200

    [MIG] V13 properties menu

--- pms/models/inherited_ir_http.py
+++ pms/models/inherited_ir_http.py
@@ -24,13 +24,13 @@ class IrHttp(models.AbstractModel):
         })
         # TODO: This user context update should be placed in other function ¿? 
         res['user_context'].update({'allowed_pms_property_ids': [(property.id) for property in user.pms_property_ids]})
-        # update current_company based on current_pms_property
-        if user.pms_property_id.company_id in user.company_ids:
-            user.company_id = user.pms_property_id.company_id
-            res['company_id'] = user.pms_property_id.company_id.id
-        else:
-            raise MissingError(
-                _("Wrong property and company access settings for this user. "
-                  "Please review property and company for user %s") % user.name)
+        # TODO: update current_company based on current_pms_property
+        # if user.pms_property_id.company_id in user.company_ids:
+        #     user.company_id = user.pms_property_id.company_id
+        #     res['company_id'] = user.pms_property_id.company_id.id
+        # else:
+        #     raise MissingError(
+        #         _("Wrong property and company access settings for this user. "
+        #           "Please review property and company for user %s") % user.name)
 
         return res
