831b1e8d61144ca0293e3af1a6c540d530a531e4
Darío Lodeiros <dario@aloxa.eu>
2020-10-03 08:42:08 +0200

    [IMP] Guidelines file names

--- pms/models/__init__.py
+++ pms/models/__init__.py
@@ -2,11 +2,11 @@
 # Copyright 2018  Dario Lodeiros
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import inherited_ir_http
-from . import inherited_payment_return
+from . import ir_http
+from . import payment_return
 from . import pms_board_service_room_type
 from . import pms_property
-from . import inherited_res_users
+from . import res_users
 from . import pms_floor
 from . import pms_folio
 from . import pms_reservation
@@ -16,22 +16,22 @@ from . import pms_amenity
 from . import pms_amenity_type
 from . import pms_room_type
 from . import pms_service
-from . import inherited_account_move
-from . import inherited_product_template
-from . import inherited_res_company
-from . import inherited_account_payment
+from . import account_move
+from . import product_template
+from . import res_company
+from . import account_payment
 from . import pms_room_type_restriction
 from . import pms_room_type_restriction_item
 from . import pms_reservation_line
 from . import pms_checkin_partner
-from . import inherited_product_pricelist
-from . import inherited_res_partner
-from . import inherited_mail_compose_message
+from . import product_pricelist
+from . import res_partner
+from . import mail_compose_message
 from . import pms_room_type_class
 from . import pms_room_closure_reason
 from . import pms_service_line
 from . import pms_board_service
 from . import pms_board_service_room_type_line
 from . import pms_board_service_line
-from . import inherited_account_move_line
+from . import account_move_line
 from . import pms_cancelation_rule
