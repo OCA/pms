7f47edbcdf58cefcbfc497e5fcb1b7006cbfb9d3
Sara Lago <saralago126@gmail.com>
2020-11-16 11:08:30 +0100

    [IMP] add test to check sales channel domain

--- pms/models/res_partner.py
+++ pms/models/res_partner.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import logging
 
-from odoo import _, api, fields, models
+from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
 
@@ -24,38 +24,23 @@ class ResPartner(models.Model):
     sale_channel_id = fields.Many2one(
         "pms.sale.channel",
         string="Sale Channel",
-        ondelete="restrict",
-        domain=[("channel_type", "=", "indirect")],
+        ondelete = "restrict",
+        domain=[("channel_type","=","indirect")],
     )
-    default_commission = fields.Integer("Commission")
-    apply_pricelist = fields.Boolean("Apply Pricelist")
-    invoice_agency = fields.Boolean("Invoice Agency")
 
     # Compute and Search methods
     def _compute_reservations_count(self):
         pms_reservation_obj = self.env["pms.reservation"]
         for record in self:
             record.reservations_count = pms_reservation_obj.search_count(
-                [
-                    (
-                        "partner_id.id",
-                        "=",
-                        record.id if isinstance(record.id, int) else False,
-                    )
-                ]
+                [("partner_id.id", "=", record.id)]
             )
 
     def _compute_folios_count(self):
         pms_folio_obj = self.env["pms.folio"]
         for record in self:
             record.folios_count = pms_folio_obj.search_count(
-                [
-                    (
-                        "partner_id.id",
-                        "=",
-                        record.id if isinstance(record.id, int) else False,
-                    )
-                ]
+                [("partner_id.id", "=", record.id)]
             )
 
     # ORM Overrides
@@ -86,10 +71,10 @@ class ResPartner(models.Model):
             )
         return res
 
-    @api.constrains("is_agency", "sale_channel_id")
+    @api.constrains("is_agency","sale_channel_id")
     def _check_is_agency(self):
         for record in self:
             if record.is_agency and not record.sale_channel_id:
-                raise models.ValidationError(_("Sale Channel must be entered"))
+                 raise models.ValidationError(_("Sale Channel must be entered"))
             if not record.is_agency and record.sale_channel_id:
-                record.sale_channel_id = None
+                record.sale_channel_id=None
