c5642d46f340449eea2d1eb1e0f56861257e72fc
Brais Abeijón <>
2020-11-27 15:36:21 +0100

    [ADD] pms: Test case for product.pricelist rule priority

--- pms/tests/__init__.py
+++ pms/tests/__init__.py
@@ -21,4 +21,4 @@
 ##############################################################################
 from . import test_pms_reservation
 from . import test_pms_pricelist
-from . import test_pms_checkin_partner
+from . import test_pms_pricelist_priority
