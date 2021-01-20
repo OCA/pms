94dc9bc71638e4bfea3ab54b66919ec541acfdc4
Darío Lodeiros <dario@commitsun.com>
2021-01-20 11:41:31 +0100

    14.0 pms account flow (#36)
    * [WIP] Basic tests definition
    
    * [DEL] Default diff invoicing
    
    * [WIP] Reservation refact invoice fields
    
    * [FIX] test price without taxes
    
    * [FIX] Security csv merge
    
    * [WIP]pms: Wizard adv inv views
    
    * [ADD] Wizard Filter Invoice Days
    
    * [WIP] Payment WorkFlow

--- pms/models/account_payment.py
+++ pms/models/account_payment.py
@@ -1,7 +1,6 @@
 # Copyright 2017  Dario Lodeiros
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import _, api, fields, models
-from odoo.exceptions import except_orm
+from odoo import _, fields, models
 
 
 class AccountPayment(models.Model):
@@ -9,78 +8,6 @@ class AccountPayment(models.Model):
 
     # Fields declaration
     folio_id = fields.Many2one("pms.folio", string="Folio Reference")
-    amount_total_folio = fields.Float(
-        compute="_compute_folio_amount",
-        store=True,
-        string="Total amount in folio",
-    )
-    save_amount = fields.Monetary(string="onchange_amount")
-    save_date = fields.Date()
-    save_journal_id = fields.Integer()
-
-    # Compute and Search methods
-
-    @api.depends("state")
-    def _compute_folio_amount(self):
-        # FIXME: Finalize method
-        res = []
-        fol = ()
-        for payment in self:
-            if payment.folio_id:
-                fol = payment.env["pms.folio"].search(
-                    [("id", "=", payment.folio_id.id)]
-                )
-            else:
-                return
-            if not any(fol):
-                return
-            if len(fol) > 1:
-                raise except_orm(
-                    _("Warning"),
-                    _(
-                        "This pay is related with \
-                                                more than one Reservation."
-                    ),
-                )
-            else:
-                fol.compute_amount()
-            return res
-
-    # Constraints and onchanges
-    # @api.onchange("amount", "payment_date", "journal_id")
-    # def onchange_amount(self):
-    #     if self._origin:
-    #         self.save_amount = self._origin.amount
-    #         self.save_journal_id = self._origin.journal_id.id
-    #         self.save_date = self._origin.payment_date
-
-    # Action methods
-    # def return_payment_folio(self):
-    #     journal = self.journal_id
-    #     partner = self.partner_id
-    #     amount = self.amount
-    #     reference = self.communication
-    #     account_move_lines = self.move_line_ids.filtered(
-    #         lambda x: (x.account_id.internal_type == "receivable")
-    #     )
-    #     return_line_vals = {
-    #         "move_line_ids": [(6, False, [x.id for x in account_move_lines])],
-    #         "partner_id": partner.id,
-    #         "amount": amount,
-    #         "reference": reference,
-    #     }
-    #     return_vals = {
-    #         "journal_id": journal.id,
-    #         "line_ids": [(0, 0, return_line_vals)],
-    #     }
-    #     return_pay = self.env["payment.return"].create(return_vals)
-    #     if self.save_amount:
-    #         self.amount = self.save_amount
-    #     if self.save_date:
-    #         self.payment_date = self.save_date
-    #     if self.save_journal_id:
-    #         self.journal_id = self.env["account.journal"].browse(self.save_journal_id)
-    #     return_pay.action_confirm()
 
     # Business methods
 
--- pms/wizards/__init__.py
+++ pms/wizards/__init__.py
@@ -3,3 +3,6 @@ from . import wizard_massive_changes
 from . import wizard_advanced_filters
 from . import wizard_folio
 from . import wizard_folio_availability
+from . import folio_make_invoice_advance
+from . import wizard_invoice_filter_days
+from . import wizard_payment_folio
