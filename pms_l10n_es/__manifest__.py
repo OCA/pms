945a455b9f02d503c61c13529f9d40bfafa15fc7
Darío Lodeiros <dario@commitsun.com>
2021-01-24 08:19:24 +0100

    [IMP] pms_l10n_es: Guidelines module name

--- pms_l10n_es/__manifest__.py
+++ pms_l10n_es/__manifest__.py
@@ -1,10 +1,8 @@
-# Copyright 2013 Nicolas Bessi (Camptocamp SA)
-# Copyright 2014 Agile Business Group (<http://www.agilebg.com>)
-# Copyright 2015 Grupo ESOC (<http://www.grupoesoc.es>)
+# Copyright 2020 CommitSun (<http://www.commitsun.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Adaptation to spanish law",
+    "name": "PMS Spanish Adaptation",
     "version": "14.0.1.0.0",
     "author": "CommitSun, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
