085bf3fee5471ee1b531fbacec16568dd76fe8eb
braisab <braisterbutalino@gmail.com>
2021-03-03 18:11:33 +0100

    [WIP]pms: Folio user portal

--- pms/__manifest__.py
+++ pms/__manifest__.py
@@ -67,6 +67,7 @@
         "views/webclient_templates.xml",
         "views/ir_sequence_views.xml",
         "views/account_journal_views.xml",
+        "views/folio_portal_templates.xml",
         "wizards/wizard_reservation.xml",
         "wizards/wizard_massive_changes.xml",
         "wizards/wizard_advanced_filters.xml",
