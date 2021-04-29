f3d760b064e8aa6b77034f489930def46c6c6634
miguelpadin <miguel.padin.dominguez@gmail.com>
2021-04-29 17:56:35 +0200

    [FIX] fix pre-commit

--- pms_l10n_es/models/pms_property.py
+++ pms_l10n_es/models/pms_property.py
@@ -1,18 +1,19 @@
-from odoo import _, fields, models
+from odoo import fields, models
 
 
 class PmsProperty(models.Model):
     _inherit = "pms.property"
 
     police_type = fields.Selection(
-                                    [
-                                        ("guardia_civil", "Guardia Civil"),
-                                        ("policia_nacional","Policía Nacional"),
-                                        ("ertxaintxa", "Ertxaintxa"),
-                                        ("mossos", "Mossos_d'esquadra")
-                                    ],
-                                    string="Police Type",
-                                    default="guardia_civil")
+        [
+            ("guardia_civil", "Guardia Civil"),
+            ("policia_nacional", "Policía Nacional"),
+            ("ertxaintxa", "Ertxaintxa"),
+            ("mossos", "Mossos_d'esquadra"),
+        ],
+        string="Police Type",
+        default="guardia_civil",
+    )
     police_number = fields.Char("Police Number", size=10)
     police_user = fields.Char("Police User")
     police_pass = fields.Char("Police Password")
