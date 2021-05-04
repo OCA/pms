4cd08482b709168d2284d93675e50b22b2714443
miguelpadin <miguel.padin.dominguez@gmail.com>
2021-05-04 09:19:33 +0200

    [WIP] send file via GC api

--- pms_l10n_es/wizards/traveller_report.py
+++ pms_l10n_es/wizards/traveller_report.py
@@ -1,6 +1,7 @@
 import base64
 import datetime
 from datetime import date
+from odoo.modules.module import get_module_resource
 
 import requests
 
@@ -87,11 +88,79 @@ class TravellerReport(models.TransientModel):
         )
         user = pms_property.police_user
         password = pms_property.police_pass
-        if user and password:
-            data = [self.txt_binary]
-            requests.post(
-                "https://usuario:password@hospederias.guardiacivil.es/"
-                "hospederias/servlet/ControlRecepcionFichero",
-                auth=(user, password),
-                data=data,
-            )
+        headers = {'Content-Type': 'multipart/form-data;'}
+
+        date_start = str(date.today()) + " 0:00:00"
+        date_end = str(date.today()) + " 23:59:59"
+        lines = self.env["pms.checkin.partner"].search(
+            [
+                ("state", "=", "onboard"),
+                ("arrival", ">=", date_start),
+                ("arrival", "<=", date_end),
+            ]
+        )
+        content = (
+            "1|"
+            + pms_property.police_number.upper()
+            + "|"
+            + pms_property.name.upper()
+        )
+        content += "|"
+        content += datetime.datetime.now().strftime("%Y%m%d|%H%M")
+        content += "|" + str(len(lines))
+        for line in lines:
+            if line.document_type == "D":
+                content += "\n2|" + line.document_number.upper() + "||"
+            else:
+                content += "\n2||" + line.document_number.upper() + "|"
+            content += line.document_type + "|"
+            content += line.document_expedition_date.strftime("%Y%m%d") + "|"
+            content += line.lastname.upper() + "|"
+            if line.lastname2:
+                content += line.lastname2.upper()
+            content += "|" + line.firstname.upper() + "|"
+            if line.gender == "female":
+                content += "F|"
+            else:
+                content += "M|"
+            content += line.birthdate_date.strftime("%Y%m%d") + "|"
+            content += line.nationality_id.name.upper() + "|"
+            content += line.arrival.strftime("%Y%m%d") + "|"
+
+        sequence_num = self.env["ir.sequence"].next_by_code(
+            "traveller.report.wizard"
+        )
+        txt_binary = self.env["traveller.report.wizard"].create(
+            {
+                "txt_filename": pms_property.police_number + "." + sequence_num,
+                "txt_binary": base64.b64encode(str.encode(content)),
+                "txt_message": content,
+            }
+        )
+        # print(txt_binary.content)
+
+        import io
+        f = io.StringIO(
+            content
+        )
+
+        # file_object = get_module_resource('pms_l10n_es', 'static', 'cert.pem')
+        session = requests.Session()
+        # a = requests.Request(
+        #     'POST',
+        #     url="https://28000JAM22:ESB01642958@hospederias.guardiacivil.es/hospederias/servlet/ControlRecepcionFichero",
+        #     # data=f,
+        #     files={'file': ('28000JAM22.169', f, 'application/octet-stream')},
+        #     # verify=get_module_resource('pms_l10n_es', 'static', 'cert.pem'),
+        #     # headers={'Content-Type': "multipart/form-data"}
+        # )
+        # # pre = a.prepare()
+        # fff = 0
+        #
+        a = session.post(
+                url="https://28000JAM22:ESB01642958@hospederias.guardiacivil.es/hospederias/servlet/ControlRecepcionFichero",
+                files={'file': ('28000JAM22.169', f, 'application/octet-stream')},
+                verify=get_module_resource('pms_l10n_es', 'static', 'cert.pem'),
+                # headers={'Content-Type': "multipart/form-data"}
+        )
+        print(a)
