ca6997feb92f55983b4b4ff8c234cff50f6815c6
braisab <38132068+braisab@users.noreply.github.com>
2021-05-18 10:51:16 +0200

    [FIX]Enabled advanced pricelist when pms is installed (#92)

--- pms/init_hook.py
+++ pms/init_hook.py
@@ -2,9 +2,15 @@ from odoo import SUPERUSER_ID
 from odoo.api import Environment
 
 
-def post_init_hook(cr, _):
+def pre_init_hook(cr):
     with Environment.manage():
         env = Environment(cr, SUPERUSER_ID, {})
+        ResConfig = env["res.config.settings"]
+        default_values = ResConfig.default_get(list(ResConfig.fields_get()))
+        default_values.update(
+            {"group_product_pricelist": True, "group_sale_pricelist": True}
+        )
+        ResConfig.sudo().create(default_values).execute()
         env["ir.config_parameter"].sudo().set_param(
             "product.product_pricelist_setting", "advanced"
         )
