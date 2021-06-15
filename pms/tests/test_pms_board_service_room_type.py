670391a4ed1c15be7da5748229a8367d2b6b34fb
miguelpadin <miguel.padin.dominguez@gmail.com>
2021-06-15 02:34:49 +0200

    [REF] pms: refactor room type tests

--- pms/tests/test_pms_board_service_room_type.py
+++ pms/tests/test_pms_board_service_room_type.py
@@ -2,66 +2,6 @@ from .common import TestPms
 
 
 class TestPmsBoardServiceRoomType(TestPms):
-
-    # def _create_common_scenario(self):
-    #     self.company1 = self.env["res.company"].create(
-    #         {
-    #             "name": "Pms_Company_Test",
-    #         }
-    #     )
-    #     self.folio_sequence = self.env["ir.sequence"].create(
-    #         {
-    #             "name": "PMS Folio",
-    #             "code": "pms.folio",
-    #             "padding": 4,
-    #             "company_id": self.company1.id,
-    #         }
-    #     )
-    #     self.reservation_sequence = self.env["ir.sequence"].create(
-    #         {
-    #             "name": "PMS Reservation",
-    #             "code": "pms.reservation",
-    #             "padding": 4,
-    #             "company_id": self.company1.id,
-    #         }
-    #     )
-    #     self.checkin_sequence = self.env["ir.sequence"].create(
-    #         {
-    #             "name": "PMS Checkin",
-    #             "code": "pms.checkin.partner",
-    #             "padding": 4,
-    #             "company_id": self.company1.id,
-    #         }
-    #     )
-    #     self.property1 = self.env["pms.property"].create(
-    #         {
-    #             "name": "Pms_property_test1",
-    #             "company_id": self.company1.id,
-    #             "default_pricelist_id": self.env.ref("product.list0").id,
-    #             "folio_sequence_id": self.folio_sequence.id,
-    #             "reservation_sequence_id": self.reservation_sequence.id,
-    #             "checkin_sequence_id": self.checkin_sequence.id,
-    #         }
-    #     )
-    #     self.property2 = self.env["pms.property"].create(
-    #         {
-    #             "name": "Pms_property_test2",
-    #             "company_id": self.company1.id,
-    #             "default_pricelist_id": self.env.ref("product.list0").id,
-    #             "folio_sequence_id": self.folio_sequence.id,
-    #             "reservation_sequence_id": self.reservation_sequence.id,
-    #             "checkin_sequence_id": self.checkin_sequence.id,
-    #         }
-    #     )
-    #     self.room_type_availability = self.env["pms.availability.plan"].create(
-    #         {"name": "Availability plan for TEST"}
-    #     )
-    #
-    #     # create room type class
-    #     self.room_type_class = self.env["pms.room.type.class"].create(
-    #         {"name": "Room", "default_code": "ROOM"}
-    #     )
-
     def test_create_rt_props_gt_bs_props(self):
         """
         Create board service for a room type and the room type
