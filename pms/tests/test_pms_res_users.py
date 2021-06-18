e78196cce0b5742cd8a37900a924589c83eba027
Sara Lago <saralago126@gmail.com>
2021-06-18 21:45:45 +0200

    [IMP] pms: refactoring and adding checkin flow tests

--- pms/tests/test_pms_res_users.py
+++ pms/tests/test_pms_res_users.py
@@ -1,14 +1,11 @@
 from odoo.exceptions import ValidationError
-from odoo.tests import common
+
+from .common import TestPms
 
 
-class TestPmsResUser(common.SavepointCase):
-    def create_common_scenario(self):
-        # create a room type availability
-        self.room_type_availability = self.env["pms.availability.plan"].create(
-            {"name": "Availability plan 1"}
-        )
-
+class TestPmsResUser(TestPms):
+    def setUp(self):
+        super().setUp()
         # create a company and properties
         self.company_A = self.env["res.company"].create(
             {
@@ -20,104 +17,50 @@ class TestPmsResUser(common.SavepointCase):
                 "name": "Pms_Company2",
             }
         )
-        self.folio_sequenceA = self.env["ir.sequence"].create(
-            {
-                "name": "PMS Folio",
-                "code": "pms.folio",
-                "padding": 4,
-                "company_id": self.company_A.id,
-            }
-        )
-        self.reservation_sequenceA = self.env["ir.sequence"].create(
-            {
-                "name": "PMS Reservation",
-                "code": "pms.reservation",
-                "padding": 4,
-                "company_id": self.company_A.id,
-            }
-        )
-        self.checkin_sequenceA = self.env["ir.sequence"].create(
-            {
-                "name": "PMS Checkin",
-                "code": "pms.checkin.partner",
-                "padding": 4,
-                "company_id": self.company_A.id,
-            }
-        )
-        self.folio_sequenceB = self.env["ir.sequence"].create(
-            {
-                "name": "PMS Folio",
-                "code": "pms.folio",
-                "padding": 4,
-                "company_id": self.company_B.id,
-            }
-        )
-        self.reservation_sequenceB = self.env["ir.sequence"].create(
-            {
-                "name": "PMS Reservation",
-                "code": "pms.reservation",
-                "padding": 4,
-                "company_id": self.company_B.id,
-            }
-        )
-        self.checkin_sequenceB = self.env["ir.sequence"].create(
-            {
-                "name": "PMS Checkin",
-                "code": "pms.checkin.partner",
-                "padding": 4,
-                "company_id": self.company_B.id,
-            }
-        )
         self.property_A1 = self.env["pms.property"].create(
             {
                 "name": "Pms_property",
                 "company_id": self.company_A.id,
-                "default_pricelist_id": self.env.ref("product.list0").id,
-                "folio_sequence_id": self.folio_sequenceA.id,
-                "reservation_sequence_id": self.reservation_sequenceA.id,
-                "checkin_sequence_id": self.checkin_sequenceA.id,
+                "default_pricelist_id": self.pricelist1.id,
             }
         )
         self.property_A2 = self.env["pms.property"].create(
             {
                 "name": "Pms_property2",
                 "company_id": self.company_A.id,
-                "default_pricelist_id": self.env.ref("product.list0").id,
-                "folio_sequence_id": self.folio_sequenceA.id,
-                "reservation_sequence_id": self.reservation_sequenceA.id,
-                "checkin_sequence_id": self.checkin_sequenceA.id,
+                "default_pricelist_id": self.pricelist1.id,
             }
         )
         self.property_B1 = self.env["pms.property"].create(
             {
                 "name": "Pms_propertyB1",
                 "company_id": self.company_B.id,
-                "default_pricelist_id": self.env.ref("product.list0").id,
-                "folio_sequence_id": self.folio_sequenceB.id,
-                "reservation_sequence_id": self.reservation_sequenceB.id,
-                "checkin_sequence_id": self.checkin_sequenceB.id,
+                "default_pricelist_id": self.pricelist1.id,
             }
         )
 
-    def test_property_not_allowed(self):
+    def test_property_not_in_allowed_properties(self):
         """
-        Property not allowed, it belongs to another company
+        Property not allowed for the user
+        Check a user cannot have an active property
+        that is not in the allowed properties
 
         Company_A ---> Property_A1, Property_A2
         Company_B ---> Property_B1
 
+
         """
         # ARRANGE
-        name = "test user"
-        login = "test_user"
-        self.create_common_scenario()
         Users = self.env["res.users"]
         # ACT & ASSERT
-        with self.assertRaises(ValidationError), self.cr.savepoint():
+        with self.assertRaises(
+            ValidationError,
+            msg="Some property is not included in the allowed properties",
+        ):
             Users.create(
                 {
-                    "name": name,
-                    "login": login,
+                    "name": "Test User",
+                    "login": "test_user",
                     "company_ids": [(4, self.company_A.id)],
                     "company_id": self.company_A.id,
                     "pms_property_ids": [(4, self.property_A1.id)],
@@ -125,18 +68,26 @@ class TestPmsResUser(common.SavepointCase):
                 }
             )
 
-    def test_check_allowed_property_ids(self):
+    def test_property_not_in_allowed_companies(self):
+        """
+        Property not allowed for the user
+        Check a user cannot have a property in allowed properties
+        that does not belong to their companies
+
+        Company_A ---> Property_A1, Property_A2
+        Company_B ---> Property_B1
+
+        """
         # ARRANGE
-        name = "test user2"
-        login = "test_user2"
-        self.create_common_scenario()
         Users = self.env["res.users"]
         # ACT & ASSERT
-        with self.assertRaises(ValidationError), self.cr.savepoint():
+        with self.assertRaises(
+            ValidationError, msg="Some property doesn't belong to the allowed companies"
+        ):
             Users.create(
                 {
-                    "name": name,
-                    "login": login,
+                    "name": "Test User",
+                    "login": "test_user",
                     "company_ids": [(4, self.company_A.id)],
                     "company_id": self.company_A.id,
                     "pms_property_ids": [
@@ -146,3 +97,68 @@ class TestPmsResUser(common.SavepointCase):
                     "pms_property_id": self.property_A1.id,
                 }
             )
+
+    def test_property_in_allowed_properties(self):
+        """
+        Successful user creation
+        Check user creation with active property in allowed properties
+
+        Company_A ---> Property_A1, Property_A2
+        Company_B ---> Property_B1
+
+        """
+        # ARRANGE
+        Users = self.env["res.users"]
+        # ACT
+        user1 = Users.create(
+            {
+                "name": "Test User",
+                "login": "test_user",
+                "company_ids": [(4, self.company_A.id)],
+                "company_id": self.company_A.id,
+                "pms_property_ids": [
+                    (4, self.property_A1.id),
+                    (4, self.property_A2.id),
+                ],
+                "pms_property_id": self.property_A1.id,
+            }
+        )
+        # ASSERT
+        self.assertIn(
+            user1.pms_property_id,
+            user1.pms_property_ids,
+            "Active property not in allowed properties",
+        )
+
+    def test_properties_belong_to_companies(self):
+        """
+        Successful user creation
+        Check user creation with active property and allowed properties
+        belonging to the allowed companies
+
+        Company_A ---> Property_A1, Property_A2
+        Company_B ---> Property_B1
+
+        """
+        # ARRANGE
+        Users = self.env["res.users"]
+        # ACT
+        user1 = Users.create(
+            {
+                "name": "Test User",
+                "login": "test_user",
+                "company_ids": [(4, self.company_A.id)],
+                "company_id": self.company_A.id,
+                "pms_property_ids": [
+                    (4, self.property_A1.id),
+                    (4, self.property_A2.id),
+                ],
+                "pms_property_id": self.property_A1.id,
+            }
+        )
+        # ASSERT
+        self.assertEqual(
+            user1.pms_property_id.company_id,
+            user1.company_id,
+            "Active property doesn't belong to active company",
+        )
