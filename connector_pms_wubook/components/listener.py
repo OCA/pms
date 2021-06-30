db01f2bc0559339b2d3789413aaaecc430cb3538
Eric Antones <eantones@nuobit.com>
2021-06-30 13:16:07 +0200

    [IMP] connector_pms_wubook: improve listeners

--- connector_pms_wubook/components/listener.py
+++ connector_pms_wubook/components/listener.py
@@ -10,13 +10,27 @@ class ChannelWubookListener(AbstractComponent):
     _name = "channel.wubook.listener"
     _inherit = "base.connector.listener"
 
-    @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
-    def on_record_create(self, record, fields=None):
-        print("XXXXXXXXXXXX on_record_create")
-        # for binding in record.channel_wubook_bind_ids:
-        #     binding.resync_export()
+    # @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
+    # def on_record_create(self, record, fields=None):
+    #     print("XXXXXXXXXXXX on_record_create", record)
+    #     # for binding in record.channel_wubook_bind_ids:
+    #     #     binding.resync_export()
 
     @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
     def on_record_write(self, record, fields=None):
         for binding in record.channel_wubook_bind_ids:
             binding.resync_export()
+
+
+class ChannelWubookBindingListener(AbstractComponent):
+    _name = "channel.wubook.binding.listener"
+    _inherit = "base.connector.listener"
+
+    # @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
+    # def on_record_create(self, record, fields=None):
+    #     print("XXXXXXXXXXXX on_record_create", record)
+    #     binding.resync_export()
+
+    @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
+    def on_record_write(self, record, fields=None):
+        record.resync_export()
