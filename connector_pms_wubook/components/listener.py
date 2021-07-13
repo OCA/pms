fd7592074ddd587711f9ee81f7a7204ca9fe1fb2
Eric Antones <eantones@nuobit.com>
2021-07-13 09:05:04 +0200

    [FIX] connector_pms_wubook: use export_record instead of resync

--- connector_pms_wubook/components/listener.py
+++ connector_pms_wubook/components/listener.py
@@ -3,34 +3,76 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo.addons.component.core import AbstractComponent
-from odoo.addons.component_event import skip_if
-
+from odoo.addons.component_event.components.event import skip_if
 
 class ChannelWubookListener(AbstractComponent):
     _name = "channel.wubook.listener"
     _inherit = "base.connector.listener"
 
+    def _data(self, record):
+        return record
+
+    # Create listener on_record_create GENERIC on non-binding model
+    # makes no sense because we don't have any bindings yet.
+    # We can deal with that on specific models because we know the
+    # context exactly like any data that allow us to infer the backend/s
+    # where create the binding/s
     # @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
     # def on_record_create(self, record, fields=None):
-    #     print("XXXXXXXXXXXX on_record_create", record)
-    #     # for binding in record.channel_wubook_bind_ids:
-    #     #     binding.resync_export()
+    #     print(
+    #         "-------------------------- (%s)(%s) LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL model listener create %s"
+    #         % (
+    #             self.env.context.get("saved_from_parent"),
+    #             record.env.context.get("saved_from_parent"),
+    #             record,
+    #         ),
+    #         self._data(record),
+    #     )
 
     @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
     def on_record_write(self, record, fields=None):
+        print(
+            "-------------------------- (%s)(%s) LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL model listener write %s"
+            % (
+                self.env.context.get("saved_from_parent"),
+                record.env.context.get("saved_from_parent"),
+                record,
+            ),
+            self._data(record),
+        )
         for binding in record.channel_wubook_bind_ids:
-            binding.resync_export()
+            binding.export_record(binding.backend_id, record)
 
 
-class ChannelWubookBindingListener(AbstractComponent):
-    _name = "channel.wubook.binding.listener"
-    _inherit = "base.connector.listener"
-
-    # @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
-    # def on_record_create(self, record, fields=None):
-    #     print("XXXXXXXXXXXX on_record_create", record)
-    #     binding.resync_export()
-
-    @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
-    def on_record_write(self, record, fields=None):
-        record.resync_export()
+# class ChannelWubookBindingListener(AbstractComponent):
+#     _name = "channel.wubook.binding.listener"
+#     _inherit = "base.connector.listener"
+#
+#     def _data(self, record):
+#         return record
+#
+#     @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
+#     def on_record_create(self, record, fields=None):
+#         print(
+#             "-------------------------- (%s)(%s) LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL binding listener create %s"
+#             % (
+#                 self.env.context.get("saved_from_parent"),
+#                 record.env.context.get("saved_from_parent"),
+#                 record,
+#             ),
+#             self._data(record),
+#         )
+#         record.export_record(record.backend_id, record.odoo_id)
+#
+#     @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
+#     def on_record_write(self, record, fields=None):
+#         print(
+#             "-------------------------- (%s)(%s) LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL binding listener write %s"
+#             % (
+#                 self.env.context.get("saved_from_parent"),
+#                 record.env.context.get("saved_from_parent"),
+#                 record,
+#             ),
+#             self._data(record),
+#         )
+#         record.export_record(record.backend_id, record.odoo_id)
