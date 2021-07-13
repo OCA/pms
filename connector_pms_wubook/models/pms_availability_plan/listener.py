5e7a990b285c408393ce58a3b233803828ea91f2
Eric Antones <eantones@nuobit.com>
2021-07-13 09:05:47 +0200

    [UPD] connector_pms_wubook: disable temporarily availability, plan, plan rule and pricelist listeners

--- connector_pms_wubook/models/pms_availability_plan/listener.py
+++ connector_pms_wubook/models/pms_availability_plan/listener.py
@@ -1,12 +1,41 @@
 # Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-
 from odoo.addons.component.core import Component
 
-
-class ChannelWubookPmsAvailabilityPlanListener(Component):
-    _name = "channel.wubook.pms.availability.plan.listener"
-    _inherit = "channel.wubook.listener"
-
-    _apply_on = "pms.availability.plan"
+# class ChannelWubookPmsAvailabilityPlanListener(Component):
+#     _name = "channel.wubook.pms.availability.plan.listener"
+#     _inherit = "channel.wubook.listener"
+#
+#     _apply_on = "pms.availability.plan"
+#
+#     def _data(self, record):
+#         return f"{record.name}"
+#
+#     # @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
+#     # def on_record_create(self, record, fields=None):
+#     #     print("-------------------------- (%s)(%s) LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL model listener create ESPECIFIC %s" % (
+#     #         self.env.context.get('saved_from_parent'), record.env.context.get('saved_from_parent'), record._name))
+#     #     #super().on_record_create(record, fields=fields)
+#     #
+#     # @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
+#     # def on_record_write(self, record, fields=None):
+#     #     print(
+#     #         "-------------------------- (%s)(%s) LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL model listener write ESPECIFIC %s"
+#     #         % (
+#     #             self.env.context.get("saved_from_parent"),
+#     #             record.env.context.get("saved_from_parent"),
+#     #             record._name
+#     #         )
+#     #     )
+#     #     #super().on_record_write(record, fields=fields)
+#
+#
+# class ChannelWubookPmsAvailabilityPlanListener(Component):
+#     _name = "channel.wubook.pms.availability.plan.binding.listener"
+#     _inherit = "channel.wubook.binding.listener"
+#
+#     _apply_on = "channel.wubook.pms.availability.plan"
+#
+#     def _data(self, record):
+#         return f"{record.name}"
--- connector_pms_wubook/models/product_pricelist/listener.py
+++ connector_pms_wubook/models/product_pricelist/listener.py
@@ -2,11 +2,11 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo.addons.component.core import Component
-
-
-class ChannelWubookProductPricelistListener(Component):
-    _name = "channel.wubook.product.pricelist.listener"
-    _inherit = "channel.wubook.listener"
-
-    _apply_on = "product.pricelist"
+# from odoo.addons.component.core import Component
+#
+#
+# class ChannelWubookProductPricelistListener(Component):
+#     _name = "channel.wubook.product.pricelist.listener"
+#     _inherit = "channel.wubook.listener"
+#
+#     _apply_on = "product.pricelist"
