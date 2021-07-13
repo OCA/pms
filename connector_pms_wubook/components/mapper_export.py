81a2e709833b8b17fe827680bc7c1a6862821170
Eric Antones <eantones@nuobit.com>
2021-07-13 23:53:39 +0200

    [IMP] connector_pms_wubook: added child mapper binder

--- connector_pms_wubook/components/mapper_export.py
+++ connector_pms_wubook/components/mapper_export.py
@@ -12,3 +12,8 @@ class ChannelMapperExport(AbstractComponent):
 class ChannelWubookChildMapperExport(AbstractComponent):
     _name = "channel.wubook.child.mapper.export"
     _inherit = ["channel.child.mapper.export", "base.channel.wubook.connector"]
+
+
+class ChannelWubookChildBinderMapperExport(AbstractComponent):
+    _name = "channel.wubook.child.binder.mapper.export"
+    _inherit = ["channel.child.binder.mapper.export", "base.channel.wubook.connector"]
