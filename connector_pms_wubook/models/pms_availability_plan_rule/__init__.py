8492ea37e6bfbf582cb51cf00da7e7124c255ead
Eric Antones <eantones@nuobit.com>
2021-07-13 23:15:27 +0200

    [IMP] connector_pms_wubook: added binder to Availability Plan Rule

--- connector_pms_wubook/models/pms_availability_plan_rule/__init__.py
+++ connector_pms_wubook/models/pms_availability_plan_rule/__init__.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-
+from . import binder
 from . import binding
 from . import mapper_export
 from . import mapper_import
