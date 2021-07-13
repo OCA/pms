013a254d058e78f4601fa3d18330be95feb26032
Eric Antones <eantones@nuobit.com>
2021-07-13 15:44:53 +0200

    [IMP] connector_pms_wubook: added last sync date for export to allow different import/export sync dates

--- connector_pms_wubook/models/pms_availability_plan/listener.py
+++ connector_pms_wubook/models/pms_availability_plan/listener.py
@@ -1,7 +1,6 @@
 # Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-from odoo.addons.component.core import Component
 
 # class ChannelWubookPmsAvailabilityPlanListener(Component):
 #     _name = "channel.wubook.pms.availability.plan.listener"
