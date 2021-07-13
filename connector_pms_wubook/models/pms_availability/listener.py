96cec49e40d2cca2ad4eaf67f66f240de85259c4
Eric Antones <eantones@nuobit.com>
2021-07-13 09:05:47 +0200

    [UPD] connector_pms_wubook: disable temporarily availability, plan, plan rule and pricelist listeners

--- connector_pms_wubook/models/pms_availability/listener.py
+++ connector_pms_wubook/models/pms_availability/listener.py
@@ -2,26 +2,27 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo.addons.component.core import Component
-from odoo.addons.component_event import skip_if
-
-
-class ChannelWubookPmsAvailabilityListener(Component):
-    _name = "channel.wubook.pms.availability.listener"
-    _inherit = "channel.wubook.listener"
-
-    _apply_on = "pms.availability"
-
-    @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
-    def on_record_create(self, record, fields=None):
-        for backend_id in record.room_type_id.channel_wubook_bind_ids.backend_id:
-            self.env["channel.wubook.pms.availability"].export_record(
-                backend_id, record
-            )
-
-
-class ChannelWubookPmsAvailabilityBindingListener(Component):
-    _name = "channel.wubook.pms.availability.binding.listener"
-    _inherit = "channel.wubook.binding.listener"
-
-    _apply_on = "channel.wubook.pms.availability"
+# from odoo.addons.component.core import Component
+# from odoo.addons.component_event.components.event import skip_if
+#
+# class ChannelWubookPmsAvailabilityListener(Component):
+#     _name = "channel.wubook.pms.availability.listener"
+#     _inherit = "channel.wubook.listener"
+#
+#     _apply_on = "pms.availability"
+#
+#     @skip_if(lambda self, record, **kwargs: self.no_connector_export(record))
+#     def on_record_create(self, record, fields=None):
+#         super().on_record_create(record, fields=fields)
+#         backends = record.room_type_id.channel_wubook_bind_ids.backend_id.filtered(
+#             lambda x: x.pms_property_id == record.pms_property_id
+#         )
+#         for backend in backends:
+#             self.env["channel.wubook.pms.availability"].export_record(backend, record)
+#
+#
+# class ChannelWubookPmsAvailabilityBindingListener(Component):
+#     _name = "channel.wubook.pms.availability.binding.listener"
+#     _inherit = "channel.wubook.binding.listener"
+#
+#     _apply_on = "channel.wubook.pms.availability"
