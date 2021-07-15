7adeaaab907006ab6b913a9b6b5f3ecdbdb7ec4a
Eric Antones <eantones@nuobit.com>
2021-07-15 13:13:45 +0200

    [FIX] connector_pms_wubook: error exporting records older than 2 days ago

--- connector_pms_wubook/models/pms_availability_plan/mapper_export.py
+++ connector_pms_wubook/models/pms_availability_plan/mapper_export.py
@@ -1,6 +1,7 @@
 # Copyright 2021 Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo import fields
 
 from odoo.addons.component.core import Component
 
@@ -31,9 +32,14 @@ class ChannelWubookPmsAvailabilityPlanChildBinderMapperExport(Component):
     _apply_on = "channel.wubook.pms.availability.plan.rule"
 
     def skip_item(self, map_record):
-        return (
-            map_record.source.pms_property_id != self.backend_record.pms_property_id
-            or map_record.source.synced_export
+        return any(
+            [
+                map_record.source.pms_property_id
+                != self.backend_record.pms_property_id,
+                map_record.source.synced_export,
+                # Wubook does not allow to update records older than 2 days ago
+                (fields.Date.today() - map_record.source.date).days > 2,
+            ]
         )
 
     def get_all_items(self, mapper, items, parent, to_attr, options):
