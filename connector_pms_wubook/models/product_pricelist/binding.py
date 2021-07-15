f2b0d4f193fea4c7aafe52e59c46da993f3e6412
Eric Antones <eantones@nuobit.com>
2021-07-15 20:04:28 +0200

    [FIX] connector_pms_wubook: no Pricelist or Availability Plan exported if it's done from backend

--- connector_pms_wubook/models/product_pricelist/binding.py
+++ connector_pms_wubook/models/product_pricelist/binding.py
@@ -84,6 +84,7 @@ class ChannelWubookProductPriceBinding(models.Model):
         return self.export_batch(
             backend_record=backend_record,
             domain=[
+                "|",
                 ("pms_property_ids", "=", False),
                 ("pms_property_ids", "in", backend_record.pms_property_id.ids),
             ],
