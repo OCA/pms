8a99efafb59f1b5891f36783b1b2f314b790d472
miguelpadin <miguel.padin.dominguez@gmail.com>
2021-07-26 13:07:37 +0200

    [ADD] pms_api_rest: module created

---
+++ pms_api_rest/__init__.py
@@ -0,0 +1,5 @@
+from . import controllers
+from . import datamodels
+from . import models
+from . import services
+
---
+++ pms_api_rest/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "API REST PMS",
+    "author": "Commit [Sun], Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pms",
+    "category": "Generic Modules/Property Management System",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "pms",
+        "base_rest",
+        "base_rest_datamodel",
+        "web",
+        "auth_signup",
+    ],
+    "external_dependencies": {
+        "python": ["jwt", "simplejson", "marshmallow"],
+    },
+    "installable": True,
+}
