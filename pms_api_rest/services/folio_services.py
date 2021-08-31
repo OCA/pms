cf45bce8f8a7c4b6e213136d48b673c805f4c07c
miguelpadin <miguel.padin.dominguez@gmail.com>
2021-08-31 18:15:29 +0200

    [IMP] pms-pwa: service folios

--- pms_api_rest/services/folio_services.py
+++ pms_api_rest/services/folio_services.py
@@ -1,10 +1,6 @@
-import json
-
 from odoo.addons.base_rest import restapi
 from odoo.addons.base_rest_datamodel.restapi import Datamodel
 from odoo.addons.component.core import Component
-
-from ..datamodels.pms_folio_short_info import PmsReservationSchema
 
 
 class PmsFolioService(Component):
@@ -26,78 +22,41 @@ class PmsFolioService(Component):
         output_param=Datamodel("pms.folio.short.info", is_list=True),
         auth="public",
     )
-    def search(self, folio_search_param):
+    def get_folios(self, folio_search_param):
         domain = []
         if folio_search_param.name:
             domain.append(("name", "like", folio_search_param.name))
         if folio_search_param.id:
             domain.append(("id", "=", folio_search_param.id))
-        res = []
+        result_folios = []
         PmsFolioShortInfo = self.env.datamodels["pms.folio.short.info"]
-        PmsReservationShortInfo = self.env.datamodels["pms.reservation.short.info"]
         for folio in (
             self.env["pms.folio"]
-                .sudo()
-                .search(
+            .sudo()
+            .search(
                 domain,
             )
         ):
             reservations = []
             for reservation in folio.reservation_ids:
-                reservation_schema = PmsReservationSchema()
-                room = reservation.preferred_room_id.name if reservation.preferred_room_id else ""
-                room_type = reservation.room_type_id.name if reservation.room_type_id else ""
-                data = {
-                            "id": reservation.id,
-                            "checkin": str(reservation.checkin),
-                            "checkout": str(reservation.checkout),
-                            "preferredRoomId": room,
-                            "roomTypeId": room_type,
-                            "priceTotal": reservation.price_total,
-                            "adults": reservation.adults,
-                            "pricelist": reservation.pricelist_id.name,
-                        }
-                reservations.append(reservation_schema.load(data))
-                '''{
-                "id": reservation.id,
-                "checkin": str(reservation.checkin),
-                "checkout": str(reservation.checkout),
-                "preferredRoomId": reservation.preferred_room_id.name
-                if reservation.preferred_room_id
-                else "",
-                "roomTypeId": reservation.room_type_id.name
-                if reservation.room_type_id
-                else "",
-                "priceTotal": reservation.price_total,
-                "adults": reservation.adults,
-                "pricelist": reservation.pricelist_id.name,
-                }'''
-
-
-
-
-
-
-                '''
                 reservations.append(
-                    PmsReservationSchema(
-                        "id":reservation.id,
-                        checkin=str(reservation.checkin),
-                        checkout=str(reservation.checkout),
-                        preferredRoomId=reservation.preferred_room_id.name
+                    {
+                        "id": reservation.id,
+                        "checkin": str(reservation.checkin),
+                        "checkout": str(reservation.checkout),
+                        "preferredRoomId": reservation.preferred_room_id.name
                         if reservation.preferred_room_id
                         else "",
-                        roomTypeId=reservation.room_type_id.name
+                        "roomTypeId": reservation.room_type_id.name
                         if reservation.room_type_id
                         else "",
-                        priceTotal=reservation.price_total,
-                        adults=reservation.adults,
-                        pricelist=reservation.pricelist_id.name,
-                    )
+                        "priceTotal": reservation.price_total,
+                        "adults": reservation.adults,
+                        "pricelist": reservation.pricelist_id.name,
+                    }
                 )
-                '''
 
-            res.append(
+            result_folios.append(
                 PmsFolioShortInfo(
                     id=folio.id,
                     name=folio.name,
@@ -106,17 +65,14 @@ class PmsFolioService(Component):
                     partnerEmail=folio.email if folio.email else "",
                     channelType=folio.channel_type_id if folio.channel_type_id else "",
                     agency=folio.agency_id if folio.agency_id else "",
-                    # paymentState=dict(folio.fields_get(["payment_state"])["payment_state"]["selection"])[
-                    #     folio.payment_state
-                    # ],
                     state=dict(folio.fields_get(["state"])["state"]["selection"])[
                         folio.state
                     ],
                     pendingAmount=folio.pending_amount,
-                    reservations=reservations,
+                    reservations=[] if not reservations else reservations,
                 )
             )
-        return res
+        return result_folios
 
     @restapi.method(
         [
