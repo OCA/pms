d69dba8a389bc0768f42dd825ebccd0cfd6868d9
miguelpadin <miguel.padin.dominguez@gmail.com>
2021-09-21 09:32:06 +0200

    [ADD] pms-pwa: nested reservation lines

--- pms_api_rest/datamodels/__init__.py
+++ pms_api_rest/datamodels/__init__.py
@@ -1,4 +1,4 @@
-from . import pms_reservation_short_info
-from . import pms_reservation_search_param
 from . import pms_folio_short_info
 from . import pms_folio_search_param
+from . import pms_room_short_info
+from . import pms_room_search_param
