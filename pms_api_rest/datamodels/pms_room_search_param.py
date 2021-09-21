b4b84d6335bd3694e5f88b95077f8511bc8dc3c7
miguelpadin <miguel.padin.dominguez@gmail.com>
2021-09-21 09:32:06 +0200

    [ADD] pms-pwa: nested reservation lines

--- pms_api_rest/datamodels/pms_room_search_param.py
+++ pms_api_rest/datamodels/pms_room_search_param.py
@@ -3,8 +3,8 @@ from marshmallow import fields
 from odoo.addons.datamodel.core import Datamodel
 
 
-class PmsReservationSearchParam(Datamodel):
-    _name = "pms.reservation.search.param"
+class PmsRoomSearchParam(Datamodel):
+    _name = "pms.room.search.param"
 
     id = fields.Integer(required=False, allow_none=False)
     name = fields.String(required=False, allow_none=False)
