d376d7d93025153f655fb278fcf39d60feba7212
Eric Antones <eantones@nuobit.com>
2021-09-23 15:18:41 +0200

    [FIX] connector_pms_wubook: pricelists and availability plans exported without changes

--- connector_pms_wubook/models/pms_property_availability/binding.py
+++ connector_pms_wubook/models/pms_property_availability/binding.py
@@ -25,6 +25,23 @@ class ChannelWubookPmsPropertyAvailabilityBinding(models.Model):
         inverse_name="channel_wubook_property_availability_id",
     )
 
+    def _is_synced_export(self):
+        synced = super()._is_synced_export()
+        if not synced:
+            return False
+        newrules = self.availability_ids.filtered(
+            lambda x: x.wubook_date_valid()
+            and self.backend_id not in x.channel_wubook_bind_ids.backend_id
+        )
+        if newrules:
+            return False
+
+        return all(
+            self.channel_wubook_availability_ids.filtered(
+                lambda x: x.odoo_id.wubook_date_valid()
+            ).mapped("synced_export")
+        )
+
     @api.model
     def export_data(self, backend_record=None):
         """ Prepare the export of Availability to Channel """
--- connector_pms_wubook/models/product_pricelist_item/product_pricelist_item.py
+++ connector_pms_wubook/models/product_pricelist_item/product_pricelist_item.py
@@ -33,3 +33,7 @@ class ProductPricelistItem(models.Model):
                 rec.wubook_item_type = "standard"
             else:
                 rec.wubook_item_type = False
+
+    def wubook_date_valid(self):
+        # Wubook does not allow to update records older than 2 days ago
+        return (fields.Date.today() - self.date_start_consumption).days <= 2
