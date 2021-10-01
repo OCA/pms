f8958030ba2e73478cc8de2296f831d7643fb0b6
miguelpadin <miguel.padin.dominguez@gmail.com>
2021-10-01 09:29:19 +0200

    [ADD] pms: add ine code to res.partner

--- pms_l10n_es/tests/__init__.py
+++ pms_l10n_es/tests/__init__.py
@@ -1,1 +1,2 @@
 from . import test_wizard_ine
+from . import test_res_partner
---
+++ pms_l10n_es/tests/test_res_partner.py
@@ -0,0 +1,50 @@
+from .common import TestPms
+
+
+class TestResPartner(TestPms):
+    def setUp(self):
+        super().setUp()
+
+    def test_ine_code_foreign_partner(self):
+        # ARRANGE & ACT
+        # get record of russia
+        self.country_russia = self.env["res.country"].search([("code", "=", "RU")])
+
+        # Create partner 1 (russia)
+        self.partner_1 = self.env["res.partner"].create(
+            {
+                "name": "partner1",
+                "country_id": self.country_russia.id,
+                "nationality_id": self.country_russia.id,
+                "birthdate_date": "2000-06-25",
+                "gender": "male",
+            }
+        )
+        # ASSERT
+        self.assertEqual(
+            self.partner_1.ine_code,
+            self.partner_1.country_id.code_alpha3
+        )
+
+    def test_ine_code_spanish_partner(self):
+        # ARRANGE & ACT
+        # get record of russia
+        country_spain = self.env["res.country"].search([("code", "=", "ES")])
+        state_madrid = self.env["res.country.state"].search([("name", "=", "Madrid")])
+
+        # Create partner 1 (russia)
+        self.partner_1 = self.env["res.partner"].create(
+            {
+                "name": "partner1",
+                "country_id": country_spain.id,
+                "nationality_id": country_spain.id,
+                "state_id": state_madrid.id,
+                "birthdate_date": "2000-06-25",
+                "gender": "male",
+            }
+        )
+        # ASSERT
+        self.assertEqual(
+            self.partner_1.ine_code,
+            self.partner_1.state_id.ine_code
+        )
