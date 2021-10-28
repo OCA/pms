04ee1d92a99fd2483d41f130805612abe2c0bb62
Eric Antones <eantones@nuobit.com>
2021-10-28 12:48:44 +0200

    [FIX] connector_pms_wubook: segregate board service price from the room price

--- connector_pms_wubook/models/pms_reservation_line/mapper_import.py
+++ connector_pms_wubook/models/pms_reservation_line/mapper_import.py
@@ -1,8 +1,12 @@
 # Copyright 2021 Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-
 from odoo.addons.component.core import Component
+from odoo.addons.connector.components.mapper import mapping
+from odoo.addons.connector_pms_wubook.models.pms_reservation.mapper_import import (
+    get_board_service_room_type,
+    get_room_type,
+)
 
 
 class ChannelWubookPmsReservationLineMapperImport(Component):
@@ -12,6 +16,16 @@ class ChannelWubookPmsReservationLineMapperImport(Component):
     _apply_on = "channel.wubook.pms.reservation.line"
 
     direct = [
-        ("price", "price"),
         ("day", "date"),
     ]
+
+    @mapping
+    def price(self, record):
+        price = record["price"]
+        if record["board"]:
+            room_type = get_room_type(self, record["room_id"])
+            board_service_room = get_board_service_room_type(
+                self, room_type, record["board"]
+            )
+            price -= board_service_room.amount * record["occupancy"]
+        return {"price": price}
