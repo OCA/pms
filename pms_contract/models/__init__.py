a6c9c285747b1e3f747585c03b73808d4ea59c22
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2021-11-24 09:30:10 -0600

    [ADD] pms_contract

---
+++ pms_contract/models/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2018  Alexandre Díaz
+# Copyright 2018  Dario Lodeiros
+# Copyright (c) 2021 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import (
+    contract_contract,
+    contract_line,
+    pms_service,
+    pms_property,
+)
