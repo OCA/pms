cd451c85148a589656cbd47b97adddd5253c9674
Sara Lago <saralago126@gmail.com>
2021-11-24 16:33:16 +0100

    [REF] pms_api_rest: renaming of datamodel files

--- pms_api_rest/datamodels/pms_room_info.py
+++ pms_api_rest/datamodels/pms_room_info.py
@@ -3,7 +3,7 @@ from marshmallow import fields
 from odoo.addons.datamodel.core import Datamodel
 
 
-class PmsRoomShortInfo(Datamodel):
-    _name = "pms.room.short.info"
+class PmsRoomInfo(Datamodel):
+    _name = "pms.room.info"
     id = fields.Integer(required=False, allow_none=True)
     name = fields.String(required=False, allow_none=True)
