cfcda8c97834fb675bc84ac8809f9bd1e4c19634
Sara Lago <saralago126@gmail.com>
2021-11-24 12:58:22 +0100

    [IMP] pms_api_rest: add get_reservation and get_checkin_partners

---
+++ pms_api_rest/datamodels/pms_checkin_partner_short_info.py
@@ -0,0 +1,18 @@
+from marshmallow import fields
+
+from odoo.addons.datamodel.core import Datamodel
+
+
+class PmsCheckinPartnerShortInfo(Datamodel):
+    _name = "pms.checkin.partner.short.info"
+    id = fields.Integer(required=False, allow_none=True)
+    # partner = fields.String(required=False, allow_none=True)
+    reservationId = fields.Integer(required=False, allow_none=True)
+    name = fields.String(required=False, allow_none=True)
+    email = fields.String(required=False, allow_none=True)
+    mobile = fields.String(required=False, allow_none=True)
+    nationality = fields.String(required=False, allow_none=True)
+    documentType = fields.String(required=False, allow_none=True)
+    documentNumber = fields.String(required=False, allow_none=True)
+    gender = fields.String(required=False, allow_none=True)
+    state = fields.String(required=False, allow_none=True)
