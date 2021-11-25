0d8db7a3939df6e6aca99c10a5bda81578c4978b
miguelpadin <miguel.padin.dominguez@gmail.com>
2021-11-25 12:38:23 +0100

    [ADD] pms_api_rest: add room type services

--- pms_api_rest/datamodels/__init__.py
+++ pms_api_rest/datamodels/__init__.py
@@ -5,6 +5,7 @@ from . import pms_folio_search_param
 from . import pms_folio_info
 
 from . import pms_room_info
+from . import pms_room_type_info
 from . import pms_room_search_param
 
 from . import pms_reservation_info
