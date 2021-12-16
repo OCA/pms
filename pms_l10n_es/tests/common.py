210fa37dadac49c5e024e65321c9866cb93fceca
miguelpadin <miguel.padin.dominguez@gmail.com>
2021-12-16 13:56:08 +0100

    [FIX] pms_l10n_es: fix issue traveller report & ine (filter property)

--- pms_l10n_es/tests/common.py
+++ pms_l10n_es/tests/common.py
@@ -19,6 +19,15 @@ class TestPms(common.SavepointCase):
                 "name": "Property 1",
                 "company_id": self.company1.id,
                 "default_pricelist_id": self.pricelist1.id,
+                "institution_property_id": "Dummy institution property id 1",
+            }
+        )
+        self.pms_property2 = self.env["pms.property"].create(
+            {
+                "name": "Property 2",
+                "company_id": self.company1.id,
+                "default_pricelist_id": self.pricelist1.id,
+                "institution_property_id": "Dummy institution property id 2",
             }
         )
         self.room_type_class1 = self.env["pms.room.type.class"].create(
