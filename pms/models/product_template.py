15c670b6da2e0df31e7dfbfc04f04ff345bebee8
braisab <braisterbutalino@gmail.com>
2021-12-22 20:13:44 +0100

    [IMP]: product_template daily_limit and list_price depends of property

--- pms/models/product_template.py
+++ pms/models/product_template.py
@@ -38,9 +38,11 @@ class ProductTemplate(models.Model):
         string="Daily limit",
         help="Indicates how much products can consumed in one day",
         compute="_compute_daily_limit",
-        inverse="_inverse_ir_pms_property",
-        readonly=False,
-        store=True,
+        inverse="_inverse_daily_limit",
+    )
+    list_price = fields.Float(
+        compute="_compute_list_price",
+        inverse="_inverse_list_price",
     )
     is_extra_bed = fields.Boolean(
         string="Is extra bed",
@@ -53,47 +55,108 @@ class ProductTemplate(models.Model):
         default=False,
     )
 
-    # @api.depends_context("allowed_pms_property_ids")
-    @api.depends("pms_property_ids")
+    @api.depends_context("allowed_pms_property_ids")
+    # @api.depends("pms_property_ids")
     def _compute_daily_limit(self):
         for record in self:
-            pms_property_id = False
-            if record.pms_property_ids:
-                pms_property_id = self.env.user.get_active_property_ids()[0]
+            pms_property_id = self.env.user.get_active_property_ids()[0]
             if pms_property_id:
-                property = self.env["pms.property"].browse(pms_property_id)
-            else:
-                property = False
-            if property:
-                model_id = self.env["ir.model"].browse(self._name)
-                model = self.env["ir.model"].search([("id", "=", model_id)])
-                field_id = self.env["ir.model.fields"].search(
-                    [("name", "=", "daily_limit"), ("model_id", "=", model)]
-                )
-                ir_pms_property = self.env["ir.pms.property"].search(
-                    [
-                        ("pms_property_id", "=", property.id),
-                        ("field_id", "=", field_id.id),
-                        ("res_id", "=", record),
-                    ]
-                )
-                record.daily_limit = ir_pms_property.value
+                model_id = self.env["ir.model"].browse(self._name).id
+                model = self.env["ir.model"].search([("model", "=", model_id)])
+                if model:
+                    field_id = self.env["ir.model.fields"].search(
+                        [("name", "=", "daily_limit"), ("model_id", "=", model.id)]
+                    )
+                    ir_pms_property = self.env["ir.pms.property"].search(
+                        [
+                            ("pms_property_id", "=", pms_property_id),
+                            ("field_id", "=", field_id[0].id),
+                            ("record", "=", record.id),
+                        ]
+                    )
+                    if ir_pms_property:
+                        record.daily_limit = ir_pms_property.value_integer
+                    else:
+                        record.daily_limit = False
 
-    def _inverse_ir_pms_property(self):
+    @api.depends_context("allowed_pms_property_ids")
+    # @api.depends("pms_property_ids")
+    def _compute_list_price(self):
         for record in self:
             pms_property_id = self.env.user.get_active_property_ids()[0]
-            field_id = self.env["ir.model.fields"].search(
-                [("name", "=", "daily_limit")]
-            )
-            model_id = self.env["ir.model"].search([("field_id", "=", field_id[1].id)])
-            ir_pms_property = self.env["ir.pms.property"].search(
-                [
-                    ("pms_property_id", "=", pms_property_id),
-                    ("model_id", "=", model_id.id),
-                    ("field_id", "=", field_id[1].id),
-                ]
-            )
-            if ir_pms_property:
-                ir_pms_property.value = record.daily_limit
-            # else:
-            #     crear
+            if pms_property_id:
+                model_id = self.env["ir.model"].browse(self._name).id
+                model = self.env["ir.model"].search([("model", "=", model_id)])
+                if model:
+                    field_id = self.env["ir.model.fields"].search(
+                        [("name", "=", "list_price"), ("model_id", "=", model.id)]
+                    )
+                    ir_pms_property = self.env["ir.pms.property"].search(
+                        [
+                            ("pms_property_id", "=", pms_property_id),
+                            ("field_id", "=", field_id[0].id),
+                            ("record", "=", record.id),
+                        ]
+                    )
+                    if ir_pms_property:
+                        record.list_price = ir_pms_property.value_float
+
+    def _inverse_daily_limit(self):
+        for record in self:
+            pms_property_id = self.env.user.get_active_property_ids()[0]
+            if pms_property_id:
+                model_id = self.env["ir.model"].browse(self._name).id
+                model = self.env["ir.model"].search([("model", "=", model_id)])
+                if model:
+                    field_id = self.env["ir.model.fields"].search(
+                        [("name", "=", "daily_limit"), ("model_id", "=", model.id)]
+                    )
+                    ir_pms_property = self.env["ir.pms.property"].search(
+                        [
+                            ("pms_property_id", "=", pms_property_id),
+                            ("field_id", "=", field_id[0].id),
+                            ("record", "=", record.id),
+                        ]
+                    )
+                    if ir_pms_property:
+                        ir_pms_property.value_integer = record.daily_limit
+                    else:
+                        self.env["ir.pms.property"].create(
+                            {
+                                "pms_property_id": pms_property_id,
+                                "model_id": model.id,
+                                "field_id": field_id[0].id,
+                                "value_integer": record.daily_limit,
+                                "record": record.id,
+                            }
+                        )
+
+    def _inverse_list_price(self):
+        for record in self:
+            pms_property_id = self.env.user.get_active_property_ids()[0]
+            if pms_property_id:
+                model_id = self.env["ir.model"].browse(self._name).id
+                model = self.env["ir.model"].search([("model", "=", model_id)])
+                if model:
+                    field_id = self.env["ir.model.fields"].search(
+                        [("name", "=", "list_price"), ("model_id", "=", model.id)]
+                    )
+                    ir_pms_property = self.env["ir.pms.property"].search(
+                        [
+                            ("pms_property_id", "=", pms_property_id),
+                            ("field_id", "=", field_id[0].id),
+                            ("record", "=", record.id),
+                        ]
+                    )
+                    if ir_pms_property:
+                        ir_pms_property.value_float = record.list_price
+                    else:
+                        self.env["ir.pms.property"].create(
+                            {
+                                "pms_property_id": pms_property_id,
+                                "model_id": model.id,
+                                "field_id": field_id[0].id,
+                                "value_float": record.list_price,
+                                "record": record.id,
+                            }
+                        )
