44834df631e1a2394e88644066ca230bf02abb51
miguelpadin <miguel.padin.dominguez@gmail.com>
2021-12-27 11:31:35 +0100

    [REF] pms-api-rest: add controller 4 login

--- pms_api_rest/services/__init__.py
+++ pms_api_rest/services/__init__.py
@@ -5,4 +5,8 @@ from . import calendar_service
 from . import partner_services
 
 from . import reservation_services
+<<<<<<< HEAD
 from . import property_services
+=======
+from . import login_service
+>>>>>>> a4394db3... [REF] pms-api-rest: add controller 4 login
