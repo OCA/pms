3adfe0aee9570f050e13a71534e7ef1932dd8ea6
miguelpadin <miguel.padin.dominguez@gmail.com>
2021-12-28 15:16:05 +0100

    [IMP] pms-api-rest: add several fields reserv. line service

--- pms_api_rest/services/room_services.py
+++ pms_api_rest/services/room_services.py
@@ -21,7 +21,6 @@ class PmsRoomService(Component):
         input_param=Datamodel("pms.room.search.param"),
         output_param=Datamodel("pms.room.info", is_list=True),
         auth="jwt_api_pms",
-
     )
     def get_rooms(self, room_search_param):
         domain = []
