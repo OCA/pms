1649b24887a78070bbe74485a9b6fdb587b2d167
Sara Lago <saralago126@gmail.com>
2022-01-07 10:17:06 +0100

    [IMP] pms_api_rest: add post reservations

--- pms_api_rest/datamodels/pms_reservation_info.py
+++ pms_api_rest/datamodels/pms_reservation_info.py
@@ -6,21 +6,20 @@ from odoo.addons.datamodel.core import Datamodel
 class PmsReservationInfo(Datamodel):
     _name = "pms.reservation.info"
     id = fields.Integer(required=False, allow_none=True)
-<<<<<<< HEAD
-    price = fields.Float(required=False, allow_none=True)
-    checkin = fields.String(required=False, allow_none=True)
-    checkout = fields.String(required=False, allow_none=True)
-=======
     partner = fields.String(required=False, allow_none=True)
     name = fields.String(required=False, allow_none=True)
     checkin = fields.String(required=False, allow_none=True)
     checkout = fields.String(required=False, allow_none=True)
-    roomTypeId = fields.String(required=False, allow_none=True)
+    roomTypeId = fields.Integer(required=False, allow_none=True)
+    roomTypeName = fields.String(required=False, allow_none=True)
     preferredRoomId = fields.String(required=False, allow_none=True)
     priceTotal = fields.Float(required=False, allow_none=True)
     priceOnlyServices = fields.Float(required=False, allow_none=True)
     priceOnlyRoom = fields.Float(required=False, allow_none=True)
-    pricelist = fields.String(required=False, allow_none=True)
+    pricelistName = fields.String(required=False, allow_none=True)
+    pricelistId = fields.Integer(required=False, allow_none=True)
     services = fields.List(fields.Dict(required=False, allow_none=True))
     messages = fields.List(fields.Dict(required=False, allow_none=True))
->>>>>>> d6e6a667... [IMP] pms_api_rest: add get_reservation and get_checkin_partners
+    property = fields.Integer(required=False, allow_none=True)
+    boardServiceId = fields.Integer(required=False, allow_none=True)
+    channelTypeId = fields.Integer(required=False, allow_none=True)
