c6fc7aaf666edebd622e75aa4e5639cd0ac9b7c0
miguelpadin <miguel.padin.dominguez@gmail.com>
2022-01-13 13:01:45 +0100

    [IMP] pms_api_rest: not using session in api rest

--- pms_api_rest/controllers/pms_rest.py
+++ pms_api_rest/controllers/pms_rest.py
@@ -5,5 +5,5 @@ class BaseRestPrivateApiController(main.RestController):
     _root_path = "/api/"
     _collection_name = "pms.services"
     _default_auth = "public"
-
+    _default_save_session = False
     _default_cors = "*"
