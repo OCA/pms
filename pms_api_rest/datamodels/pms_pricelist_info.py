a5e62e019d6217358613efa2d496948707e8e1ea
miguelpadin <miguel.padin.dominguez@gmail.com>
2022-01-13 12:36:59 +0100

    [IMP] pms_api_rest: pricelist $ avail. plan service"

---
+++ pms_api_rest/datamodels/pms_pricelist_info.py
@@ -0,0 +1,10 @@
+from marshmallow import fields
+
+from odoo.addons.datamodel.core import Datamodel
+
+
+class PmsPricelistInfo(Datamodel):
+    _name = "pms.pricelist.info"
+    id = fields.Integer(required=False, allow_none=True)
+    name = fields.String(required=False, allow_none=True)
+    pms_property_id = fields.Integer(required=False, allow_none=True)
