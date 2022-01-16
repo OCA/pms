7c2c3c2c011e6533e415253a1a3956521b527853
Darío Lodeiros <dario@commitsun.com>
2022-01-16 22:15:54 +0100

    [WIP] automatic reconcile statement line cash and linked folio payments

--- pms/models/account_bank_statement.py
+++ pms/models/account_bank_statement.py
@@ -14,3 +14,44 @@ class AccountBankStatement(models.Model):
         string="Company",
         help="The company for Account Bank Statement",
     )
+
+    def button_post(self):
+        """
+        Override the default method to add autoreconcile payments and statement lines
+        """
+        lines_of_moves_to_post = self.line_ids.filtered(
+            lambda line: line.move_id.state != "posted"
+        )
+        super(AccountBankStatement, self).button_post()
+        for line in lines_of_moves_to_post:
+            folio_ids = line.folio_ids.ids
+            if folio_ids:
+                to_reconcile_ids = self.env["account.move.line"].search(
+                    [
+                        ("move_id.folio_ids", "in", folio_ids),
+                        ("reconciled", "=", False),
+                        "|",
+                        (
+                            "account_id",
+                            "=",
+                            self.journal_id.payment_debit_account_id.id,
+                        ),
+                        (
+                            "account_id",
+                            "=",
+                            self.journal_id.payment_credit_account_id.id,
+                        ),
+                        ("journal_id", "=", self.journal_id.id),
+                    ]
+                )
+                if to_reconcile_ids:
+                    statement_move_line = line.move_id.line_ids.filtered(
+                        lambda line: line.account_id.reconcile
+                    )
+                    payment_line = self.env["account.move.line"].browse(
+                        to_reconcile_ids.ids
+                    )[0]
+                    if payment_line and statement_move_line:
+                        statement_move_line.account_id = payment_line.account_id
+                        lines_to_reconcile = payment_line + statement_move_line
+                        lines_to_reconcile.reconcile()
