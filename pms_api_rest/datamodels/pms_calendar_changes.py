7e26b5bdd328305e5912ddb0ad4feb93d595d3c7
Sara Lago <saralago126@gmail.com>
2022-01-25 13:01:08 +0100

    [REF] pms_api_rest: consistency in datamodels and services filenames

--- pms_api_rest/datamodels/pms_calendar_changes.py
+++
@@ -1,8 +0,0 @@
-from marshmallow import fields
-
-from odoo.addons.datamodel.core import Datamodel
-
-
-class PmsCalendarChanges(Datamodel):
-    _name = "pms.calendar.changes"
-    reservationLinesChanges = fields.List(fields.Dict(required=False, allow_none=True))
