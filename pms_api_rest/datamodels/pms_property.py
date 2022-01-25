57dcb07427c5d57c19f8998435be45774bae13a4
Sara Lago <saralago126@gmail.com>
2022-01-25 13:01:08 +0100

    [REF] pms_api_rest: consistency in datamodels and services filenames

--- pms_api_rest/datamodels/pms_property.py
+++ pms_api_rest/datamodels/pms_property.py
@@ -3,6 +3,13 @@ from marshmallow import fields
 from odoo.addons.datamodel.core import Datamodel
 
 
+class PmsPropertySearchParam(Datamodel):
+    _name = "pms.property.search.param"
+
+    id = fields.Integer(required=False, allow_none=False)
+    name = fields.String(required=False, allow_none=False)
+
+
 class PmsPropertyInfo(Datamodel):
     _name = "pms.property.info"
     id = fields.Integer(required=False, allow_none=True)
