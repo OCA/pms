54a80689aa5367ab253f5a6aac54a9ed73bb4aee
Sara Lago <saralago126@gmail.com>
2022-01-25 13:01:08 +0100

    [REF] pms_api_rest: consistency in datamodels and services filenames

--- pms_api_rest/datamodels/pms_pricelist_item.py
+++ pms_api_rest/datamodels/pms_pricelist_item.py
@@ -3,6 +3,13 @@ from marshmallow import fields
 from odoo.addons.datamodel.core import Datamodel
 
 
+class PmsPricelistItemSearchParam(Datamodel):
+    _name = "pms.pricelist.item.search.param"
+    date_from = fields.String(required=True, allow_none=False)
+    date_to = fields.String(required=True, allow_none=False)
+    pms_property_id = fields.Integer(required=True, allow_none=False)
+
+
 class PmsPricelistItemInfo(Datamodel):
     _name = "pms.pricelist.item.info"
     pricelist_item_id = fields.Integer(required=False, allow_none=True)
--- pms_api_rest/datamodels/user_input.py
+++
@@ -1,9 +0,0 @@
-from marshmallow import fields
-
-from odoo.addons.datamodel.core import Datamodel
-
-
-class PmsApiRestUserInput(Datamodel):
-    _name = "pms.api.rest.user.input"
-    username = fields.String(required=False, allow_none=True)
-    password = fields.String(required=False, allow_none=True)
