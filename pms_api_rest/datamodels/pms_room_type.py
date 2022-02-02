b12bbe9b19b6b471f0d6820dff8b9d0988155b81
Sara Lago <saralago126@gmail.com>
2022-02-02 16:43:48 +0100

    [IMP] pms_api_rest: add several properties in query params

--- pms_api_rest/datamodels/pms_room_type.py
+++ pms_api_rest/datamodels/pms_room_type.py
@@ -7,9 +7,11 @@ class PmsRoomTypeSearchParam(Datamodel):
     _name = "pms.room.type.search.param"
     id = fields.Integer(required=False, allow_none=True)
     name = fields.String(required=False, allow_none=True)
+    pms_property_ids = fields.List(fields.Integer(), required=False)
 
 
 class PmsRoomTypeInfo(Datamodel):
     _name = "pms.room.type.info"
     id = fields.Integer(required=False, allow_none=True)
     name = fields.String(required=False, allow_none=True)
+    pms_property_ids = fields.List(fields.Integer(), required=False)
