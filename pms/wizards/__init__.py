0c38537c2487e89649a20638dd132b40e3c0733f
Darío Lodeiros <dario@commitsun.com>
2022-02-06 18:57:27 +0100

    [IMP]pms: Add wizard duplicate folio and improvemente in folio changes

--- pms/wizards/__init__.py
+++ pms/wizards/__init__.py
@@ -6,3 +6,4 @@ from . import folio_make_invoice_advance
 from . import wizard_payment_folio
 from . import wizard_folio_changes
 from . import wizard_several_partners
+from . import pms_booking_duplicate
