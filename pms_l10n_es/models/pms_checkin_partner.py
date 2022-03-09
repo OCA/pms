23e833e4bbd7a895ee37b64eb0267a239e2fcd91
braisab <braisterbutalino@gmail.com>
2022-03-09 20:34:05 +0100

    [IMP]pms_l10n_es: added support_number field in res_partner_id_number and added to checkin_partner too

--- pms_l10n_es/models/pms_checkin_partner.py
+++ pms_l10n_es/models/pms_checkin_partner.py
@@ -1,6 +1,6 @@
 import logging
 
-from odoo import api, models
+from odoo import api, fields, models
 
 CODE_SPAIN = "ES"
 
@@ -9,6 +9,26 @@ _logger = logging.getLogger(__name__)
 
 class PmsCheckinParnert(models.Model):
     _inherit = "pms.checkin.partner"
+
+    support_number = fields.Char(
+        string="Support number",
+        help="ID support number",
+        readonly=False,
+        store=True,
+        compute="_compute_support_number",
+    )
+
+    @api.depends("partner_id")
+    def _compute_support_number(self):
+        for record in self:
+            if not record.support_number:
+                if (
+                    record.partner_id.id_numbers
+                    and record.partner_id.id_numbers[0].support_number
+                ):
+                    record.support_number = record.partner_id.id_numbers[
+                        0
+                    ].support_number
 
     @api.model
     def _checkin_mandatory_fields(self, country=False, depends=False):
