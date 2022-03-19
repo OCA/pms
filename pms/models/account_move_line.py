f600af00febec4ad98e54a17f551847ff94f9450
Darío Lodeiros <dario@commitsun.com>
2022-03-19 15:51:06 +0100

    [IMP]pms: add origin agency in invoices and payments

--- pms/models/account_move_line.py
+++ pms/models/account_move_line.py
@@ -10,6 +10,11 @@ class AccountMoveLine(models.Model):
 
     # Fields declaration
     # TODO: REVIEW why not a Many2one?
+    name = fields.Char(
+        compute="_compute_name",
+        store=True,
+        readonly=False,
+    )
     folio_line_ids = fields.Many2many(
         string="Folio Lines",
         help="The folio lines in the account move lines",
@@ -41,47 +46,16 @@ class AccountMoveLine(models.Model):
         readonly=False,
         check_pms_properties=True,
     )
-    move_id = fields.Many2one(check_pms_properties=True)
-
-    @api.depends("move_id")
-    def _compute_pms_property_id(self):
-        for rec in self:
-            if rec.move_id and rec.move_id.pms_property_id:
-                rec.pms_property_id = rec.move_id.pms_property_id
-            elif not rec.pms_property_id:
-                rec.pms_property_id = False
-
-    @api.depends("name")
-    def _compute_name_changed_by_user(self):
-        for record in self:
-            # if not record._context.get("auto_name"):
-            if not self._context.get("auto_name"):
-                record.name_changed_by_user = True
-            else:
-                record.name_changed_by_user = False
-
-    name = fields.Char(
-        compute="_compute_name",
+    origin_agency_id = fields.Many2one(
+        string="Origin Agency",
+        help="The agency where the folio account move originates",
+        comodel_name="res.partner",
+        domain="[('is_agency', '=', True)]",
+        compute="_compute_origin_agency_id",
         store=True,
         readonly=False,
     )
-
-    @api.depends(
-        "folio_line_ids",
-        "payment_id",
-        "payment_id.folio_ids",
-        "statement_line_id",
-        "statement_line_id.folio_ids",
-    )
-    def _compute_folio_ids(self):
-        if self.folio_line_ids:
-            self.folio_ids = self.folio_line_ids.mapped("folio_id")
-        elif self.payment_id:
-            self.folio_ids = self.payment_id.folio_ids
-        elif self.statement_line_id:
-            self.folio_ids = self.statement_line_id.folio_ids
-        else:
-            self.folio_ids = False
+    move_id = fields.Many2one(check_pms_properties=True)
 
     @api.depends("quantity")
     def _compute_name(self):
@@ -104,3 +78,51 @@ class AccountMoveLine(models.Model):
             #           qty=record.quantity)
             #     record.with_context(auto_name=True)
             #       ._compute_name_changed_by_user()
+
+    @api.depends("move_id")
+    def _compute_pms_property_id(self):
+        for rec in self:
+            if rec.move_id and rec.move_id.pms_property_id:
+                rec.pms_property_id = rec.move_id.pms_property_id
+            elif not rec.pms_property_id:
+                rec.pms_property_id = False
+
+    @api.depends("name")
+    def _compute_name_changed_by_user(self):
+        for record in self:
+            # if not record._context.get("auto_name"):
+            if not self._context.get("auto_name"):
+                record.name_changed_by_user = True
+            else:
+                record.name_changed_by_user = False
+
+    @api.depends(
+        "folio_line_ids",
+        "payment_id",
+        "payment_id.folio_ids",
+        "statement_line_id",
+        "statement_line_id.folio_ids",
+    )
+    def _compute_folio_ids(self):
+        if self.folio_line_ids:
+            self.folio_ids = self.folio_line_ids.mapped("folio_id")
+        elif self.payment_id:
+            self.folio_ids = self.payment_id.folio_ids
+        elif self.statement_line_id:
+            self.folio_ids = self.statement_line_id.folio_ids
+        else:
+            self.folio_ids = False
+
+    @api.depends("folio_line_ids")
+    def _compute_origin_agency_id(self):
+        """
+        Compute the origin agency of the account move line,
+        if the line has multiple agencies in origin,
+        (p.e. nights with different agencies in origin),
+        the first one is returned (REVIEW: is this correct?)
+        """
+        self.origin_agency_id = False
+        for line in self:
+            agencies = line.mapped("folio_line_ids.origin_agency_id")
+            if agencies:
+                line.origin_agency_id = agencies[0]
