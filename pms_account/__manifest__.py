831ceb18d6bd232a1d680b8ff40259bcac2dc1f1
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2022-04-12 12:29:44 -0500

    [IMP] pms_account (#141)

--- pms_account/__manifest__.py
+++ pms_account/__manifest__.py
@@ -6,11 +6,11 @@
     "name": "Property Management System",
     "summary": "Manage the accounting aspects of your properties",
     "version": "14.0.1.0.0",
-    "development_status": "Alpha",
+    "development_status": "Beta",
     "category": "Generic Modules/Property Management System",
     "website": "https://github.com/OCA/pms",
     "author": "Commit [Sun], Open Source Integrators, Odoo Community Association (OCA)",
-    "maintainers": ["eantones"],
+    "maintainers": ["max3903"],
     "license": "AGPL-3",
     "depends": [
         "account",
