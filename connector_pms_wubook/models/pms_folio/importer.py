8e7da2d33c2fb9f3f2222f97d01a27182428f9a1
Darío Lodeiros <dario@commitsun.com>
2022-04-16 09:28:06 +0200

    [IMP]connector_pms_wubook: Tax included adapter

--- connector_pms_wubook/models/pms_folio/importer.py
+++ connector_pms_wubook/models/pms_folio/importer.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, SUPERUSER_ID
+from odoo import _
 
 from odoo.addons.component.core import Component
 
