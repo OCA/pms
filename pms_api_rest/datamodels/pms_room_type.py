70ed4a86bbdba524710bd807d63d03ee0e403654
braisab <braisterbutalino@gmail.com>
2022-05-06 17:41:52 +0200

    [IMP]pms_api_rest: added short_name in room datamodel and service and added default_code in room_type datamodel and service

--- pms_api_rest/datamodels/pms_room_type.py
+++ pms_api_rest/datamodels/pms_room_type.py
@@ -15,3 +15,4 @@ class PmsRoomTypeInfo(Datamodel):
     id = fields.Integer(required=False, allow_none=True)
     name = fields.String(required=False, allow_none=True)
     pms_property_ids = fields.List(fields.Integer(), required=False)
+    defaultCode = fields.String(required=False, allow_none=True)
