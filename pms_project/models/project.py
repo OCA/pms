630a347d302bbaa9d6ccc42046d2ff2fc9f79d8e
Jorge Luis Juarez <jorge.juarez@casai.com>
2022-05-17 18:44:52 -0500

    [Feature] link project with properties

---
+++ pms_project/models/project.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2022 Casai (https://www.casai.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class Project(models.Model):
+    _inherit = "project.project"
+
+    property_id = fields.Many2one("pms.property")
