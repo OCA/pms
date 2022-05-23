6e1f66b6665e98c59b1ede103686369a9a23c6fc
Jorge Luis Juarez <jorge.juarez@casai.com>
2022-05-23 12:28:17 -0500

    Improve web template (#69)
    * Improve web template
    
    * Separate header, improve section 1
    
    * Website amenities
    
    * Separate templates
    
    * Separate templates II
    
    * Separate templates III
    
    * Fix: reduce lines lenght

--- connector_guesty/controllers/website.py
+++ connector_guesty/controllers/website.py
@@ -7,6 +7,18 @@ from odoo.http import request
 
 from odoo.addons.pms_website.controllers.website import Website
 
+DEMO_PICTURE_01 = "{}/pictures/14056/{}/{}.jpg".format(
+    "https://dl6dfy55q2gns.cloudfront.net",
+    "69af29bdd2d3b7621ca12d007fa640d1464437c7",
+    "69af29bdd2d3b7621ca12d007fa640d1464437c",
+)
+
+DEMO_PICTURE_02 = "{}/pictures/14056/{}/{}.jpg".format(
+    "https://dl6dfy55q2gns.cloudfront.net",
+    "25e4599fe67112c1daab3981b897566bfd34612f",
+    "25e4599fe67112c1daab3981b897566bfd34612",
+)
+
 
 class CasaiWebsite(Website):
     @http.route(
@@ -23,3 +35,18 @@ class CasaiWebsite(Website):
             "connector_guesty.property_casai_website",
             self._prepare_property_values(pms_property, category, search, **kwargs),
         )
+
+    def _prepare_property_values(self, pms_property, category, search, **kwargs):
+        rs = super()._prepare_property_values(pms_property, category, search, **kwargs)
+        rs["slider_01"] = {
+            "pictures": [
+                DEMO_PICTURE_01,
+                DEMO_PICTURE_02,
+                DEMO_PICTURE_01,
+                DEMO_PICTURE_02,
+                DEMO_PICTURE_01,
+                DEMO_PICTURE_02,
+            ]
+        }
+        rs["section_id"] = {"picture": DEMO_PICTURE_01}
+        return rs
