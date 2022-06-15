228eac4bb3cc46ed845e95a7d662d8dfd65e1eac
Darío Lodeiros <dario@commitsun.com>
2022-06-15 12:50:17 +0200

    [ADD]pms_api_rest: extra beds service

---
+++ pms_api_rest/datamodels/pms_extra_bed.py
@@ -0,0 +1,20 @@
+from marshmallow import fields
+
+from odoo.addons.datamodel.core import Datamodel
+
+
+class PmsExtraBedSearchParam(Datamodel):
+    _name = "pms.extra.beds.search.param"
+    id = fields.Integer(required=False, allow_none=True)
+    name = fields.String(required=False, allow_none=True)
+    pmsPropertyId = fields.Integer(required=True, allow_none=False)
+    dateFrom = fields.String(required=False, allow_none=True)
+    dateTo = fields.String(required=False, allow_none=True)
+
+
+class PmsExtraBedInfo(Datamodel):
+    _name = "pms.extra.bed.info"
+    id = fields.Integer(required=False, allow_none=True)
+    name = fields.String(required=False, allow_none=True)
+    dailyLimitConfig = fields.Integer(required=False, allow_none=True)
+    dailyLimitAvail = fields.Integer(required=False, allow_none=True)
