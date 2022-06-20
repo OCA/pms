d3c6db5c7577c5ad3c659b8e44ce05cc655de7db
Darío Lodeiros <dario@commitsun.com>
2022-06-20 18:48:31 +0200

    [ADD]pms_api_rest: reservation_lines get and patch services

--- pms_api_rest/services/__init__.py
+++ pms_api_rest/services/__init__.py
@@ -5,6 +5,7 @@ from . import pms_calendar_service
 from . import pms_partner_service
 
 from . import pms_reservation_service
+from . import pms_reservation_line_service
 from . import pms_property_service
 from . import pms_login_service
 from . import pms_pricelist_service
