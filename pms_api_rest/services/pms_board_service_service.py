855638539f019833bfa1715b3307cd28acd778a5
Darío Lodeiros <dario@commitsun.com>
2022-06-22 10:13:24 +0200

    [ADD]pms_api_rest: boards services

---
+++ pms_api_rest/services/pms_board_service_service.py
@@ -0,0 +1,91 @@
+from odoo import _
+from odoo.exceptions import MissingError
+
+from odoo.addons.base_rest import restapi
+from odoo.addons.base_rest_datamodel.restapi import Datamodel
+from odoo.addons.component.core import Component
+
+
+class PmsBoardServiceService(Component):
+    _inherit = "base.rest.service"
+    _name = "pms.board.service.service"
+    _usage = "board-service"
+    _collection = "pms.services"
+
+    @restapi.method(
+        [
+            (
+                [
+                    "/",
+                ],
+                "GET",
+            )
+        ],
+        input_param=Datamodel("pms.board.service.search.param"),
+        output_param=Datamodel("pms.board.service.info", is_list=True),
+        auth="jwt_api_pms",
+    )
+    def get_board_services(self, board_services_search_param):
+        domain = []
+        if board_services_search_param.name:
+            domain.append(("name", "like", board_services_search_param.name))
+        if board_services_search_param.ids:
+            domain.append(("id", "in", board_services_search_param.ids))
+        if board_services_search_param.roomTypeId:
+            domain.append(
+                ("pms_room_type_id", "=", board_services_search_param.roomTypeId)
+            )
+        if board_services_search_param.pmsPropertyId:
+            domain.extend(
+                [
+                    "|",
+                    (
+                        "pms_property_ids",
+                        "in",
+                        board_services_search_param.pmsPropertyId,
+                    ),
+                    ("pms_property_ids", "=", False),
+                ]
+            )
+
+        result_board_services = []
+        PmsBoardServiceInfo = self.env.datamodels["pms.board.service.info"]
+        for board_service in self.env["pms.board.service.room.type"].search(
+            domain,
+        ):
+            result_board_services.append(
+                PmsBoardServiceInfo(
+                    id=board_service.id,
+                    name=board_service.pms_board_service_id.display_name,
+                    roomTypeId=board_service.pms_room_type_id.id,
+                    amount=board_service.amount,
+                )
+            )
+        return result_board_services
+
+    @restapi.method(
+        [
+            (
+                [
+                    "/<int:board_service_id>",
+                ],
+                "GET",
+            )
+        ],
+        output_param=Datamodel("pms.board.service.info", is_list=False),
+        auth="jwt_api_pms",
+    )
+    def get_board_service(self, board_service_id):
+        board_service = self.env["pms.board.service.room.type"].search(
+            [("id", "=", board_service_id)]
+        )
+        if board_service:
+            PmsBoardServiceInfo = self.env.datamodels["pms.board.service.info"]
+            return PmsBoardServiceInfo(
+                id=board_service.id,
+                name=board_service.pms_board_service_id.display_name,
+                roomTypeId=board_service.pms_room_type_id.id,
+                amount=board_service.amount,
+            )
+        else:
+            raise MissingError(_("Amenity Type not found"))
