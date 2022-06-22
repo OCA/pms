a4a15d05041f7ace7f1a63eea43687a17d1bac2c
Darío Lodeiros <dario@commitsun.com>
2022-06-22 10:13:24 +0200

    [ADD]pms_api_rest: boards services

---
+++ pms_api_rest/datamodels/pms_board_service.py
@@ -0,0 +1,19 @@
+from marshmallow import fields
+
+from odoo.addons.datamodel.core import Datamodel
+
+
+class PmsBoardServiceSearchParam(Datamodel):
+    _name = "pms.board.service.search.param"
+    ids = fields.List(fields.Integer(required=False, allow_none=True))
+    name = fields.String(required=False, allow_none=True)
+    pmsPropertyId = fields.Integer(required=False, allow_none=True)
+    roomTypeId = fields.Integer(required=False, allow_none=True)
+
+
+class PmsBoardServiceInfo(Datamodel):
+    _name = "pms.board.service.info"
+    id = fields.Integer(required=True, allow_none=False)
+    name = fields.String(required=True, allow_none=False)
+    roomTypeId = fields.Integer(required=True, allow_none=False)
+    amount = fields.Float(required=False, allow_none=False)
