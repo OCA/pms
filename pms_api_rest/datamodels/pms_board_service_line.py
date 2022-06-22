fde3bbe350706e3c11d8bbfc6d1ee5eeac358381
Darío Lodeiros <dario@commitsun.com>
2022-06-22 10:13:24 +0200

    [ADD]pms_api_rest: boards services

---
+++ pms_api_rest/datamodels/pms_board_service_line.py
@@ -0,0 +1,17 @@
+from marshmallow import fields
+
+from odoo.addons.datamodel.core import Datamodel
+
+
+class PmsBoardServiceLineSearchParam(Datamodel):
+    _name = "pms.board.service.line.search.param"
+    pmsBoarServiceId = fields.Integer(required=True, allow_none=False)
+
+
+class PmsBoardServiceLineInfo(Datamodel):
+    _name = "pms.board.service.line.info"
+    id = fields.Integer(required=True, allow_none=False)
+    name = fields.String(required=True, allow_none=False)
+    boardServiceId = fields.Integer(required=True, allow_none=False)
+    productId = fields.Integer(required=True, allow_none=False)
+    amount = fields.Float(required=False, allow_none=False)
