dbfe4bdbbdf39cc14652496abd379428e14f22cb
braisab <braisterbutalino@gmail.com>
2022-06-23 21:39:28 +0200

    [IMP]pms_api_rest: added services and datamodels for get and patch checkin_partners

--- pms_api_rest/services/__init__.py
+++ pms_api_rest/services/__init__.py
@@ -9,5 +9,9 @@ from . import pms_property_service
 from . import pms_login_service
 from . import pms_pricelist_service
 from . import pms_availability_plan_service
+from . import pms_id_categories_service
+from . import res_country_services
+from . import res_partner_category_services
+from . import res_city_zip_service
 from . import pms_room_type_class_service
 from . import pms_ubication_service
