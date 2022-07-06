8393e7a04c5e80efef3586c24bb0bca4c9564e04
braisab <braisterbutalino@gmail.com>
2022-07-06 20:11:25 +0200

    [REF]pms_api_rest: api rest routes and datamodel names refactor

--- pms_api_rest/datamodels/pms_room_type.py
+++ pms_api_rest/datamodels/pms_room_type.py
@@ -7,13 +7,13 @@ class PmsRoomTypeSearchParam(Datamodel):
     _name = "pms.room.type.search.param"
     id = fields.Integer(required=False, allow_none=True)
     name = fields.String(required=False, allow_none=True)
-    pms_property_ids = fields.List(fields.Integer(), required=False)
+    pmsPropertyIds = fields.List(fields.Integer(), required=False)
 
 
 class PmsRoomTypeInfo(Datamodel):
     _name = "pms.room.type.info"
     id = fields.Integer(required=False, allow_none=True)
     name = fields.String(required=False, allow_none=True)
-    pms_property_ids = fields.List(fields.Integer(), required=False)
+    pmsPropertyIds = fields.List(fields.Integer(), required=False)
     defaultCode = fields.String(required=False, allow_none=True)
     price = fields.Float(required=False, allow_none=True)
