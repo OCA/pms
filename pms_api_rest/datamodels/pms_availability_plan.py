f4a1d1deae920af15819114fdf2fd092de9fa4a1
braisab <braisterbutalino@gmail.com>
2022-07-06 20:11:25 +0200

    [REF]pms_api_rest: api rest routes and datamodel names refactor

--- pms_api_rest/datamodels/pms_availability_plan.py
+++ pms_api_rest/datamodels/pms_availability_plan.py
@@ -7,4 +7,4 @@ class PmsAvailabilityPlanInfo(Datamodel):
     _name = "pms.availability.plan.info"
     id = fields.Integer(required=False, allow_none=True)
     name = fields.String(required=False, allow_none=True)
-    pms_property_ids = fields.List(fields.Integer(required=False, allow_none=True))
+    pmsPropertyIds = fields.List(fields.Integer(required=False, allow_none=True))
