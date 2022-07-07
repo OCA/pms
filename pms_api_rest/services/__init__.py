6349c2dc010d53f106e9f3d6ad17ef4d56d966c7
braisab <braisterbutalino@gmail.com>
2022-07-07 09:58:18 +0200

    [REF]pms_api_rest: rename fields from services to service

--- pms_api_rest/services/__init__.py
+++ pms_api_rest/services/__init__.py
@@ -1,6 +1,6 @@
 from . import pms_folio_service
 from . import pms_room_service
-from . import pms_room_type_services
+from . import pms_room_type_service
 from . import pms_calendar_service
 from . import pms_partner_service
 
@@ -11,8 +11,8 @@ from . import pms_login_service
 from . import pms_pricelist_service
 from . import pms_availability_plan_service
 from . import pms_id_categories_service
-from . import res_country_services
-from . import res_partner_category_services
+from . import res_country_service
+from . import res_partner_category_service
 from . import res_city_zip_service
 from . import pms_room_type_class_service
 from . import pms_ubication_service
