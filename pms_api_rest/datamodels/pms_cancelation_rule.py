e5ad7974c8cf97a63ca19c9b42e13d53ac3df85e
Sara <saralago126@gmail.com>
2022-07-07 15:52:22 +0200

    [IMP] pms_api_rest: add cancelation rule and sale channel services

---
+++ pms_api_rest/datamodels/pms_cancelation_rule.py
@@ -0,0 +1,14 @@
+from marshmallow import fields
+
+from odoo.addons.datamodel.core import Datamodel
+
+
+class PmsCancelationRuleSearchParam(Datamodel):
+    _name = "pms.cancelation.rule.search.param"
+    pricelistId = fields.Integer(required=False, allow_none=True)
+    pmsPropertyId = fields.String(required=False, allow_none=True)
+
+class PmsCancelationRuleInfo(Datamodel):
+    _name = "pms.cancelation.rule.info"
+    id = fields.Integer(required=True, allow_none=False)
+    name = fields.String(required=True, allow_none=False)
