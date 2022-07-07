5aa649381e4dcf382a6329ef5fc93894f52c43dd
Darío Lodeiros <dario@commitsun.com>
2022-07-07 16:14:39 +0200

    [IMP]pms_api_rest: del id field in search datamodels

--- pms_api_rest/datamodels/pms_board_service.py
+++ pms_api_rest/datamodels/pms_board_service.py
@@ -5,7 +5,6 @@ from odoo.addons.datamodel.core import Datamodel
 
 class PmsBoardServiceSearchParam(Datamodel):
     _name = "pms.board.service.search.param"
-    ids = fields.List(fields.Integer(required=False, allow_none=True))
     name = fields.String(required=False, allow_none=True)
     pmsPropertyId = fields.Integer(required=False, allow_none=True)
     roomTypeId = fields.Integer(required=False, allow_none=True)
