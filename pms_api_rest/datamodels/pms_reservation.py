d1061b39c85765ac2c03e0c7010b21585fdc9b60
Darío Lodeiros <dario@commitsun.com>
2022-07-07 13:10:16 +0200

    [RFC]pms_api_rest: Replace subdict folio by reservation datamodel and service

--- pms_api_rest/datamodels/pms_reservation.py
+++ pms_api_rest/datamodels/pms_reservation.py
@@ -1,3 +1,4 @@
+from sre_parse import State
 from marshmallow import fields
 
 from odoo.addons.datamodel.core import Datamodel
@@ -6,24 +7,46 @@ from odoo.addons.datamodel.core import Datamodel
 class PmsReservationInfo(Datamodel):
     _name = "pms.reservation.info"
     id = fields.Integer(required=False, allow_none=True)
-    partner = fields.String(required=False, allow_none=True)
     name = fields.String(required=False, allow_none=True)
-    checkin = fields.String(required=False, allow_none=True)
-    checkout = fields.String(required=False, allow_none=True)
-    roomTypeId = fields.Integer(required=False, allow_none=True)
-    roomTypeName = fields.String(required=False, allow_none=True)
-    preferredRoomName = fields.String(required=False, allow_none=True)
-    preferredRoomId = fields.Integer(required=False, allow_none=True)
-    priceTotal = fields.Float(required=False, allow_none=True)
-    priceOnlyServices = fields.Float(required=False, allow_none=True)
-    priceOnlyRoom = fields.Float(required=False, allow_none=True)
-    pricelistName = fields.String(required=False, allow_none=True)
-    pricelistId = fields.Integer(required=False, allow_none=True)
-    services = fields.List(fields.Dict(required=False, allow_none=True))
-    messages = fields.List(fields.Dict(required=False, allow_none=True))
+    folioId = fields.Integer(required=False, allow_none=True)
+    folioSequence = fields.Integer(required=False, allow_none=True)
+    partnerName = fields.String(required=False, allow_none=True)
     pmsPropertyId = fields.Integer(required=False, allow_none=True)
     boardServiceId = fields.Integer(required=False, allow_none=True)
-    boardServiceName = fields.String(required=False, allow_none=True)
-    channelTypeId = fields.Integer(required=False, allow_none=True)
+    saleChannelId = fields.Integer(required=False, allow_none=True)
+    agencyId = fields.Integer(required=False, allow_none=True)
+
+    checkin = fields.String(required=False, allow_none=True)
+    checkout = fields.String(required=False, allow_none=True)
+    arrivalHour = fields.String(required=False, allow_none=True)
+    departureHour = fields.String(required=False, allow_none=True)
+    roomTypeId = fields.Integer(required=False, allow_none=True)
+    preferredRoomId = fields.Integer(required=False, allow_none=True)
+    pricelistId = fields.Integer(required=False, allow_none=True)
+
     adults = fields.Integer(required=False, allow_none=True)
     overbooking = fields.Boolean(required=False, allow_none=True)
+    externalReference = fields.String(required=False, allow_none=True)
+    state = fields.String(required=False, allow_none=True)
+    children = fields.Integer(required=False, allow_none=True)
+    readyForCheckin = fields.Boolean(required=False, allow_none=True)
+    allowedCheckout = fields.Boolean(required=False, allow_none=True)
+    isSplitted = fields.Boolean(required=False, allow_none=True)
+    pendingCheckinData = fields.Integer(required=False, allow_none=True)
+    createDate = fields.String(required=False, allow_none=True)
+    segmentationId = fields.Integer(required=False, allow_none=True)
+    cancellationPolicyId = fields.Integer(required=False, allow_none=True)
+    toAssign = fields.Boolean(required=False, allow_none=True)
+    reservationType = fields.String(required=False, allow_none=True)
+
+    priceTotal = fields.Float(required=False, allow_none=True)
+    discount = fields.Float(required=False, allow_none=True)
+    commissionAmount = fields.Float(required=False, allow_none=True)
+    commissionPercent = fields.Float(required=False, allow_none=True)
+    priceOnlyServices = fields.Float(required=False, allow_none=True)
+    priceOnlyRoom = fields.Float(required=False, allow_none=True)
+    pendingAmount = fields.Float(required=False, allow_none=True)
+
+    # TODO: Refact
+    #services = fields.List(fields.Dict(required=False, allow_none=True))
+    #messages = fields.List(fields.Dict(required=False, allow_none=True))
