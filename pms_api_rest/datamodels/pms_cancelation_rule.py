9198c26fab0ba3c1b06a0987159360801620429b
Darío Lodeiros <dario@commitsun.com>
2022-07-07 19:01:51 +0200

    [FIX]pms_api_rest: Agency without image

--- pms_api_rest/datamodels/pms_cancelation_rule.py
+++ pms_api_rest/datamodels/pms_cancelation_rule.py
@@ -8,6 +8,7 @@ class PmsCancelationRuleSearchParam(Datamodel):
     pricelistId = fields.Integer(required=False, allow_none=True)
     pmsPropertyId = fields.String(required=False, allow_none=True)
 
+
 class PmsCancelationRuleInfo(Datamodel):
     _name = "pms.cancelation.rule.info"
     id = fields.Integer(required=True, allow_none=False)
