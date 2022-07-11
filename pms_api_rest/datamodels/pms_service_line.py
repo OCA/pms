f1cc74dbd3ba4ed3d5b949b1dcf4ffc70e75f410
Sara <saralago126@gmail.com>
2022-07-11 17:02:27 +0200

    [RFC]pms_api_rest: refactor in datamodels and services

--- pms_api_rest/datamodels/pms_service_line.py
+++ pms_api_rest/datamodels/pms_service_line.py
@@ -7,7 +7,7 @@ class PmsServiceLineInfo(Datamodel):
     _name = "pms.service.line.info"
     id = fields.Integer(required=False, allow_none=True)
     isBoardService = fields.Boolean(required=False, allow_none=True)
-    productId = fields.Integer(required=False,allow_none=True)
+    productId = fields.Integer(required=False, allow_none=True)
     date = fields.String(required=False, allow_none=True)
     priceUnit = fields.Float(required=False, allow_none=True)
     priceTotal = fields.Float(required=False, allow_none=True)
