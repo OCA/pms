64fc69639b10aa6361d75eacbacb8207d3c5db0a
Darío Lodeiros <dario@commitsun.com>
2022-07-21 19:26:21 +0200

    [RFC]pms_api_rest: Clean fields in service line datamodel

--- pms_api_rest/datamodels/pms_service_line.py
+++ pms_api_rest/datamodels/pms_service_line.py
@@ -6,9 +6,7 @@ from odoo.addons.datamodel.core import Datamodel
 class PmsServiceLineInfo(Datamodel):
     _name = "pms.service.line.info"
     id = fields.Integer(required=False, allow_none=True)
-    isBoardService = fields.Boolean(required=False, allow_none=True)
-    productId = fields.Integer(required=False, allow_none=True)
     date = fields.String(required=False, allow_none=True)
     priceUnit = fields.Float(required=False, allow_none=True)
-    priceTotal = fields.Float(required=False, allow_none=True)
     discount = fields.Float(required=False, allow_none=True)
+    quantity = fields.Integer(required=False, allow_none=True)
