042af6fcd52c7ba41ae36a906ae85fa2a422ba40
braisab <braisterbutalino@gmail.com>
2022-07-22 14:08:19 +0200

    [IMP]pms_api_rest: added min stay and quota in demo users

--- pms_api_rest/models/res_users.py
+++ pms_api_rest/models/res_users.py
@@ -7,16 +7,20 @@ class ResUsers(models.Model):
     availability_rule_field_ids = fields.Many2many(
         string="Availability Rules",
         help="Configurable availability rules",
-        default=lambda self: self._get_default_avail_rule_fields(),
+        default=lambda self: self._default_avail_rule_fields(),
         comodel_name="ir.model.fields",
         relation="ir_model_fields_res_users_rel",
         column1="ir_model_fields",
         column2="res_users",
-
     )
 
-    def _get_default_avail_rule_fields(self):
-        default_avail_rule_fields = self.env['ir.model.fields'].search([('model_id', '=', 'pms.availability.plan.rule'), ('name', 'in', ('min_stay', 'quota'))])
+    def _default_avail_rule_fields(self):
+        default_avail_rule_fields = self.env["ir.model.fields"].search(
+            [
+                ("model_id", "=", "pms.availability.plan.rule"),
+                ("name", "in", ("min_stay", "quota")),
+            ]
+        )
         if default_avail_rule_fields:
             return default_avail_rule_fields.ids
         else:
