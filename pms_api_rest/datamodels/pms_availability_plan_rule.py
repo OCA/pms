f6f8ddc8766322a5ccc97e93191bd8fe60d46aef
braisab <braisterbutalino@gmail.com>
2022-08-10 19:52:56 +0200

    [IMP]: changes in avail rules and pricelist items services and datamodels for create/write

--- pms_api_rest/datamodels/pms_availability_plan_rule.py
+++ pms_api_rest/datamodels/pms_availability_plan_rule.py
@@ -1,12 +1,13 @@
 from marshmallow import fields
 
 from odoo.addons.datamodel.core import Datamodel
+from odoo.addons.datamodel.fields import NestedModel
 
 
 class PmsAvailabilityPlanRuleSearchParam(Datamodel):
     _name = "pms.availability.plan.rule.search.param"
-    dateFrom = fields.String(required=True, allow_none=False)
-    dateTo = fields.String(required=True, allow_none=False)
+    dateFrom = fields.String(required=False, allow_none=False)
+    dateTo = fields.String(required=False, allow_none=False)
     pmsPropertyId = fields.Integer(required=True, allow_none=False)
 
 
@@ -25,3 +26,8 @@ class PmsAvailabilityPlanRuleInfo(Datamodel):
     quota = fields.Integer(required=False, allow_none=True)
     maxAvailability = fields.Integer(required=False, allow_none=True)
     pmsPropertyId = fields.Integer(required=False, allow_none=True)
+
+
+class PmsAvailabilityPlanRulesInfo(Datamodel):
+    _name = "pms.availability.plan.rules.info"
+    availabilityPlanRules = fields.List(NestedModel("pms.availability.plan.rule.info"))
