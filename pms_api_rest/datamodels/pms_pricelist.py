5780584f4ef68a25a42fb53f071ce6230254c8ba
Darío Lodeiros <dario@commitsun.com>
2022-09-01 17:12:36 +0200

    [IMP]pms_api_rest: get pricelists improvements (refact + saleChannelId param)

--- pms_api_rest/datamodels/pms_pricelist.py
+++ pms_api_rest/datamodels/pms_pricelist.py
@@ -3,6 +3,14 @@ from marshmallow import fields
 from odoo.addons.datamodel.core import Datamodel
 
 
+class PmsPricelistSearch(Datamodel):
+    _name = "pms.pricelist.search"
+
+    pmsPropertyId = fields.Integer(required=False, allow_none=True)
+    pmsPropertyIds = fields.List(fields.Integer(), required=False)
+    saleChannelId = fields.Integer(required=False, allow_none=True)
+
+
 class PmsPricelistInfo(Datamodel):
     _name = "pms.pricelist.info"
     id = fields.Integer(required=False, allow_none=True)
@@ -10,3 +18,4 @@ class PmsPricelistInfo(Datamodel):
     cancelationRuleId = fields.Integer(required=False, allow_none=True)
     defaultAvailabilityPlanId = fields.Integer(required=False, allow_none=True)
     pmsPropertyIds = fields.List(fields.Integer(required=False, allow_none=True))
+    saleChannelIds = fields.List(fields.Integer(required=False, allow_none=True))
