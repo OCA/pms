0eb65fc9e12b8568dfd9c272b7c7a226f196620d
Sara <saralago126@gmail.com>
2022-09-13 14:49:14 +0200

    [IMP] pms_api_rest: add account_journals service and remove get_payment_methods in properties service

--- pms_api_rest/datamodels/pms_account_journal.py
+++ pms_api_rest/datamodels/pms_account_journal.py
@@ -3,6 +3,11 @@ from marshmallow import fields
 from odoo.addons.datamodel.core import Datamodel
 
 
+class PmsAccountJournalSearchParam(Datamodel):
+    _name = "pms.account.journal.search.param"
+    pmsPropertyId = fields.Integer(required=False, allow_none=False)
+
+
 class PmsAccountJournalInfo(Datamodel):
     _name = "pms.account.journal.info"
     id = fields.Integer(required=False, allow_none=True)
