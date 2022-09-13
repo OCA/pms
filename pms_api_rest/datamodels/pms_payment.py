ca15e3d09c66a5f705f06dac0d80764038de497e
Sara <saralago126@gmail.com>
2022-09-13 14:54:28 +0200

    [IMP] pms_api_rest: add get for payments and invoices in partner service

---
+++ pms_api_rest/datamodels/pms_payment.py
@@ -0,0 +1,12 @@
+from marshmallow import fields
+
+from odoo.addons.datamodel.core import Datamodel
+
+
+class PmsPaymentInfo(Datamodel):
+    _name = "pms.payment.info"
+    id = fields.Integer(required=False, allow_none=True)
+    amount = fields.Float(required=False, allow_none=True)
+    journalId = fields.Integer(required=False, allow_none=True)
+    date = fields.String(required=False, allow_none=True)
+    memo = fields.String(required=False, allow_none=True)
