a6c530e8375767d4b3c8d7cebdef8115e8cd7030
Darío Lodeiros <dario@commitsun.com>
2022-09-29 20:00:39 +0200

    [ADD]l10n_es_SII: and refactoring pms documents

--- pms_l10n_es/models/res_partner.py
+++ pms_l10n_es/models/res_partner.py
@@ -147,3 +147,4 @@ class ResPartner(models.Model):
                     record.with_context({"ignore_vat_update": True}).write(
                         {"vat": vat_with_code}
                     )
+
---
+++ pms_l10n_es_SII/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Set automatically SII fields from Checkin document Partners
