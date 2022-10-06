777dba97911e21e29d3212ce973ff5a251e7807e
miguelpadin <miguel.padin.dominguez@gmail.com>
2022-10-06 10:40:21 +0200

    [IMP] pms-api-rest: booking engine folio service & room closure reasons

---
+++ pms_api_rest/datamodels/pms_room_closure_reason.py
@@ -0,0 +1,12 @@
+from marshmallow import fields
+
+from odoo.addons.datamodel.core import Datamodel
+
+class PmsRoomClosureReasonInfo(Datamodel):
+    _name = "pms.room.closure.reason.info"
+    id = fields.Integer(required=False, allow_none=True)
+    name = fields.String(required=False, allow_none=True)
+    description = fields.String(required=False, allow_none=True)
+
+
+
