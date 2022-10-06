c1b21117f15dc0456cd2de5af78864128d5053ab
miguelpadin <miguel.padin.dominguez@gmail.com>
2022-10-06 10:40:21 +0200

    [IMP] pms-api-rest: booking engine folio service & room closure reasons

---
+++ pms_api_rest/services/pms_room_closure_reason_service.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+
+from odoo import _, fields
+from odoo.exceptions import MissingError
+from odoo.osv import expression
+
+from odoo.addons.base_rest import restapi
+from odoo.addons.base_rest_datamodel.restapi import Datamodel
+from odoo.addons.component.core import Component
+
+
+class PmsClosureReasonService(Component):
+    _inherit = "base.rest.service"
+    _name = "pms.closure.reason.service"
+    _usage = "room-closure-reasons"
+    _collection = "pms.services"
+
+    @restapi.method(
+        [
+            (
+                [
+                    "/",
+                ],
+                "GET",
+            )
+        ],
+        output_param=Datamodel("pms.room.closure.reason.info", is_list=True),
+        auth="jwt_api_pms",
+    )
+    def get_closure_reasons(self):
+        closure_reasons = []
+        PmsRoomClosureReasonInfo = self.env.datamodels["pms.room.closure.reason.info"]
+        for cl in self.env['room.closure.reason'].search([]):
+            closure_reasons.append(
+                PmsRoomClosureReasonInfo(
+                    id=cl.id,
+                    name=cl.name,
+                    description=cl.description
+
+            )
+            )
+        return closure_reasons
