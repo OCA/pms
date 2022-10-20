6946f41e4c47ce33995628594551ac8cc5bfae81
miguelpadin <miguel.padin.dominguez@gmail.com>
2022-10-20 10:33:59 +0200

    [FIX] pms-api-rest: fix patch reservation update

--- pms_api_rest/datamodels/pms_report.py
+++ pms_api_rest/datamodels/pms_report.py
@@ -2,6 +2,7 @@ from marshmallow import fields
 
 from odoo.addons.datamodel.core import Datamodel
 
+
 class PmsReportInfo(Datamodel):
     _name = "pms.report.info"
     pdf = fields.String(required=False, allow_none=True)
