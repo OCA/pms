b53fe2cea489142ae915111b08e5738d9504e1af
Darío Lodeiros <dario@commitsun.com>
2022-10-30 07:44:40 +0100

    [ADD]pms_api_rest: invoicing folios services

--- pms_api_rest/datamodels/pms_invoice.py
+++ pms_api_rest/datamodels/pms_invoice.py
@@ -4,13 +4,16 @@ from odoo.addons.datamodel.core import Datamodel
 from odoo.addons.datamodel.fields import NestedModel
 
 
-class PmsAccountMoveInfo(Datamodel):
-    _name = "pms.account.move.info"
+class PmsAccountInvoiceInfo(Datamodel):
+    _name = "pms.account.info"
     id = fields.Integer(required=False, allow_none=True)
     amount = fields.Float(required=False, allow_none=True)
     name = fields.String(required=False, allow_none=True)
     date = fields.String(required=False, allow_none=True)
     paymentState = fields.String(required=False, allow_none=True)
     state = fields.String(required=False, allow_none=True)
+    # partnerName??, is not enought partnerId?
     partnerName = fields.String(required=False, allow_none=True)
+    partnerId = fields.Int(required=False, allow_none=True)
     moveLines = fields.List(NestedModel("pms.invoice.line.info"))
+    saleLines = fields.List(NestedModel("pms.folio.sale.line.info"))
