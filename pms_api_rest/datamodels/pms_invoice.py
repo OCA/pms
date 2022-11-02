babf8adb5c5955b89c7dadde4557975df2c2182e
braisab <braisterbutalino@gmail.com>
2022-11-02 19:10:08 +0100

    [FIX]14.0-pms_api_rest: services and datamodels for create/update invoices

--- pms_api_rest/datamodels/pms_invoice.py
+++ pms_api_rest/datamodels/pms_invoice.py
@@ -5,15 +5,17 @@ from odoo.addons.datamodel.fields import NestedModel
 
 
 class PmsAccountInvoiceInfo(Datamodel):
-    _name = "pms.account.info"
+    _name = "pms.invoice.info"
     id = fields.Integer(required=False, allow_none=True)
     amount = fields.Float(required=False, allow_none=True)
     name = fields.String(required=False, allow_none=True)
     date = fields.String(required=False, allow_none=True)
     paymentState = fields.String(required=False, allow_none=True)
     state = fields.String(required=False, allow_none=True)
-    # partnerName??, is not enought partnerId?
+    # REVIEW: partnerName??, is not enought partnerId?
     partnerName = fields.String(required=False, allow_none=True)
-    partnerId = fields.Int(required=False, allow_none=True)
+    partnerId = fields.Integer(required=False, allow_none=True)
     moveLines = fields.List(NestedModel("pms.invoice.line.info"))
     saleLines = fields.List(NestedModel("pms.folio.sale.line.info"))
+    narration = fields.String(required=False, allow_none=True)
+    portalUrl = fields.String(required=False, allow_none=True)
