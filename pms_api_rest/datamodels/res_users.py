721d43c641c18f49b2b6f2f502fd9a2ccecfff39
Sara Lago <saralago126@gmail.com>
2022-11-02 20:16:19 +0100

    [IMP] pms_api_rest: add image in users datamodel

--- pms_api_rest/datamodels/res_users.py
+++ pms_api_rest/datamodels/res_users.py
@@ -7,3 +7,5 @@ class PmsResUsersInfo(Datamodel):
     _name = "res.users.info"
     id = fields.Integer(required=False, allow_none=True)
     name = fields.String(required=False, allow_none=True)
+    userImageBase64 = fields.String(required=False, allow_none=True)
+
