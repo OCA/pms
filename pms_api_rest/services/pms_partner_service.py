dcb4a55d0ecb2b5497bd3cbe164fecca1fdeaf9c
Darío Lodeiros <dario@commitsun.com>
2022-11-04 13:38:44 +0100

    [RFC]pms_api_rest: payments to transactions

--- pms_api_rest/services/pms_partner_service.py
+++ pms_api_rest/services/pms_partner_service.py
@@ -216,23 +216,24 @@ class PmsPartnerService(Component):
                 "GET",
             )
         ],
-        output_param=Datamodel("pms.payment.info", is_list=True),
+        output_param=Datamodel("pms.transaction.info", is_list=True),
         auth="jwt_api_pms",
     )
     def get_partner_payments(self, partner_id):
         partnerPayments = self.env["account.payment"].search(
             [("partner_id", "=", partner_id)]
         )
-        PmsPaymentInfo = self.env.datamodels["pms.payment.info"]
+        PmsTransactiontInfo = self.env.datamodels["pms.transaction.info"]
         payments = []
         for payment in partnerPayments:
             payments.append(
-                PmsPaymentInfo(
+                PmsTransactiontInfo(
                     id=payment.id,
                     amount=round(payment.amount, 2),
                     journalId=payment.journal_id.id,
                     date=payment.date.strftime("%d/%m/%Y"),
                     reference=payment.ref,
+                    transactionTypeCode=payment.pms_api_transaction_type,
                 )
             )
         return payments
