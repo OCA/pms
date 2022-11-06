605a77aa7784960d95415a3fa4767c33987b7c0d
Darío Lodeiros <dario@commitsun.com>
2022-11-06 11:13:48 +0100

    [RFC]pms_api_rest: user unique report datamodel

--- pms_api_rest/datamodels/pms_report.py
+++ pms_api_rest/datamodels/pms_report.py
@@ -3,6 +3,14 @@ from marshmallow import fields
 from odoo.addons.datamodel.core import Datamodel
 
 
-class PmsReportInfo(Datamodel):
-    _name = "pms.report.info"
-    pdf = fields.String(required=False, allow_none=True)
+class PmsReportSearchParam(Datamodel):
+    _name = "pms.report.search.param"
+    dateFrom = fields.String(required=False, allow_none=True)
+    dateTo = fields.String(required=False, allow_none=True)
+    pmsPropertyId = fields.Integer(required=False, allow_none=True)
+
+
+class PmsTransactionReportOutput(Datamodel):
+    _name = "pms.report"
+    fileName = fields.String(required=False, allow_none=True)
+    binary = fields.String(required=False, allow_none=True)
