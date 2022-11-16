e625304a61405568a91b041c0bc367551a1ce8d0
Darío Lodeiros <dario@commitsun.com>
2022-11-16 13:41:56 +0100

    [IMP]pms: improvemente workflow with invoice downpayment modifications

--- pms/models/account_payment.py
+++ pms/models/account_payment.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo import _, api, fields, models
+from odoo import api, fields, models
 
 
 class AccountPayment(models.Model):
@@ -103,173 +103,85 @@ class AccountPayment(models.Model):
                     }
                 )
 
-    # Business methods
-
-    # def modify(self):
-    #     self.cancel()
-    #     vals = {
-    #         "journal_id": self.journal_id,
-    #         "partner_id": self.partner_id,
-    #         "amount": self.amount,
-    #         "payment_date": self.payment_date,
-    #         "communication": self.communication,
-    #         "state": "draft",
-    #     }
-    #     self.update(vals)
-    #     self.with_context({"ignore_notification_post": True}).post()
-    #     self._compute_folio_amount()
-    #     if self.folio_id:
-    #         msg = _("Payment %s modified: \n") % (self.communication)
-    #         if self.save_amount and self.save_amount != self.amount:
-    #             msg += _("Amount from %s to %s %s \n") % (
-    #                 self.save_amount,
-    #                 self.amount,
-    #                 self.currency_id.symbol,
-    #             )
-    #         if self.save_date and self.save_date != self.payment_date:
-    #             msg += _("Date from %s to %s \n") % (self.save_date, self.payment_date)
-    #         if self.save_journal_id and self.save_journal_id != self.journal_id.id:
-    #             msg += _("Journal from %s to %s") % (
-    #                 self.env["account.journal"].browse(self.save_journal_id).name,
-    #                 self.journal_id.name,
-    #             )
-    #         self.folio_id.message_post(subject=_("Payment"), body=msg)
-
-    # def delete(self):
-    #     msg = False
-    #     if self.folio_id:
-    #         msg = _("Deleted payment: %s %s ") % (self.amount, self.currency_id.symbol)
-    #     self.cancel()
-    #     self.move_name = ""
-    #     self.unlink()
-    #     if msg:
-    #         self.folio_id.message_post(subject=_("Payment Deleted"), body=msg)
+    def action_draft(self):
+        for payment in self:
+            if payment._check_has_downpayment_invoice(payment):
+                downpayment_invoices = payment.reconciled_invoice_ids.filtered(
+                    lambda inv: inv._is_downpayment()
+                )
+                downpayment_invoices._reverse_moves(cancel=True)
+        return super(AccountPayment, self).action_draft()
 
     def action_post(self):
         res = super(AccountPayment, self).action_post()
         for payment in self:
-            if (
-                self.folio_ids
-                and self.company_id.pms_invoice_downpayment_policy != "no"
-                and not self.journal_id.avoid_autoinvoice_downpayment
-            ):
-                checkout_ref = max(self.folio_ids.mapped("last_checkout"))
-                if (
+            if self._check_is_downpayment_to_invoice(payment):
+                partner_id = (
+                    payment.partner_id.id or self.env.ref("pms.various_pms_partner").id
+                )
+                self._create_downpayment_invoice(
+                    payment=payment,
+                    partner_id=partner_id,
+                    payment_type=payment.partner_type,
+                )
+        return res
+
+    @api.model
+    def _check_is_downpayment_to_invoice(self, payment):
+        checkout_ref = max(self.folio_ids.mapped("last_checkout"))
+        if (
+            payment.folio_ids
+            and payment.partner_type == "customer"
+            and not self.journal_id.avoid_autoinvoice_downpayment
+            and (
+                (
                     self.company_id.pms_invoice_downpayment_policy == "all"
                     and payment.date < checkout_ref
-                ) or (
+                )
+                or (
                     self.company_id.pms_invoice_downpayment_policy
                     == "checkout_past_month"
                     and checkout_ref.month > payment.date.month
                     and checkout_ref.year >= payment.date.year
-                ):
-                    partner_id = (
-                        payment.partner_id.id
-                        or self.env.ref("pms.various_pms_partner").id
-                    )
-                    if payment.payment_type == "inbound":
-                        invoice_wizard = self.env["folio.advance.payment.inv"].create(
-                            {
-                                "partner_invoice_id": partner_id,
-                                "advance_payment_method": "fixed",
-                                "fixed_amount": payment.amount,
-                            }
-                        )
-                        move = invoice_wizard.with_context(
-                            active_ids=self.folio_ids.ids,
-                            return_invoices=True,
-                        ).create_invoices()
-                        move.action_post()
-                        move_lines = move.line_ids.filtered(
-                            lambda line: line.account_id.user_type_id.type
-                            in ("receivable", "payable")
-                        )
-                        payment_lines = payment.move_id.line_ids.filtered(
-                            lambda line: line.account_id == move_lines.account_id
-                        )
-                        if not move_lines.reconciled:
-                            (payment_lines + move_lines).reconcile()
-                    else:
-                        # We try to revert the advance payment invoice
-                        advance_invoices = self.env["account.move"].search(
-                            [
-                                ("folio_ids", "in", self.folio_ids.ids),
-                                ("state", "=", "posted"),
-                                ("payment_state", "=", "paid"),
-                                ("move_type", "=", "out_invoice"),
-                            ]
-                        )
-                        advance_invoices = advance_invoices.filtered(
-                            lambda inv: any(
-                                [
-                                    line.folio_line_ids.is_downpayment
-                                    for line in inv.invoice_line_ids
-                                ]
-                            )
-                        )
-                        if advance_invoices:
-                            match_inv = advance_invoices.filtered(
-                                lambda inv: round(inv.amount_total, 2)
-                                == round(payment.amount, 2)
-                            )
-                            move_reversal = (
-                                self.env["account.move.reversal"]
-                                .with_context(
-                                    active_model="account.move",
-                                    active_ids=match_inv.ids
-                                    if match_inv
-                                    else advance_invoices.ids,
-                                )
-                                .create(
-                                    {
-                                        "date": fields.Date.today(),
-                                        "reason": _("Deposit return"),
-                                        "refund_method": "refund",
-                                    }
-                                )
-                            )
-                            move_reversal.reverse_moves()
-                            move = move_reversal.new_move_ids
-                            invoice_line = move.invoice_line_ids.filtered(
-                                lambda l: not l.display_type
-                            )
-                            move.write(
-                                {
-                                    "invoice_line_ids": [
-                                        (
-                                            1,
-                                            invoice_line.id,
-                                            {
-                                                "price_unit": payment.amount,
-                                            },
-                                        )
-                                    ],
-                                }
-                            )
-                            move_lines = move.line_ids.filtered(
-                                lambda line: line.account_id.user_type_id.type
-                                in ("receivable", "payable")
-                            )
-                            payment_lines = payment.move_id.line_ids.filtered(
-                                lambda line: line.account_id == move_lines.account_id
-                            )
-                            move.sudo().action_post()
-                            if not move_lines.reconciled:
-                                (payment_lines + move_lines).reconcile()
-        return res
+                )
+            )
+        ):
+            return True
+        return False
 
-    # def modify_payment(self):
-    #     self.ensure_one()
-    #     view_form_id = self.env.ref("pms.account_payment_view_form_folio").id
-    #     # moves = self.mapped('move_ids.id')
-    #     return {
-    #         "name": _("Payment"),
-    #         "view_type": "form",
-    #         "views": [(view_form_id, "form")],
-    #         "view_mode": "tree,form",
-    #         "res_model": "account.payment",
-    #         "target": "new",
-    #         "init_mode": "edit",
-    #         "type": "ir.actions.act_window",
-    #         "res_id": self.id,
-    #     }
+    @api.model
+    def _check_has_downpayment_invoice(self, payment):
+        if (
+            payment.folio_ids
+            and payment.partner_type == "customer"
+            and payment.reconciled_invoice_ids.filtered(
+                lambda inv: inv._is_downpayment()
+            )
+        ):
+            return True
+        return False
+
+    def _create_downpayment_invoice(self, payment, partner_id, payment_type):
+        invoice_wizard = self.env["folio.advance.payment.inv"].create(
+            {
+                "partner_invoice_id": partner_id,
+                "advance_payment_method": "fixed",
+                "fixed_amount": payment.amount,
+            }
+        )
+        move = invoice_wizard.with_context(
+            active_ids=self.folio_ids.ids,
+            return_invoices=True,
+        ).create_invoices()
+        if payment_type == "outbound":
+            move.action_switch_invoice_into_refund_credit_note()
+        move.action_post()
+        move_lines = move.line_ids.filtered(
+            lambda line: line.account_id.user_type_id.type in ("receivable", "payable")
+        )
+        payment_lines = payment.move_id.line_ids.filtered(
+            lambda line: line.account_id == move_lines.account_id
+        )
+        if not move_lines.reconciled:
+            (payment_lines + move_lines).reconcile()
+        return move
