e64e4062e3ded490ae71b15b1acfc20d107430a1
Darío Lodeiros <dario@commitsun.com>
2022-11-30 19:22:32 +0100

    [ADD]pms_l10n_es_sii: add link to vat/aeat documents and document numbers partner

---
+++ pms_l10n_es_sii/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2009-2020 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "PMS AEAT SII Integration",
+    "author": "Commit [Sun], Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pms",
+    "category": "Generic Modules/Property Management System",
+    "version": "14.0.1.0.2",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_aeat_sii_oca",
+        "l10n_es_aeat_partner_identification",
+    ],
+    "data": ["data/pms_data.xml"],
+    "installable": True,
+}
