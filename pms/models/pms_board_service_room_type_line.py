8be6287863dfe4e5f4dd8e4bd7a497aac3537c80
Darío Lodeiros <dario@commitsun.com>
2022-12-07 11:29:36 +0100

    [IMP]pms: unique board_service_room_type by property

--- pms/models/pms_board_service_room_type_line.py
+++ pms/models/pms_board_service_room_type_line.py
@@ -1,6 +1,6 @@
 # Copyright 2017  Dario Lodeiros
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class PmsBoardServiceRoomTypeLine(models.Model):
@@ -16,15 +16,13 @@ class PmsBoardServiceRoomTypeLine(models.Model):
         comodel_name="pms.board.service.room.type",
         ondelete="cascade",
     )
-    pms_property_ids = fields.Many2many(
-        string="Properties",
-        help="Properties with access to the element;"
+    pms_property_id = fields.Many2one(
+        string="Property",
+        help="Property with access to the element;"
         " if not set, all properties can access",
         comodel_name="pms.property",
-        relation="pms_board_service_room_type_line_pms_property_rel",
-        column1="pms_board_service_room_type_id",
-        column2="pms_property_id",
         check_pms_properties=True,
+        related="pms_board_service_room_type_id.pms_property_id",
     )
     product_id = fields.Many2one(
         string="Product",
@@ -34,41 +32,12 @@ class PmsBoardServiceRoomTypeLine(models.Model):
         check_pms_properties=True,
         domain="[('is_pms_available', '=', True)]",
     )
-    # TODO def default_amount "amount of service"
     amount = fields.Float(
         string="Amount",
         help="Price for this Board Service Room Type Line/Product",
-        default=0.0,
+        default=lambda self: self._default_amount(),
         digits=("Product Price"),
     )
 
-    @api.model
-    def create(self, vals):
-        properties = False
-        if "pms_board_service_room_type_id" in vals:
-            board_service = self.env["pms.board.service.room.type"].browse(
-                vals["pms_board_service_room_type_id"]
-            )
-            properties = board_service.pms_property_ids
-        if properties:
-            vals.update(
-                {
-                    "pms_property_ids": properties,
-                }
-            )
-        return super(PmsBoardServiceRoomTypeLine, self).create(vals)
-
-    def write(self, vals):
-        properties = False
-        if "pms_board_service_room_type_id" in vals:
-            board_service = self.env["pms.board.service.room.type"].browse(
-                vals["pms_board_service_room_type_id"]
-            )
-            properties = board_service.pms_property_ids
-        if properties:
-            vals.update(
-                {
-                    "pms_property_ids": properties,
-                }
-            )
-        return super(PmsBoardServiceRoomTypeLine, self).write(vals)
+    def _default_amount(self):
+        return self.product_id.list_price
