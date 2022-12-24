58896a9e2ec2ceae8f44e723c5f53b63c8742c0f
Darío Lodeiros <dario@commitsun.com>
2022-12-24 16:09:29 +0100

    [IMP]connector_pms: add pms_property to queue jobs

---
+++ connector_pms/models/queue_job.py
@@ -0,0 +1,28 @@
+# Copyright 2021 Eric Antones <eantones@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import logging
+
+from odoo import models, fields, api
+
+_logger = logging.getLogger(__name__)
+
+
+class QueueJob(models.Model):
+    _inherit = "queue.job"
+
+    pms_property_id = fields.Many2one(
+        comodel_name='pms.property',
+        string='Property',
+        store=True,
+    )
+
+    @api.depends('args')
+    def _compute_pms_property_id(self):
+        for rec in self:
+            if rec.args[1:2] and isinstance(
+                rec.args[1:2][0], type(self.env["pms.property"])
+            ):
+                rec.pms_property_id = rec.args[1:2][0]
+            else:
+                rec.pms_property_id = False
+
