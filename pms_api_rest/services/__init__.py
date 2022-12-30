bf41e566283f299f0f9ee132119c5b6332cdd79f
miguelpadin <miguel.padin.dominguez@gmail.com>
2022-12-30 13:18:21 +0100

    [FIX] pms-api_rest: add avail datamodel & service to inits

--- pms_api_rest/services/__init__.py
+++ pms_api_rest/services/__init__.py
@@ -40,3 +40,4 @@ from . import pms_account_payment_terms_service
 from . import pms_account_journal_service
 from . import pms_invoice_service
 from . import pms_notification_service
+from . import pms_avail_service
