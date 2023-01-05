5d6523939c122d355e48328ca5f10d2791d6ada0
braisab <braisterbutalino@gmail.com>
2023-01-05 17:40:01 +0100

    [IMP]pms-api_rest: GET reservations services with toAssign param

--- pms_api_rest/datamodels/pms_search_param.py
+++ pms_api_rest/datamodels/pms_search_param.py
@@ -8,3 +8,4 @@ class PmsSearchParam(Datamodel):
 
     pmsPropertyId = fields.Integer(required=False, allow_none=True)
     pmsPropertyIds = fields.List(fields.Integer(), required=False)
+    toAssign = fields.Boolean(required=False, allow_none=True)
