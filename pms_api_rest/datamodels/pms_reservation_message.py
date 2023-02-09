1c18f64035865925bacec14c80f7f76743c84b10
braisab <braisterbutalino@gmail.com>
2023-02-09 12:54:44 +0100

    [IMP]pms_api_rest: get_folio_reservation_messages reformated to get folio and reservation messages

--- pms_api_rest/datamodels/pms_reservation_message.py
+++ pms_api_rest/datamodels/pms_reservation_message.py
@@ -3,6 +3,7 @@ from marshmallow import fields
 from odoo.addons.datamodel.core import Datamodel
 from odoo.addons.datamodel.fields import NestedModel
 
+
 class PmsReservationMessageInfo(Datamodel):
     _name = "pms.reservation.message.info"
     reservationId = fields.Integer(required=False, allow_none=True)
@@ -11,10 +12,24 @@ class PmsReservationMessageInfo(Datamodel):
     subject = fields.String(required=False, allow_none=True)
     date = fields.String(required=False, allow_none=True)
     messageType = fields.String(required=False, allow_none=True)
+    authorImageBase64 = fields.String(required=False, allow_none=True)
+
 
 class PmsFolioMessageInfo(Datamodel):
     _name = "pms.folio.message.info"
-    folioId = fields.Integer(required=False, allow_none=True)
+    author = fields.String(required=False, allow_none=True)
+    message = fields.String(required=False, allow_none=True)
+    subject = fields.String(required=False, allow_none=True)
+    date = fields.String(required=False, allow_none=True)
+    messageType = fields.String(required=False, allow_none=True)
+    authorImageBase64 = fields.String(required=False, allow_none=True)
+
+
+class PmsMessageInfo(Datamodel):
+    _name = "pms.message.info"
+    folioMessages = fields.List(
+        NestedModel("pms.reservation.message.info"), required=False, allow_none=True
+    )
     reservationMessages = fields.List(
         NestedModel("pms.reservation.message.info"), required=False, allow_none=True
     )
