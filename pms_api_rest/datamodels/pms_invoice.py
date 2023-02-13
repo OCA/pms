4656867522a4022894d3ae5cc7760d0768d46549
Sara Lago <saralago126@gmail.com>
2023-02-13 13:02:14 +0100

    [IMP] pms_api_rest: add get service for invoices

--- pms_api_rest/datamodels/pms_invoice.py
+++ pms_api_rest/datamodels/pms_invoice.py
@@ -3,6 +3,11 @@ from marshmallow import fields
 from odoo.addons.datamodel.core import Datamodel
 from odoo.addons.datamodel.fields import NestedModel
 
+class PmsInvoiceSearchParam(Datamodel):
+    _name = "pms.invoice.search.param"
+    _inherit = "pms.rest.metadata"
+    id = fields.Integer(required=False, allow_none=True)
+    name = fields.String(required=False, allow_none=True)
 
 class PmsAccountInvoiceInfo(Datamodel):
     _name = "pms.invoice.info"
@@ -15,7 +20,8 @@ class PmsAccountInvoiceInfo(Datamodel):
     # REVIEW: partnerName??, is not enought partnerId?
     partnerName = fields.String(required=False, allow_none=True)
     partnerId = fields.Integer(required=False, allow_none=True)
-    moveLines = fields.List(NestedModel("pms.invoice.line.info"))
+    moveLines = fields.List(NestedModel("pms.invoice.line.info"), required=False, allow_none=True)
+    folioId = fields.Integer(required=False, allow_none=True)
     saleLines = fields.List(NestedModel("pms.folio.sale.line.info"))
     narration = fields.String(required=False, allow_none=True)
     portalUrl = fields.String(required=False, allow_none=True)
@@ -27,3 +33,10 @@ class PmsAccountInvoiceInfo(Datamodel):
     # REVIEW: originDownPaymentId Only input field to service to
     # create downpayment invoices from payments
     originDownPaymentId = fields.Integer(required=False, allow_none=True)
+
+class PmsInvoiceResults(Datamodel):
+    _name = "pms.invoice.results"
+    invoices = fields.List(NestedModel("pms.invoice.info"))
+    total = fields.Float(required=False, allow_none=True)
+    totalInvoices = fields.Integer(required=False, allow_none=True)
+
