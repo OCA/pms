bfb19a023dbdcc03938e653a04987fffb230a48f
Vicente <vicente@comunitea.com>
2023-02-23 13:30:27 +0100

    [ADD] pos_pms_link

---
+++ pos_pms_link/__manifest__.py
@@ -0,0 +1,48 @@
+##############################################################################
+#    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+#    Copyright (C) 2022 Comunitea Servicios Tecnológicos S.L. All Rights Reserved
+#    Vicente Ángel Gutiérrez <vicente@comunitea.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'POS PMS link',
+    'summary': 'Allows to use PMS reservations on the POS interface',
+    'version': "14.0.1.0.0",
+    'author': 'Comunitea Servicios Tecnológicos S.L.',
+    'website': "http://www.comunitea.com",
+    'license': 'AGPL-3',
+    "category": "Point of Sale",
+    'depends': [
+        'point_of_sale',
+        'pms',
+    ],
+    'data': [
+        'views/assets_common.xml',
+        'views/pms_service_line.xml',
+        'views/pos_order.xml',
+        'views/pos_config.xml',
+    ],
+    'demo': [],
+    'qweb': [
+        'static/src/xml/ReservationSelectionButton.xml',
+        'static/src/xml/Screens/ReservationListScreen/ReservationDetailsEdit.xml',
+        'static/src/xml/Screens/ReservationListScreen/ReservationLine.xml',
+        'static/src/xml/Screens/ReservationListScreen/ReservationListScreen.xml',
+        'static/src/xml/Screens/PaymentScreen/PaymentScreen.xml',
+    ],
+    'installable': True,
+}
---
+++ pos_pms_link/static/src/js/Screens/ReservationListScreen/ReservationDetailsEdit.js
@@ -0,0 +1,50 @@
+odoo.define('pos_pms_link.ReservationDetailsEdit', function(require) {
+    'use strict';
+
+    const { _t } = require('web.core');
+    const { getDataURLFromFile } = require('web.utils');
+    const PosComponent = require('point_of_sale.PosComponent');
+    const Registries = require('point_of_sale.Registries');
+
+    class ReservationDetailsEdit extends PosComponent {
+        constructor() {
+            super(...arguments);
+            const reservation = this.props.reservation;
+        }
+        mounted() {
+            this.env.bus.on('save-reservation', this, this.saveChanges);
+        }
+        willUnmount() {
+            this.env.bus.off('save-reservation', this);
+        }
+        /**
+         * Save to field `changes` all input changes from the form fields.
+         */
+        captureChange(event) {
+            this.changes[event.target.name] = event.target.value;
+        }
+        saveChanges() {
+            let processedChanges = {};
+            for (let [key, value] of Object.entries(this.changes)) {
+                if (this.intFields.includes(key)) {
+                    processedChanges[key] = parseInt(value) || false;
+                } else {
+                    processedChanges[key] = value;
+                }
+            }
+            if ((!this.props.reservation.name && !processedChanges.name) ||
+                processedChanges.name === '' ){
+                return this.showPopup('ErrorPopup', {
+                  title: _t('A Customer Name Is Required'),
+                });
+            }
+            processedChanges.id = this.props.reservation.id || false;
+            this.trigger('save-changes', { processedChanges });
+        }
+    }
+    ReservationDetailsEdit.template = 'ReservationDetailsEdit';
+
+    Registries.Component.add(ReservationDetailsEdit);
+
+    return ReservationDetailsEdit;
+});
