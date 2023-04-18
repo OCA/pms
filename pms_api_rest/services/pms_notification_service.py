df30835b31596bfbcfa1676b71e242c1689ea4c9
miguelpadin <miguel.padin.dominguez@gmail.com>
2023-04-18 09:29:22 +0200

    [IMP] pms_api_rest: number of unassigned reserv. service notification

--- pms_api_rest/services/pms_notification_service.py
+++ pms_api_rest/services/pms_notification_service.py
@@ -1,5 +1,6 @@
 import datetime
 
+from odoo import fields
 from odoo.addons.base_rest import restapi
 from odoo.addons.base_rest_datamodel.restapi import Datamodel
 from odoo.addons.component.core import Component
@@ -10,6 +11,33 @@ class PmsNotificationService(Component):
     _name = "pms.notification.service"
     _usage = "notifications"
     _collection = "pms.services"
+
+    @restapi.method(
+        [
+            (
+                [
+                    "/reservations-to-assign",
+                ],
+                "GET",
+            )
+        ],
+        input_param=Datamodel("pms.notification.search", is_list=False),
+        output_param=Datamodel("pms.notification.info", is_list=False),
+        auth="jwt_api_pms",
+        cors="*",
+    )
+    def get_reservations_to_assign_notifications(self, pms_notification_search):
+        num_reservation_ids_to_assign = self.env["pms.reservation"].search_count(
+            [
+                ("pms_property_id", "=", pms_notification_search.pmsPropertyId),
+                ("checkin", ">=", fields.Date.today()),
+                ("to_assign", "=", True),
+            ],
+        )
+        PmsNotificationInfo = self.env.datamodels["pms.notification.info"]
+        return PmsNotificationInfo(
+            numReservationsToAssign=num_reservation_ids_to_assign
+        )
 
     @restapi.method(
         [
