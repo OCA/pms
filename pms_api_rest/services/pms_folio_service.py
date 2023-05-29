14ae4cfdaa4103ba78a81737b5f5e90a72ee3083
braisab <braisterbutalino@gmail.com>
2023-05-29 13:51:04 +0200

    [IMP]pms-api_rest: added nights and numServices to reservation datamodel

--- pms_api_rest/services/pms_folio_service.py
+++ pms_api_rest/services/pms_folio_service.py
@@ -235,6 +235,7 @@ class PmsFolioService(Component):
                         "numServices": len(reservation.service_ids)
                         if reservation.service_ids
                         else 0,
+                        "overbooking": reservation.overbooking,
                     }
                 )
             result_folios.append(
@@ -484,6 +485,7 @@ class PmsFolioService(Component):
                             if reservation.service_ids
                             else 0,
                             toAssign=reservation.to_assign,
+                            overbooking=reservation.overbooking,
                         )
                     )
 
