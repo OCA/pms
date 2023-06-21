415bfa57b430038c13fbd130460daac2863bab68
Robin Keunen <robin@keunen.net>
2023-06-21 15:26:14 +0200

    [REF] fix precommit

--- pms/static/src/js/widgets/datepicker/locales/bootstrap-datepicker.de.js
+++ pms/static/src/js/widgets/datepicker/locales/bootstrap-datepicker.de.js
@@ -2,14 +2,49 @@
  * German translation for bootstrap-datepicker
  * Sam Zurcher <sam@orelias.ch>
  */
-;(function($){
-	$.fn.datepicker.dates['de'] = {
-		days: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"],
-		daysShort: ["Son", "Mon", "Die", "Mit", "Don", "Fre", "Sam", "Son"],
-		daysMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
-		months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
-		monthsShort: ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
-		today: "Heute",
-		weekStart: 1
-	};
-}(jQuery));
+(function ($) {
+    $.fn.datepicker.dates.de = {
+        days: [
+            "Sonntag",
+            "Montag",
+            "Dienstag",
+            "Mittwoch",
+            "Donnerstag",
+            "Freitag",
+            "Samstag",
+            "Sonntag",
+        ],
+        daysShort: ["Son", "Mon", "Die", "Mit", "Don", "Fre", "Sam", "Son"],
+        daysMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
+        months: [
+            "Januar",
+            "Februar",
+            "März",
+            "April",
+            "Mai",
+            "Juni",
+            "Juli",
+            "August",
+            "September",
+            "Oktober",
+            "November",
+            "Dezember",
+        ],
+        monthsShort: [
+            "Jan",
+            "Feb",
+            "Mär",
+            "Apr",
+            "Mai",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Okt",
+            "Nov",
+            "Dez",
+        ],
+        today: "Heute",
+        weekStart: 1,
+    };
+})(jQuery);
