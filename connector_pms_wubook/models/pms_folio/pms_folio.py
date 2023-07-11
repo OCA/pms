c88015c589a1a9d18ce0cbb4e2b9e910359790cf
Darío Lodeiros <dario@commitsun.com>
2023-07-11 17:44:14 +0200

    [IMP]pms_connector_wubook: add search folio&reservation by external_id

--- connector_pms_wubook/models/pms_folio/pms_folio.py
+++ connector_pms_wubook/models/pms_folio/pms_folio.py
@@ -1,7 +1,7 @@
 # Copyright 2021 Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PmsFolio(models.Model):
@@ -16,3 +16,17 @@ class PmsFolio(models.Model):
     reservation_origin_code = fields.Integer(
         string="Reservation Origin Code",
     )
+
+    @api.model
+    def _name_search(self, name="", args=None, operator="ilike", limit=100):
+        args = args or []
+        domain = []
+        if name:
+            domain = [
+                "|",
+                ("name", operator, name),
+                ("reservation_origin_code", operator, name),
+                ("channel_wubook_bind_ids.external_id", operator, name),
+            ]
+        folios = self.search(domain + args, limit=limit)
+        return folios.name_get()
