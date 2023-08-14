f3bba070fb9739f6badd98af3c7eb97ff6b57265
Darío Lodeiros <dario@commitsun.com>
2023-08-14 09:50:18 +0200

    [ADD]pms_api_rest: add otas parameter in get agency service

--- pms_api_rest/datamodels/pms_agency.py
+++ pms_api_rest/datamodels/pms_agency.py
@@ -6,6 +6,7 @@ from odoo.addons.datamodel.core import Datamodel
 class PmsAgencySearchParam(Datamodel):
     _name = "pms.agency.search.param"
     name = fields.String(required=False, allow_none=True)
+    otas = fields.Boolean(required=False, allow_none=True)
 
 
 class PmsAgencyInfo(Datamodel):
