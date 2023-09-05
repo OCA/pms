3a72499c27c77d68af8d1b1d5a62cca8db196d26
Sara Lago <saralago126@gmail.com>
2023-09-05 12:46:29 +0200

    [IMP] pms_api_rest: get_pending_checkin_reservations & get_pending_checkout_reservations in pms_dashboard_service

--- pms_api_rest/datamodels/pms_dashboard.py
+++ pms_api_rest/datamodels/pms_dashboard.py
@@ -5,15 +5,13 @@ from odoo.addons.datamodel.core import Datamodel
 
 class PmsDashboardPendingReservationsSearchParam(Datamodel):
     _name = "pms.dashboard.pending.reservations.search.param"
-    dateTo = fields.String(required=False, allow_none=True)
-    dateFrom = fields.String(required=False, allow_none=True)
+    date = fields.String(required=False, allow_none=True)
+    pmsPropertyId = fields.Integer(required=False, allow_none=True)
 
 
 class PmsDashboardPendingReservations(Datamodel):
     _name = "pms.dashboard.pending.reservations"
-    id = fields.Integer(required=True, allow_none=False)
-    state = fields.String(required=False, allow_none=True)
-    reservationType = fields.String(required=False, allow_none=True)
-    checkin = fields.String(required=False, allow_none=True)
+    pendingReservations = fields.Integer(required=False, allow_none=True)
+    completedReservations = fields.Integer(required=False, allow_none=True)
 
 
