15c7841b64f08b059a964ea68c54825cbe23d6c0
Sara Lago <saralago126@gmail.com>
2023-09-06 17:31:47 +0200

    [IMP] pms_api_rest: improve dashboard_service with sql statements

--- pms_api_rest/datamodels/pms_dashboard.py
+++ pms_api_rest/datamodels/pms_dashboard.py
@@ -3,15 +3,30 @@ from marshmallow import fields
 from odoo.addons.datamodel.core import Datamodel
 
 
-class PmsDashboardPendingReservationsSearchParam(Datamodel):
-    _name = "pms.dashboard.pending.reservations.search.param"
+class PmsDashboardSearchParam(Datamodel):
+    _name = "pms.dashboard.search.param"
     date = fields.String(required=False, allow_none=True)
     pmsPropertyId = fields.Integer(required=False, allow_none=True)
 
 
+class PmsDashboardRangeDatesSearchParam(Datamodel):
+    _name = "pms.dashboard.range.dates.search.param"
+    dateFrom = fields.String(required=False, allow_none=True)
+    dateTo = fields.String(required=False, allow_none=True)
+    pmsPropertyId = fields.Integer(required=False, allow_none=True)
+
+
+
 class PmsDashboardPendingReservations(Datamodel):
     _name = "pms.dashboard.pending.reservations"
-    pendingReservations = fields.Integer(required=False, allow_none=True)
-    completedReservations = fields.Integer(required=False, allow_none=True)
+    date = fields.String(required=False, allow_none=True)
+    pendingArrivalReservations = fields.Integer(required=False, allow_none=True)
+    completedArrivalReservations = fields.Integer(required=False, allow_none=True)
+    pendingDepartureReservations = fields.Integer(required=False, allow_none=True)
+    completedDepartureReservations = fields.Integer(required=False, allow_none=True)
 
 
+
+class PmsDashboardNumericResponse(Datamodel):
+    _name = "pms.dashboard.numeric.response"
+    value = fields.Float(required=False, allow_none=True)
