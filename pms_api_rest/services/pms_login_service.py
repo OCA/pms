4c9d28d09d26339175324941c1cdedbd9f1ce781
miguelpadin <miguel.padin.dominguez@gmail.com>
2023-09-11 11:05:19 +0200

    [IMP] pms-api-rest: add userFirstName to login service

--- pms_api_rest/services/pms_login_service.py
+++ pms_api_rest/services/pms_login_service.py
@@ -73,6 +73,7 @@ class PmsLoginService(Component):
             expirationDate=timestamp_expire_in_a_min,
             userId=user_record.id,
             userName=user_record.name,
+            userFirstName=user_record.firstname,
             userEmail=user_record.email if user_record.email else None,
             userPhone=user_record.phone if user_record.phone else None,
             defaultPropertyId=user_record.pms_property_id.id,
