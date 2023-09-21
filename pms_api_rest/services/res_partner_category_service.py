24afbf4c4d7e75000c476d3c8f03e14a1ff6309c
braisab <braisterbutalino@gmail.com>
2023-09-21 12:35:57 +0200

    [IMP]pms_api_rest: added user language to countries and segmentations

--- pms_api_rest/services/res_partner_category_service.py
+++ pms_api_rest/services/res_partner_category_service.py
@@ -24,7 +24,7 @@ class PmsPartnerCategoriesService(Component):
     def get_categories(self):
         result_categories = []
         ResPartnerCategoryInfo = self.env.datamodels["res.partner.category.info"]
-        for category in self.env["res.partner.category"].search([]):
+        for category in self.env["res.partner.category"].with_context(lang=self.env.user.lang).search([]):
             result_categories.append(
                 ResPartnerCategoryInfo(
                     id=category.id,
