292b1b889d7b91b1d7338cd1aacfe2801889c7b9
Sara Lago <saralago126@gmail.com>
2023-10-17 11:14:33 +0200

    [ADD] pms_api_rest: add rss_post service, datamodel and add feed_rss module in pms_api_rest depends

---
+++ pms_api_rest/services/feed_post_service.py
@@ -0,0 +1,39 @@
+from odoo.addons.base_rest import restapi
+from odoo.addons.base_rest_datamodel.restapi import Datamodel
+from odoo.addons.component.core import Component
+
+
+class PmsFeedRss(Component):
+    _inherit = "base.rest.service"
+    _name = "pms.feed.rss.service"
+    _usage = "feed-posts"
+    _collection = "pms.services"
+
+    @restapi.method(
+        [
+            (
+                [
+                    "/",
+                ],
+                "GET",
+            )
+        ],
+        output_param=Datamodel("feed.post.info", is_list=True),
+        auth="jwt_api_pms",
+    )
+    def get_feed_posts(self):
+        result_rss = []
+        PmsFeedRss = self.env.datamodels["feed.post.info"]
+        for rss in self.env["rss.post"].search([], limit=5, order='publish_date desc'):
+            result_rss.append(
+                PmsFeedRss(
+                    postId=rss.post_id,
+                    title=rss.title,
+                    link=rss.link,
+                    description=rss.description,
+                    publishDate=str(rss.publish_date),
+                    author=rss.author if rss.author else "",
+                    imageUrl=rss.image_url if rss.image_url else "",
+                )
+            )
+        return result_rss
