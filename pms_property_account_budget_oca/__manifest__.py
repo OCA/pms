47a742ebd4dc65421261143a33ea166111fdc04e
Darío Lodeiros <dario@commitsun.com>
2023-10-18 09:28:45 +0200

    [ADD]pms_property_account_budget_oca

---
+++ pms_property_account_budget_oca/__manifest__.py
@@ -0,0 +1,14 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "PMS Property Budgets Management Integration",
+    "version": "14.0.1.0.0",
+    "category": "Accounting",
+    "license": "LGPL-3",
+    "author": "Odoo S.A., Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pms",
+    "depends": ["account_budget_oca"],
+    "data": [
+        "views/account_budget_views.xml",
+    ],
+}
