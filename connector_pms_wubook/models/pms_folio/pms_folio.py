c60744bf6f6a0fe074be97f50df8422067607ccc
Darío Lodeiros <dario@commitsun.com>
2023-10-23 10:12:25 +0200

    [FIX]connector_pms_wubook: domain _name_search folio&reservation

--- connector_pms_wubook/models/pms_folio/pms_folio.py
+++ connector_pms_wubook/models/pms_folio/pms_folio.py
@@ -27,6 +27,7 @@ class PmsFolio(models.Model):
         if name:
             domain = [
                 "|",
+                "|",
                 ("name", operator, name),
                 ("reservation_origin_code", operator, name),
                 ("channel_wubook_bind_ids.external_id", operator, name),
