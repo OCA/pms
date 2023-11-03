7c43188b836696ef63f37d7b15b92effcb21ebce
Jose Luis Algara Toledo <osotranquilo@gmail.com>
2023-11-03 11:11:58 +0100

    [ADD] new module pms hr property

---
+++ pms_hr_property/models/hr_employee.py
@@ -0,0 +1,12 @@
+# Copyright 2023 OsoTranquilo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+
+from odoo import _, api, fields, models
+
+
+class HrEmployeeBase(models.AbstractModel):
+
+    _inherit = "hr.employee.base"
+
+    property_ids = fields.Many2many("pms.property", string="Workplaces asigned")
