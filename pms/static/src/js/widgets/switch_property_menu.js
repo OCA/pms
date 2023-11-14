d9f41648a5eb4a817e7966ae0243a06d8da14a68
Jose Luis Algara Toledo <osotranquilo@gmail.com>
2023-11-14 09:51:24 +0100

    [FIX] pre-commmit problems

--- pms/static/src/js/widgets/switch_property_menu.js
+++ pms/static/src/js/widgets/switch_property_menu.js
@@ -7,7 +7,7 @@ odoo.define("web.SwitchPmsMenu", function (require) {
      * of this widget, by displaying a dropdown menu in the systray.
      */
 
-    const { _t } = require('web.core');
+    const {_t} = require("web.core");
     var config = require("web.config");
     var session = require("web.session");
     var SystrayMenu = require("web.SystrayMenu");
@@ -45,8 +45,10 @@ odoo.define("web.SwitchPmsMenu", function (require) {
         willStart: function () {
             var self = this;
             this.allowed_company_ids = String(session.user_context.allowed_company_ids)
-            .split(',')
-            .map(function (id) {return parseInt(id);});
+                .split(",")
+                .map(function (id) {
+                    return parseInt(id);
+                });
             this.allowed_pms_property_ids = String(
                 session.user_context.allowed_pms_property_ids
             )
@@ -58,30 +60,36 @@ odoo.define("web.SwitchPmsMenu", function (require) {
                 session.user_pms_properties.allowed_pms_properties;
 
             // Clean not allowed company
-            var display_properties = []
-            var pms_propertyID = []
+            var display_properties = [];
+            var pms_propertyID = [];
             var alowed_properties = this.allowed_pms_property_ids.filter((val, ind) => {
-                return this.allowed_pms_property_ids.indexOf(val) === ind;});
+                return this.allowed_pms_property_ids.indexOf(val) === ind;
+            });
 
-            this.user_pms_properties.forEach(element => {
-                if (this.allowed_company_ids.includes(element[2])){
+            this.user_pms_properties.forEach((element) => {
+                if (this.allowed_company_ids.includes(element[2])) {
                     display_properties.push(element);
-                    pms_propertyID.push(element[0])
+                    pms_propertyID.push(element[0]);
+                } else if (alowed_properties.indexOf(element[0]) >= 0) {
+                    alowed_properties.splice(alowed_properties.indexOf(element[0]), 1);
                 }
-                else if (alowed_properties.indexOf(element[0]) >= 0) {
-                    alowed_properties.splice(alowed_properties.indexOf(element[0]),1)
-                };});
+            });
 
-            this.user_pms_properties = display_properties
-            this.user_pms_properties.sort(function(a,b){return a[1] > b[1] ? 1 : a[1] < b[1] ?-1 :0});
+            this.user_pms_properties = display_properties;
+            this.user_pms_properties.sort(function (a, b) {
+                return a[1] > b[1] ? 1 : a[1] < b[1] ? -1 : 0;
+            });
 
             // set permanent data
             session.user_context.allowed_pms_property_ids = alowed_properties;
             utils.set_cookie("pms_pids", alowed_properties);
 
             this.allowed_pms_property_ids = alowed_properties;
-            if (this.user_pms_properties.length != this.allowed_pms_property_ids.length) {
-                this.user_pms_properties.unshift([0,  _t('All propertys')])}
+            if (
+                this.user_pms_properties.length != this.allowed_pms_property_ids.length
+            ) {
+                this.user_pms_properties.unshift([0, _t("All propertys")]);
+            }
 
             this.current_pms_property = this.allowed_pms_property_ids[0];
             this.current_pms_property_name = _.find(
@@ -118,8 +126,9 @@ odoo.define("web.SwitchPmsMenu", function (require) {
             var pms_propertyID = dropdownItem.data("pms_property-id");
             var allowed_pms_property_ids = this.allowed_pms_property_ids;
             if (pms_propertyID === 0) {
-                for (let i=1; i < this.user_pms_properties.length; i++) {
-                    allowed_pms_property_ids.push(this.user_pms_properties[i][0]);}
+                for (let i = 1; i < this.user_pms_properties.length; i++) {
+                    allowed_pms_property_ids.push(this.user_pms_properties[i][0]);
+                }
             } else {
                 if (dropdownItem.find(".fa-square-o").length) {
                     // 1 enabled pms_property: Stay in single pms proeprty mode
@@ -173,8 +182,9 @@ odoo.define("web.SwitchPmsMenu", function (require) {
             var allowed_pms_property_ids = this.allowed_pms_property_ids;
             var current_pms_property_id = allowed_pms_property_ids[0];
             if (pms_propertyID === 0) {
-                for (let i=1; i < this.user_pms_properties.length; i++) {
-                    allowed_pms_property_ids.push(this.user_pms_properties[i][0]);}
+                for (let i = 1; i < this.user_pms_properties.length; i++) {
+                    allowed_pms_property_ids.push(this.user_pms_properties[i][0]);
+                }
             } else {
                 if (dropdownItem.find(".fa-square-o").length) {
                     allowed_pms_property_ids.push(pms_propertyID);
