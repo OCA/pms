7a43d2c6988e545c0d8a124ffb1190a163805c79
Darío Lodeiros <dario@commitsun.com>
2023-12-19 10:37:34 +0100

    [RFC]pms_hr_property: precommit green

--- pms_hr_property/__manifest__.py
+++ pms_hr_property/__manifest__.py
@@ -2,20 +2,20 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'PMS Hr Property',
-    'summary': """
+    "name": "PMS Hr Property",
+    "summary": """
         Adds to the employee the property on which he works.""",
-    'version': '14.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'OsoTranquilo,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/pms',
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "OsoTranquilo,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/pms",
     "category": "PMS/HR",
-    'depends': [
+    "depends": [
         "hr",
         "pms",
     ],
-    'data': [
-        'views/hr_employee_view.xml',
+    "data": [
+        "views/hr_employee_view.xml",
     ],
     "installable": True,
 }
