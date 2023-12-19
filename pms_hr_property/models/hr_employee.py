02b47e851434b3d26d9b94fe484c5720c517df22
Darío Lodeiros <dario@commitsun.com>
2023-12-19 10:37:34 +0100

    [RFC]pms_hr_property: precommit green

--- pms_hr_property/models/hr_employee.py
+++ pms_hr_property/models/hr_employee.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 
-from odoo import _, api, fields, models
+from odoo import fields, models
 
 
 class HrEmployeeBase(models.AbstractModel):
