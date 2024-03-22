05ee506de88de59c3fda672ff1d02b61bc19a394
braisab <braisterbutalino@gmail.com>
2024-03-22 18:48:33 +0100

    [IMP]14.0-pms_api_rest: origin input data field in checkin partner modal

---
+++ pms_api_rest/models/pms_checkin_partner.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+
+class PmsCheckinPartner(models.Model):
+    _inherit = 'pms.checkin.partner'
+
+    origin_input_data = fields.Selection([
+        ('wizard', 'Wizard'),
+        ('form', 'Form'),
+        ('regular_customer', 'Regular Customer'),
+        ('ocr', 'OCR'),
+        ('precheckin', 'Precheckin'),
+    ], string='Origin Input Data')
