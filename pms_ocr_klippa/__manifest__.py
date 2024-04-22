2016ee52f91d0adc27485199ead08c1ee7020a46
Darío Lodeiros <dario@commitsun.com>
2024-04-22 12:58:37 +0200

    [ADD]pms_ocr_klippa: ADD klippa OCR basic flow

---
+++ pms_ocr_klippa/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020-21 Jose Luis Algara (Alda Hotels <https://www.aldahotels.es>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "OCR Klippa",
+    "version": "14.0.1.0.1",
+    "author": "Commit [Sun], Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": True,
+    "category": "Generic Modules/Property Management System",
+    "website": "https://github.com/OCA/pms",
+    "depends": [
+        "pms_api_rest",
+    ],
+    "data": [
+        "data/pms_ocr_klippa_data.xml",
+        "views/res_partner_id_category_views.xml",
+    ],
+    "installable": True,
+}
