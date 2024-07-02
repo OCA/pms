0502bd317c4d8b2cc6fbe5e2651059bdb9cc5932
miguelpadin <miguel.padin.dominguez@gmail.com>
2024-07-02 16:54:36 +0200

    [IMP] pms-api-rest: add canDownloadIneReport to property datamodel

--- pms_api_rest/services/pms_property_service.py
+++ pms_api_rest/services/pms_property_service.py
@@ -90,6 +90,7 @@ class PmsPropertyService(Component):
                     companyPrivacyPolicy=privacy_policy
                     if prop.company_id.privacy_policy
                     else None,
+                    canDownloadIneReport=prop.ine_tourism_number and prop.ine_category_id,
                 )
             )
         return result_properties
@@ -161,6 +162,7 @@ class PmsPropertyService(Component):
                 if pms_property.company_id.privacy_policy
                 else None,
                 isUsedOCR=True if pms_property.ocr_checkin_supplier else False,
+                canDownloadIneReport=pms_property.ine_tourism_number and pms_property.ine_category_id,
             )
 
         return res
