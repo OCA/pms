c724551212ca051fe44dd32f93ee906c71aa4fc9
Darío Lodeiros <dario@commitsun.com>
2024-07-25 09:05:13 +0200

    [FIX]pms_api_rest: datamodels errors with params attributes

--- pms_api_rest/datamodels/pms_reservation_line.py
+++ pms_api_rest/datamodels/pms_reservation_line.py
@@ -16,14 +16,14 @@ class PmsReservationLineSearchParam(Datamodel):
 
 class PmsReservationLineInfo(Datamodel):
     _name = "pms.reservation.line.info"
-    id = fields.Integer(required=False, allow_none=False)
-    date = fields.String(required=False, allow_none=False)
-    price = fields.Float(required=False, allow_none=False)
-    discount = fields.Float(required=False, allow_none=False)
-    cancelDiscount = fields.Float(required=False, allow_none=False)
-    roomId = fields.Integer(required=False, allow_none=False)
-    reservationId = fields.Integer(required=False, allow_none=False)
-    pmsPropertyId = fields.Integer(required=False, allow_none=False)
+    id = fields.Integer(required=False, allow_none=True)
+    date = fields.String(required=False, allow_none=True)
+    price = fields.Float(required=False, allow_none=True)
+    discount = fields.Float(required=False, allow_none=True)
+    cancelDiscount = fields.Float(required=False, allow_none=True)
+    roomId = fields.Integer(required=False, allow_none=True)
+    reservationId = fields.Integer(required=False, allow_none=True)
+    pmsPropertyId = fields.Integer(required=False, allow_none=True)
     isReselling = fields.Boolean(required=False, allow_none=True)
     reservationType = fields.String(required=False, allow_none=True)
     state = fields.String(required=False, allow_none=True)
