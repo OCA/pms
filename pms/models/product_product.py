2cff2a75a3166098cf097befa3140a12a8ddf3b6
Darío Lodeiros <dario@commitsun.com>
2024-07-27 11:16:03 +0200

    [FIX]pms: fix children board get price

--- pms/models/product_product.py
+++ pms/models/product_product.py
@@ -18,33 +18,41 @@ class ProductProduct(models.Model):
         compute="_compute_room_type_id",
     )
 
+    @api.depends_context("consumption_date", "board_service_line_id")
     # pylint: disable=W8110
-    @api.depends_context("consumption_date")
     def _compute_product_price(self):
         super(ProductProduct, self)._compute_product_price()
 
+    @api.depends_context("consumption_date", "board_service_line_id")
     def _compute_board_price(self):
         pms_property_id = (
             self.env.context.get("property") or self.env.user.pms_property_ids.ids
         )
         for record in self:
             if self._context.get("board_service"):
-                record.board_price = (
-                    self.env["pms.board.service.room.type.line"]
-                    .search(
-                        [
-                            (
-                                "pms_board_service_room_type_id",
-                                "=",
-                                self._context.get("board_service"),
-                            ),
-                            ("product_id", "=", record.id),
-                            ("pms_property_id", "=", pms_property_id),
-                        ],
-                        limit=1,
+                if self._context.get("board_service_line_id"):
+                    record.board_price = (
+                        self.env["pms.board.service.room.type.line"]
+                        .browse(self._context.get("board_service_line_id"))
+                        .amount
                     )
-                    .amount
-                )
+                else:
+                    record.board_price = (
+                        self.env["pms.board.service.room.type.line"]
+                        .search(
+                            [
+                                (
+                                    "pms_board_service_room_type_id",
+                                    "=",
+                                    self._context.get("board_service"),
+                                ),
+                                ("product_id", "=", record.id),
+                                ("pms_property_id", "=", pms_property_id),
+                            ],
+                            limit=1,
+                        )
+                        .amount
+                    )
             else:
                 record.board_price = False
 
