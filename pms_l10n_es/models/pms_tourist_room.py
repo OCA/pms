3340d8fe7fb14aab48faf0928b73aa193f2794d3
irluidev <iramirez.spain@gmail.com>
2024-07-31 10:57:06 +0200

    [FIX] pms_tourism_room: fix calculation of tourist room in the property information card

--- pms_l10n_es/models/pms_tourist_room.py
+++ pms_l10n_es/models/pms_tourist_room.py
@@ -12,14 +12,16 @@ class PmsProperty(models.Model):
     total_tourist_rooms = fields.Integer(
         string="Tourist Rooms",
         help="Number of tourist rooms in the hotel.",
+        compute="_compute_total_rooms",
         store=True,
         default=lambda self: self._get_default_total_rooms(),
     )
 
     @api.depends("room_ids")
     def _compute_total_rooms(self):
-        for record in self:
-            record.total_rooms = len(record.room_ids)
+        target_rooms = self.env["pms.room"].search([("pms_property_id", "=", self.id)])
+        self.total_tourist_rooms = len(target_rooms)
+        return self.total_tourist_rooms
 
     @api.constrains("total_tourist_rooms")
     def _check_total_tourism_rooms(self):
@@ -33,10 +35,8 @@ class PmsProperty(models.Model):
 
     @api.model
     def _get_default_total_rooms(self):
-        first_property = self.env["pms.property"].search([], limit=1)
-        if first_property:
-            target_rooms = self.env["pms.room"].search(
-                [("pms_property_id", "=", first_property.id)]
-            )
-            return len(target_rooms)
+        target_rooms = self.env["pms.room"].search([("pms_property_id", "=", self.id)])
+        if target_rooms:
+            self.total_tourist_rooms = len(target_rooms)
+            return self.total_tourist_rooms
         return 0
