75a82a7e2727035f2fff129c3ff7e2bc050d7625
irluidev <iramirez.spain@gmail.com>
2024-08-09 17:21:11 +0200

    [FIX]pms_helpdesk_mgmt: adding validation of consistency between property and company

--- pms_helpdesk_mgmt/models/helpdesk_ticket.py
+++ pms_helpdesk_mgmt/models/helpdesk_ticket.py
@@ -2,7 +2,8 @@
 # Copyright (C) 2024 Oso Tranquilo <informatica@gmail.com>
 # Copyright (C) 2024 Consultores Hoteleros Integrales <www.aldahotels.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class PmsHelpdeskTicket(models.Model):
@@ -26,20 +27,26 @@ class PmsHelpdeskTicket(models.Model):
     @api.onchange("company_id")
     def _onchange_company_id(self):
         if self.company_id:
-            properties = (
-                self.env["pms.property"]
-                .sudo()
-                .search(
-                    [
-                        ("user_ids", "in", [self.env.user.id]),
-                        ("company_id", "=", self.company_id.id),
-                    ]
-                )
-            )
-            property_ids = properties.ids if properties else []
-            return {"domain": {"pms_property_id": [("id", "in", property_ids)]}}
+            allowed_pms_property_ids = self.env.user.get_active_property_ids()
+            return {
+                "domain": {"pms_property_id": [("id", "in", allowed_pms_property_ids)]}
+            }
         else:
             return {"domain": {"pms_property_id": []}}
+
+    @api.constrains("company_id", "pms_property_id")
+    def _check_property_company_consistency(self):
+        for record in self:
+            if (
+                record.pms_property_id
+                and record.pms_property_id.company_id != record.company_id
+            ):
+                raise UserError(
+                    _(
+                        "The selected property does not belong to the selected company. "
+                        "Please select a property that belongs to the correct company."
+                    )
+                )
 
     @api.onchange("pms_property_id")
     def _onchange_property_id(self):
