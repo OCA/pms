b58ffa703bcf690701ad713f5cd03b259ddd603e
Darío Lodeiros <dario@commitsun.com>
2024-10-22 10:00:38 +0200

    [FIX]connector_pms_wubook: temporal fix component issues with listener and adapter

--- connector_pms_wubook/models/pms_room_type/listener.py
+++ connector_pms_wubook/models/pms_room_type/listener.py
@@ -2,11 +2,9 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo.addons.component.core import Component
+# from odoo.addons.component.core import Component
 
 
-class ChannelWubookPmsRoomTypeListener(Component):
-    _name = "channel.wubook.pms.room.type.listener"
-    _inherit = "channel.wubook.listener"
-
-    _apply_on = "pms.room.type"
+# class ChannelWubookPmsRoomTypeListener(Component):
+#    _name = "channel.wubook.pms.room.type.listener"
+#    _inherit = "channel.wubook.listener"
