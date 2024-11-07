b8220fa103a5e31139216179924f202c75bea0bf
Darío Lodeiros <dario@commitsun.com>
2024-11-07 09:33:46 +0100

    [WIP]pms_api_rest: manifest 16

--- pms_api_rest/__manifest__.py
+++ pms_api_rest/__manifest__.py
@@ -3,7 +3,7 @@
     "author": "Commit [Sun], Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/pms",
     "category": "Generic Modules/Property Management System",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "pms",
