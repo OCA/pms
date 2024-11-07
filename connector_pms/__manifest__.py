d1b643d05daf37f0b575d80f8ef1a19dc8e351fb
Darío Lodeiros <dario@commitsun.com>
2024-11-07 13:10:03 +0100

    [IMP]connector_pms: pre-commit stuff

--- connector_pms/__manifest__.py
+++ connector_pms/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "PMS Connector",
     "summary": "Channel PMS connector Base",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "development_status": "Alpha",
     "category": "Connector",
