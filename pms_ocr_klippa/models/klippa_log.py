c3063af29505cfbae49451922ae1e98126d7fc10
Darío Lodeiros <dario@commitsun.com>
2025-02-01 18:08:27 +0100

    [MIG]pms_ocr_klippa: precommit updates

--- pms_ocr_klippa/models/klippa_log.py
+++ pms_ocr_klippa/models/klippa_log.py
@@ -26,11 +26,9 @@ class KlippaLog(models.Model):
         help="Back Image",
     )
     klippa_response = fields.Text(
-        string="Klippa Response",
         help="Response",
     )
     klippa_status = fields.Char(
-        string="Klippa Status",
         help="Klippa Status",
     )
     request_datetime = fields.Datetime(
@@ -42,55 +40,42 @@ class KlippaLog(models.Model):
         help="Response Date",
     )
     request_duration = fields.Float(
-        string="Request Duration",
         help="Request Duration",
     )
     mapped_duration = fields.Float(
-        string="Mapped Duration",
         help="Mapped Duration",
     )
     total_duration = fields.Float(
-        string="Total Duration",
         help="Total Duration",
     )
     endpoint = fields.Char(
-        string="Endpoint",
         help="Endpoint",
     )
     request_size = fields.Integer(
-        string="Request Size",
         help="Request Size",
     )
     response_size = fields.Integer(
-        string="Response Size",
         help="Response Size",
     )
     request_headers = fields.Text(
-        string="Request Headers",
         help="Request Headers",
     )
     request_url = fields.Char(
-        string="Request URL",
         help="Request URL",
     )
     service_response = fields.Text(
-        string="Resvice Response",
         help="Resvice Response",
     )
     final_status = fields.Char(
-        string="Final Status",
         help="Final Status",
     )
     error = fields.Text(
-        string="Error",
         help="Error",
     )
     nominatim_status = fields.Char(
-        string="Nominatim Status",
         help="Nominatim Status",
     )
     nominatim_response = fields.Text(
-        string="Nominatim Response",
         help="Nominatim Response",
     )
 
--- pms_ocr_klippa/models/res_partner_id_category.py
+++ pms_ocr_klippa/models/res_partner_id_category.py
@@ -4,9 +4,5 @@ from odoo import fields, models
 class ResPartnerIdCategory(models.Model):
     _inherit = "res.partner.id_category"
 
-    klippa_code = fields.Char(
-        string="Klippa Code",
-    )
-    klippa_subtype_code = fields.Char(
-        string="Klippa Subtype Code",
-    )
+    klippa_code = fields.Char()
+    klippa_subtype_code = fields.Char()
