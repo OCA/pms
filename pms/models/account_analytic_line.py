721f1c849527709baae7d2e59b052bd2e83fc45b
Omar (Comunitea) <omar@comunitea.com>
2025-02-03 15:14:06 +0100

    [IMP][FIX] pms: Views and scripts related to analytic account and model creation

--- pms/models/account_analytic_line.py
+++ pms/models/account_analytic_line.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class AccountAnalyticDistribution(models.Model):
@@ -10,6 +10,20 @@ class AccountAnalyticDistribution(models.Model):
         check_pms_properties=True,
         index=True,
     )
+
+    @api.model
+    def default_get(self, fields):
+        res = super().default_get(fields)
+        if "pms_property_id" in fields and self.env.context.get("default_analytic_distribution"):
+            distribution = self.env.context["default_analytic_distribution"]
+            if distribution.keys():
+                property_ids = self.env["pms.property"].search(
+                    [("analytic_account_id", "in", [int(x) for x in distribution.keys()])]
+                )
+                if property_ids:
+                    res["pms_property_id"] = property_ids[0].id
+                    res['company_id'] = property_ids[0].company_id.id
+        return res
 
     def _get_distribution(self, vals):
         pms_property_id = self.env.context.get("pms_property_id")
