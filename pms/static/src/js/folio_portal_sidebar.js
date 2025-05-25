9e3ee994f277118e833280604134e05f10f50970
DarioLodeiros <dariodafoz@gmail.com>
2025-05-25 10:33:59 +0200

    [FIX]pms: adatp folio portal to 16.0 with minimal features

--- pms/static/src/js/folio_portal_sidebar.js
+++ pms/static/src/js/folio_portal_sidebar.js
@@ -8,67 +8,143 @@ odoo.define("account.FolioPortalSidebar", function (require) {
 
     publicWidget.registry.FolioPortalSidebar = PortalSidebar.extend({
         selector: ".o_portal_folio_sidebar",
-        events: {
-            "click .o_portal_folio_print": "_onPrintFolio",
-        },
 
+        /**
+         * @class
+         */
+        init: function (parent, options) {
+            this._super.apply(this, arguments);
+            this.authorizedTextTag = ["em", "b", "i", "u"];
+            this.spyWatched = $('body[data-target=".navspy"]');
+        },
         /**
          * @override
          */
         start: function () {
             var def = this._super.apply(this, arguments);
-
-            var $folioHtml = this.$el.find("iframe#folio_html");
-            var updateIframeSize = this._updateIframeSize.bind(this, $folioHtml);
-
-            $(window).on("resize", updateIframeSize);
-
-            var iframeDoc =
-                $folioHtml[0].contentDocument || $folioHtml[0].contentWindow.document;
-            if (iframeDoc.readyState === "complete") {
-                updateIframeSize();
-            } else {
-                $folioHtml.on("load", updateIframeSize);
+            var $spyWatcheElement = this.$el.find('[data-id="portal_sidebar"]');
+            this._setElementId($spyWatcheElement);
+            // Nav Menu ScrollSpy
+            this._generateMenu();
+            // After signature, automatically open the popup for payment
+            if (
+                $.bbq.getState("allow_payment") === "yes" &&
+                this.$("#o_sale_portal_paynow").length
+            ) {
+                this.el.querySelector("#o_sale_portal_paynow").click();
+                $.bbq.removeState("allow_payment");
             }
-
             return def;
         },
 
         // --------------------------------------------------------------------------
-        // Handlers
-        // --------------------------------------------------------------------------
+        // Private
+        // ---------------------------------------------------------------------------
 
         /**
-         * Called when the iframe is loaded or the window is resized on customer portal.
-         * The goal is to expand the iframe height to display the full report without scrollbar.
+         * create an unique id and added as a attribute of spyWatched element
          *
          * @private
-         * @param {Object} $el: the iframe
+         * @param {String} prefix
+         * @param {Object} $el
+         *
          */
-        _updateIframeSize: function ($el) {
-            var $wrapwrap = $el.contents().find("div#wrapwrap");
-            // Set it to 0 first to handle the case where scrollHeight is too big for its content.
-            $el.height(0);
-            $el.height($wrapwrap[0].scrollHeight);
-
-            // Scroll to the right place after iframe resize
-            if (!utils.isValidAnchor(window.location.hash)) {
-                return;
-            }
-            var $target = $(window.location.hash);
-            if (!$target.length) {
-                return;
-            }
-            dom.scrollTo($target[0], {duration: 0});
+        _setElementId: function (prefix, $el) {
+            var id = _.uniqueId(prefix);
+            this.spyWatched.find($el).attr("id", id);
+            return id;
         },
         /**
+         * Generate the new spy menu
+         *
          * @private
-         * @param {MouseEvent} ev
+         *
          */
-        _onPrintFolio: function (ev) {
-            ev.preventDefault();
-            var href = $(ev.currentTarget).attr("href");
-            this._printIframeContent(href);
+        _generateMenu: function () {
+            var self = this,
+                lastLI = false,
+                lastUL = null,
+                $bsSidenav = this.$el.find(".bs-sidenav");
+
+            $(
+                "#quote_content [id^=quote_header_], #quote_content [id^=quote_]",
+                this.spyWatched
+            ).attr("id", "");
+            _.each(
+                this.spyWatched.find("#quote_content h2, #quote_content h3"),
+                function (el) {
+                    var id, text;
+                    switch (el.tagName.toLowerCase()) {
+                        case "h2":
+                            id = self._setElementId("quote_header_", el);
+                            text = self._extractText($(el));
+                            if (!text) {
+                                break;
+                            }
+                            lastLI = $("<li class='nav-item'>")
+                                .append(
+                                    $(
+                                        '<a class="nav-link" style="max-width: 200px;" href="#' +
+                                            id +
+                                            '"/>'
+                                    ).text(text)
+                                )
+                                .appendTo($bsSidenav);
+                            lastUL = false;
+                            break;
+                        case "h3":
+                            id = self._setElementId("quote_", el);
+                            text = self._extractText($(el));
+                            if (!text) {
+                                break;
+                            }
+                            if (lastLI) {
+                                if (!lastUL) {
+                                    lastUL = $("<ul class='nav flex-column'>").appendTo(
+                                        lastLI
+                                    );
+                                }
+                                $("<li class='nav-item'>")
+                                    .append(
+                                        $(
+                                            '<a class="nav-link" style="max-width: 200px;" href="#' +
+                                                id +
+                                                '"/>'
+                                        ).text(text)
+                                    )
+                                    .appendTo(lastUL);
+                            }
+                            break;
+                    }
+                    el.setAttribute("data-anchor", true);
+                }
+            );
+            this.trigger_up("widgets_start_request", {$target: $bsSidenav});
+        },
+        /**
+         * Extract text of menu title for sidebar
+         *
+         * @private
+         * @param {Object} $node
+         *
+         */
+        _extractText: function ($node) {
+            var self = this;
+            var rawText = [];
+            _.each($node.contents(), function (el) {
+                var current = $(el);
+                if ($.trim(current.text())) {
+                    var tagName = current.prop("tagName");
+                    if (
+                        _.isUndefined(tagName) ||
+                        (!_.isUndefined(tagName) &&
+                            _.contains(self.authorizedTextTag, tagName.toLowerCase()))
+                    ) {
+                        rawText.push($.trim(current.text()));
+                    }
+                }
+            });
+            return rawText.join(" ");
         },
     });
 });
