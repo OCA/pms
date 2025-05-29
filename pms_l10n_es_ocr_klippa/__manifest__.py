ec2f639160ea44f40772ab368858e4dd0f93e089
jesusvmayor <jesus@roomdoo.com>
2025-05-29 16:07:27 +0200

    [FIX]pms: Adapt partner id categories to new legal requirements.

---
+++ pms_l10n_es_ocr_klippa/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Spanish localization for OCR Klippa",
+    "version": "16.0.0.1.0",
+    "author": "Commit [Sun], Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Generic Modules/Property Management System",
+    "website": "https://github.com/OCA/pms",
+    "depends": [
+        "pms_l10n_es",
+        "pms_ocr_klippa",
+    ],
+    "data": [
+        "data/pms_ocr_klippa_data.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ pms_l10n_es_ocr_klippa/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Spanish localization for the klippa OCR
