e7d825725550241fed84d6b22d1c62f7bbcda4f2
jesusvmayor <jesus@roomdoo.com>
2025-12-09 11:49:02 +0100

    [REM]pms: Remove self bill functionality.

--- pms/models/pms_property.py
+++ pms/models/pms_property.py
@@ -979,11 +979,6 @@ class PmsProperty(models.Model):
         partner = self.env["res.partner"].browse(partner_invoice_id)
         # For simplified invoices
         if (
-            self.company_id.partner_id.id == partner.id
-            and self.company_id.self_billed_journal_id
-        ):
-            return self.company_id.self_billed_journal_id
-        if (
             not partner
             or partner.id == self.env.ref("pms.various_pms_partner").id
             or (
