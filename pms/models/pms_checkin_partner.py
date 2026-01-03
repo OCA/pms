ba8e74230e7f228eec186d1d9ea4763ad88966e5
DarioLodeiros <dariodafoz@gmail.com>
2026-01-03 09:26:24 +0100

    [ADD]pms: change related by compute in checkin name

--- pms/models/pms_checkin_partner.py
+++ pms/models/pms_checkin_partner.py
@@ -51,7 +51,11 @@ class PmsCheckinPartner(models.Model):
         comodel_name="pms.property",
         related="reservation_id.pms_property_id",
     )
-    name = fields.Char(help="Checkin partner name", related="partner_id.name")
+    name = fields.Char(
+        help="Checkin partner name",
+        compute="_compute_name",
+        store=True,
+    )
     email = fields.Char(
         string="E-mail",
         help="Checkin Partner Email",
@@ -344,11 +348,16 @@ class PmsCheckinPartner(models.Model):
                 else:
                     record.state = "precheckin"
 
-    @api.depends("partner_id")
+    @api.depends("partner_id", "firstname", "lastname", "lastname2")
     def _compute_name(self):
         for record in self:
-            if not record.name or record.partner_id.name:
+            if record.partner_id.name:
                 record.name = record.partner_id.name
+            else:
+                name = self.env["res.partner"]._get_computed_name(
+                    record.firstname, record.lastname, record.lastname2
+                )
+                record.name = name
 
     @api.depends("partner_id")
     def _compute_email(self):
