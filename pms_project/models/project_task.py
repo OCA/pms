2a2eeae4ab4c7ede03c318261e40cfe97ad23773
migration <migration@ursais.com>
2026-06-05 18:22:03 +0000

    [MIG] pms_project: Migration to 19.0

--- pms_project/models/project_task.py
+++ pms_project/models/project_task.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2022 Open Source Integrators
+# Copyright (C) 2022 Gray Matter Logic
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import fields, models
 
