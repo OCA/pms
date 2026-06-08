c1a8914b4370bfc7581e1f6a7f92393eee6268ac
Maxime Chambreuil <maxime.chambreuil@graymatterlogic.com>
2026-06-07 21:25:26 -0600

    [MIG] pms_website_sale: Migration to 19.0

--- pms_website_sale/__manifest__.py
+++ pms_website_sale/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright (c) 2021 Open Source Integrators
+# Copyright (c) 2021 Gray Matter Logic
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "PMS - Website Sale",
     "summary": "Allow online booking of your properties",
-    "version": "14.0.1.0.0",
-    "development_status": "Alpha",
+    "version": "19.0.1.0.0",
+    "development_status": "Beta",
     "category": "Generic Modules/Property Management System",
     "website": "https://github.com/OCA/pms",
-    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "author": "Gray Matter Logic, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["pms_website", "pms_sale", "website_sale"],
     "data": [
@@ -15,7 +15,15 @@
         "security/ir.model.access.csv",
         "security/pms_website_sale.xml",
         "views/templates.xml",
-        "views/assets.xml",
     ],
+    "assets": {
+        "web.assets_frontend": [
+            "pms_website_sale/static/src/js/pms_property.js",
+            "pms_website_sale/static/src/js/pms_property_booking.js",
+        ],
+        "web.assets_tests": [
+            "pms_website_sale/static/tests/tours/pms_website_sale_tour.js",
+        ],
+    },
     "maintainers": ["max3903"],
 }
--- pms_website_sale/readme/DESCRIPTION.rst
+++
@@ -1,3 +0,0 @@
-This module is the base module for the property management system (PMS) modules.
-
-It provides the "Properties" apps with menu, settings, groups and data.
