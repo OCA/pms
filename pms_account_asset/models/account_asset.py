03053dfdadb17b840d7f5b99a0811242317f8027
Maxime Chambreuil <maxime.chambreuil@graymatterlogic.com>
2026-06-07 21:23:33 -0600

    [MIG] pms_account_asset: Migration to 19.0

--- pms_account_asset/models/account_asset.py
+++ pms_account_asset/models/account_asset.py
@@ -1,6 +1,6 @@
 # Copyright 2019  Pablo Quesada
 # Copyright 2019  Dario Lodeiros
-# Copyright (c) 2021 Open Source Integrators
+# Copyright (c) 2021 Gray Matter Logic
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import fields, models
 
